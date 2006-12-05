a872044e2a829e2f4fdfbdd6c2f359e77998e6b5
radebatz <radebatz@2e913b60-282b-11dc-bd19-815372732833>
2006-12-05 07:19:58 +0000

    *** empty log message ***
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@48 2e913b60-282b-11dc-bd19-815372732833

--- zen-cart/admin/includes/boxes/extra_boxes/zenmagick_tools_dhtml.php
+++ zen-cart/admin/includes/boxes/extra_boxes/zenmagick_tools_dhtml.php
@@ -38,13 +38,52 @@ if (!defined('IS_ADMIN_FLAG')) {
 require_once('../zenmagick/init.php');
 require_once('../zenmagick/admin_init.php');
 
-$contents = file_get_contents(DIR_WS_BOXES . "extras_dhtml.php");
+    //********** enable ZenMagick menu *************
+    define('ZEN_ADMIN_FILE', DIR_WS_BOXES . "extras_dhtml.php");
+    $contents = file_get_contents(ZEN_ADMIN_FILE);
+    if (false === strpos($contents, "zenmagick_dhtml.php") && zm_setting('isAdminAutoRebuild')) {
+        // patch
+        if (is_writeable(ZEN_ADMIN_FILE)) {
+            zm_log("** ZenMagick: patching zen-cart admin to auto-enable ZenMagick admin menu", 1);
+            $handle = fopen(ZEN_ADMIN_FILE, "at");
+            fwrite($handle, "\n<?php require(DIR_WS_BOXES . 'zenmagick_dhtml.php'); ?>");
+            fclose($handle);
+        } else {
+            zm_log("** ZenMagick: no permission to patch zen-cart admin extras_dhtml.php", 1);
+        }
+    }
 
-if (false === strpos($contents, "zenmagick_dhtml.php") && zm_setting('isAdminAutoRebuild')) {
-    // patch
-    zm_log("** ZenMagick: patching zen-cart admin to auto-enable ZenMagick admin menu", 1);
-    $handle = fopen(DIR_WS_BOXES . "extras_dhtml.php", "at");
-    fwrite($handle, "\n<?php require(DIR_WS_BOXES . 'zenmagick_dhtml.php'); ?>");
-    fclose($handle);
-}
+
+    //******** auto build ZenMagick specific sidebox dummies *************
+    $handle = opendir($zm_runtime->getThemeBoxPath());
+    $zmBoxes = array();
+    while (false !== ($file = readdir($handle))) {
+        $zmBoxes[$file] = $file;
+    }
+    closedir($handle);
+
+    define('ZEN_DIR_FS_BOXES', DIR_FS_CATALOG.DIR_WS_MODULES."sideboxes/");
+    $zcBoxes = array();
+    $handle = opendir(ZEN_DIR_FS_BOXES);
+    while (false !== ($file = readdir($handle))) {
+        $zcBoxes[$file] = $file;
+    }
+    closedir($handle);
+
+    $missingBoxes = array();
+    foreach ($zmBoxes as $box) {
+        if (!array_key_exists($box, $zcBoxes) && '.' != $box && '..' != $box && zm_ends_with($box, '.php')) {
+            $missingBoxes[$box] = $box;
+        } 
+    }
+
+    // create empty dummy files...
+    foreach ($missingBoxes as $box) {
+        if (!file_exists(ZEN_DIR_FS_BOXES.$box)) {
+            $handle = fopen(ZEN_DIR_FS_BOXES.$box, 'a');
+            fwrite($handle, '<?php /** dummy file created by ZenMagick **/ ?>');
+            fclose($handle);
+        }
+    }
+
 ?>
