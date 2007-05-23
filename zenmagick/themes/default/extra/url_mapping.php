9a7c13de3d5aa1bc91453ad17978e8b68cdfdce7
radebatz <radebatz@2e913b60-282b-11dc-bd19-815372732833>
2007-05-23 10:59:55 +0000

    *** empty log message ***
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@179 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/themes/default/extra/url_mapping.php
+++ zenmagick/themes/default/extra/url_mapping.php
@@ -26,6 +26,8 @@
 
     /*
      * The central place for mapping views to controller.
+     *
+     * NOTE: If controller name and view name correspond, no entry is required.
      */
 
     // global
@@ -39,45 +41,13 @@
     $zm_urlMapper->addMapping('address_book_process', 'address_book_edit', 'address_book_edit');
     $zm_urlMapper->addMapping('address_book_process', 'address_book_delete', 'address_book_delete');
 
-    // advanced_search
-    $zm_urlMapper->addMapping('advanced_search');
-
-    // advanced_search_result
-    $zm_urlMapper->addMapping('advanced_search_result');
-
-    // checkout_confirmation
-    $zm_urlMapper->addMapping('checkout_confirmation');
-
-    // checkout_payment_address
-    $zm_urlMapper->addMapping('checkout_payment_address');
-
-    // checkout_payment
-    $zm_urlMapper->addMapping('checkout_payment');
-
-    // checkout_shipping_address
-    $zm_urlMapper->addMapping('checkout_shipping_address');
-
-    // checkout_shipping
-    $zm_urlMapper->addMapping('checkout_shipping');
-
-    // checkout_success
-    $zm_urlMapper->addMapping('checkout_success');
-
     // contact_us
     $zm_urlMapper->addMapping('contact_us');
-    //TODO: the redirect is in still in zen-cart code
-    $zm_urlMapper->addMapping('contact_us_success');
     $zm_urlMapper->addMapping('contact_us', 'contact_us_success', 'success', true, true);
-
-    // create_account
-    $zm_urlMapper->addMapping('create_account');
 
     // discount_coupon
     $zm_urlMapper->addMapping('discount_coupon');
     $zm_urlMapper->addMapping('discount_coupon', 'discount_coupon_info', 'info');
-
-    // featured_products
-    $zm_urlMapper->addMapping('featured_products');
 
     // gv_send
     $zm_urlMapper->addMapping('gv_send');
@@ -94,21 +64,9 @@
     $zm_urlMapper->addMapping('login', 'login', null, false, true);
     $zm_urlMapper->addMapping('login', 'account', 'success', true, true);
 
-    // logoff
-    $zm_urlMapper->addMapping('logoff', 'logoff', 'success', false, true);
-
-    // site_map
-    $zm_urlMapper->addMapping('site_map');
-
-    // page
-    $zm_urlMapper->addMapping('page');
-
     // password_forgotten
     $zm_urlMapper->addMapping('password_forgotten');
     $zm_urlMapper->addMapping('password_forgotten', 'login', 'success', true, true);
-
-    // product_comparison
-    $zm_urlMapper->addMapping('product_comparison');
 
     // product_info
     $zm_urlMapper->addMapping('product_info');
@@ -117,55 +75,13 @@
     $zm_urlMapper->addMapping('product_info', 'document_product_info', 'document_product_info');
     $zm_urlMapper->addMapping('product_info', 'product_free_shipping_info', 'product_free_shipping_info');
 
-    // product_reviews
-    $zm_urlMapper->addMapping('product_reviews');
-
-    // product_reviews_info
-    $zm_urlMapper->addMapping('product_reviews_info');
-
-    // product_reviews_write
-    $zm_urlMapper->addMapping('product_reviews_write');
-
-    // products_new
-    $zm_urlMapper->addMapping('products_new');
-
-    // products_new
-    $zm_urlMapper->addMapping('specials');
-
-    // reviews
-    $zm_urlMapper->addMapping('reviews');
-
-    // static
-    $zm_urlMapper->addMapping('static');
-
-    // tell_a_friend
-    $zm_urlMapper->addMapping('tell_a_friend');
-
-    // account
-    $zm_urlMapper->addMapping('account');
-
     // account_edit
     $zm_urlMapper->addMapping('account_edit');
     $zm_urlMapper->addMapping('account_edit', 'account', 'success', true, true);
 
-    // account_history
-    $zm_urlMapper->addMapping('account_history');
-
-    // account_history_info
-    $zm_urlMapper->addMapping('account_history_info');
-
-    // account_newsletters
-    $zm_urlMapper->addMapping('account_newsletters');
-
-    // account_notifications
-    $zm_urlMapper->addMapping('account_notifications');
-
     // account_password
     $zm_urlMapper->addMapping('account_password');
     $zm_urlMapper->addMapping('account_password', 'account', 'success', true, true);
-
-    // address_book
-    $zm_urlMapper->addMapping('address_book');
 
     // shopping_cart
     $zm_urlMapper->addMapping('shopping_cart');
