861d175e13891a30fad8cb584ff615b1a8aab220
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2007-09-05 23:47:54 +0000

    new crossell support plugin
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@337 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_useo3/Readme.txt
+++ zenmagick/plugins/request/zm_useo3/Readme.txt
@@ -1,15 +1,31 @@
-This is the ZenMagick plugin for Ultimate SEO 3.x support.
+This is the ZenMagick plugin for Crossell support.
 
 
 Installation
 ============
-1) Download and unpack Ultimate SEO 3.x and unpack into your zen-cart
-   installation as documented. DO NOT MODIFY ANY FILES!!
-2) Unzip the plugin package into the zenmagick/plugins directory.
+1) Download and unpack the zen-cart Crossell module from
+   here: http://www.zen-cart.com/index.php?main_page=product_contrib_info&cPath=40_60&products_id=76
+2) Unzip this plugin into the zenmagick/plugins directory.
 3) Install plugin using the ZenMagick Plugin Manager.
-4) Reload a storefront page to activate Ultimate SEO installation itself
-5) Configure Ultimate SEO 3.x options
+4) Enjoy
 
 
-After the plugin has been installed you should have the Ultimate SEO
-configuration options available in the admin interface.
+Usage
+=====
+In your product template you may use the following code:
+
+    <?php if (is_object($zm_crossell)) { $crossellProducts = $zm_crossell->getXSellForProductId($zm_product->getId()); ?>
+        <?php if (0 < count($crossellProducts)) { ?>
+          <h2>You might also be interested in any of these products:</h2>
+          <?php foreach ($crossellProducts as $xprod) { ?>
+              Product: <?php echo $xprod->getName() ?><br>
+              Model: <?php echo $xprod->getModel() ?><br>
+          <?php } ?>
+        <?php } ?>
+    <?php } ?>
+
+Basically, the returned products have all the properties and methods available as all other
+product objects created by ZenMagick.
+
+Please note that as long as you have a product id to use for the lookup, you can display crossell
+products whereever you like (sidebox, etc).
---
+++ zenmagick/plugins/request/zm_useo3/zm_crossell.php
@@ -0,0 +1,120 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006,2007 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+// in case admin switches aren't added properly, assume default settings:
+if (!defined('MAX_DISPLAY_XSELL')) define('MAX_DISPLAY_XSELL',6);
+if (!defined('MIN_DISPLAY_XSELL')) define('MIN_DISPLAY_XSELL',1);
+if (!defined('XSELL_DISPLAY_PRICE')) define('XSELL_DISPLAY_PRICE','false');
+if (!defined('SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS')) define('SHOW_PRODUCT_INFO_COLUMNS_XSELL_PRODUCTS',3);
+
+
+/**
+ * Plugin for crossell support.
+ *
+ * @package net.radebatz.zenmagick.plugins.zm_crossell
+ * @author DerManoMann
+ * @version $Id$
+ */
+class zm_crossell extends ZMPlugin {
+
+    /**
+     * Default c'tor.
+     */
+    function zm_crossell() {
+        parent::__construct('ZenMagick Crossell', 'Adds Crossell support for ZenMagick', '${plugin.version}');
+        $this->setLoaderSupport('ALL');
+    }
+
+    /**
+     * Default c'tor.
+     */
+    function __construct() {
+        $this->zm_crossell();
+    }
+
+    /**
+     * Default d'tor.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+
+    /**
+     * Init this plugin.
+     */
+    function init() {
+    global $zm_categories;
+
+        parent::init();
+
+        $zm_categories = $this->create("Categories");
+    }
+
+
+    /**
+     * Get x-sell products for the given productId.
+     *
+     * <p>This will return no products, if the number of products is less than the 
+     * configured minimum. It is possible to ignore the min setting by setting
+     * <code>ignoreMinLimit</code> to <code>true</code>.</p>
+     *
+     * <p>Will return an empty list if the plugin is disabled.</p>
+     *
+     * @param int productId The product id.
+     * @param boolean ignoreMinLimit If set to true, the minimum limit will be ignored; default is false
+     * @return array List of <code>ZMProduct</code> objects.
+     */
+    function getXSellForProductId($productId, $ignoreMinLimit=false) {
+    global $zm_products;
+
+        if (!$this->isEnabled()) {
+            return array();
+        }
+
+        $db = $this->getDB();
+        $sql = "select distinct xp.xsell_id from " . TABLE_PRODUCTS_XSELL . " xp
+                where xp.products_id = :productId
+                order by xp.sort_order asc limit :limit";
+        $sql = $db->bindVars($sql, ":productId", $productId, "integer");
+        $sql = $db->bindVars($sql, ":limit", MAX_DISPLAY_XSELL, "integer");
+
+        $productIds = array();
+        $results = $db->Execute($sql);
+        while (!$results->EOF) {
+            array_push($productIds, $results->fields['xsell_id']);
+            $results->MoveNext();
+        }
+
+        if (!$ignoreMinLimit && count(productIds) < MIN_DISPLAY_XSELL) {
+            return array();
+        }
+
+        return $zm_products->getProductsForIds($productIds);
+    }
+
+}
+
+?>
