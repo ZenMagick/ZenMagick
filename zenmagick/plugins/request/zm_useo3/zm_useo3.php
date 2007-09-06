85c6e4b1c06e2a7bbc566ae2f57d21c3d8bf3c66
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2007-09-06 07:00:14 +0000

    revert accidental changes
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@340 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/request/zm_useo3/zm_useo3.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006,2007 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+/**
+ * Plugin for Ultimate SEO 3.x support.
+ *
+ * <p><strong>NOTE:</strong> Loader support is set to <em>ALL</em> to hide the
+ * function <code>zen_href_link_seo</code> if not installed/enabled.</p>
+ *
+ * @package net.radebatz.zenmagick.plugins.zm_useo3
+ * @author mano
+ * @version $Id: zm_useo3.php 216 2007-07-16 22:07:44Z DerManoMann $
+ */
+class zm_useo3 extends ZMPlugin {
+
+    /**
+     * Default c'tor.
+     */
+    function zm_useo3() {
+        parent::__construct('ZenMagick Ultimate SEO3', 'Ultimate SEO 3.x for ZenMagick', '${plugin.version}');
+        $this->setLoaderSupport('ALL');
+    }
+
+    /**
+     * Default c'tor.
+     */
+    function __construct() {
+        $this->zm_useo3();
+    }
+
+    /**
+     * Default d'tor.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+}
+
+?>
