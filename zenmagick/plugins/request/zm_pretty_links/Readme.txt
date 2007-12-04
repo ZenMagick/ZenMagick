48efd7345d8ada385a6507f31626b658c1c72e39
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2007-12-04 04:32:48 +0000

    add pretty links plugin
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@525 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/request/zm_pretty_links/Readme.txt
@@ -0,0 +1,10 @@
+This is the ZenMagick plugin for pretty link (SEO) support.
+
+
+Installation
+============
+1) Unzip the plugin package into the zenmagick/plugins directory.
+2) Install plugin using the ZenMagick Plugin Manager.
+3) Add the provided .htaccess redirect rules to your main .htaccess file
+   NOTE: If you have already a .htaccess file, make sure to not override
+   existing rules.
---
+++ zenmagick/plugins/request/zm_pretty_links/zm_pretty_links.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006,2007 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+/**
+ * Plugin for pretty link (SEO) support.
+ *
+ * @package org.zenmagick.plugins.zm_pretty_links
+ * @author mano
+ * @version $Id$
+ */
+class zm_pretty_links extends ZMPlugin {
+
+    /**
+     * Default c'tor.
+     */
+    function __construct() {
+        parent::__construct('ZenMagick Pretty Links', 'Pretty Links (SEO) for ZenMagick', '${plugin.version}');
+        $this->setLoaderSupport('ALL');
+    }
+
+    /**
+     * Default c'tor.
+     */
+    function zm_pretty_links() {
+        $this->__construct();
+    }
+
+    /**
+     * Default d'tor.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+}
+
+?>
