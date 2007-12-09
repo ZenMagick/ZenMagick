a224f05e8ab7a40e9dcb4e01b757107cd30d12d4
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2007-12-09 22:30:06 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@552 2e913b60-282b-11dc-bd19-815372732833

--- zen-cart/includes/functions/extra_functions/zenmagick_zencart_overrides.php
+++ zen-cart/includes/functions/extra_functions/zenmagick_zencart_overrides.php
@@ -38,6 +38,7 @@ if (!function_exists('zen_mail')) {
         // use zen_mail_org as fallback for emails without ZenMagick template
         if ('none' != zm_email_formats($module)) {
             // call ZenMagick implementation
+            // NOTE: zm_mail will eventually call zen_mail_org to actually send the generated email...
             zm_mail($subject, $module, $block, $toAddress, $toName, $fromAddress, $fromName);
         } else {
             // call renamed original function
