fe1883369afffb1fdb8141770d60feabf6625745
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2007-12-17 21:03:13 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@566 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_product_associations/sql/associations_demo.sql
+++ zenmagick/plugins/request/zm_product_associations/sql/associations_demo.sql
@@ -10,3 +10,5 @@
 
 INSERT INTO zm_product_associations(association_type, source_product_id, target_product_id, start_date) 
   VALUES (1, 19, 7, now());
+INSERT INTO zm_product_associations(association_type, source_product_id, target_product_id, start_date) 
+  VALUES (1, 19, 6, now());
