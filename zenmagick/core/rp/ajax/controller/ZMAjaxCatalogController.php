3f6cd420ce7300b1a6be936c60380cc0dff7a902
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-01-16 22:28:25 +0000

    renamed
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@624 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/core/rp/ajax/controller/ZMAjaxCatalogController.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006,2007 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Ajax controller for JSON catalog data.
+ *
+ * @author mano
+ * @package org.zenmagick.rp.ajax.controller
+ * @version $Id$
+ */
+class ZMAjaxCatalogController extends ZMAjaxController {
+
+    /**
+     * Default c'tor.
+     */
+    function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Default c'tor.
+     */
+    function ZMAjaxCatalogController() {
+        $this->__construct();
+    }
+
+    /**
+     * Default d'tor.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+    /**
+     * Get product information for the given product id.
+     *
+     * @param int productd The product id.
+     * @return void
+     */
+    function getProductForIdJSON() {
+    global $zm_request, $zm_products;
+
+        $productId = $zm_request->getParameter('productId', 0);
+
+        $flatObj = $this->flattenObject($zm_products->getProductForId($productId),
+            array('id', 'name', 'description', 'model', 
+                'attributes' => array('id', 'type', 'name',
+                    'values' => array('id', 'name', 'default')
+                )
+            )
+        );
+        $json = $this->toJSON($flatObj);
+        $this->setJSONHeader($json);
+    }
+
+    /**
+     * Get products for the given category id.
+     *
+     * @param int categoryId The categoryId id.
+     * @param boolean all Admin only parameter to allow to retrieve inactive products also.
+     * @return void
+     */
+    function getProductsForCategoryIdJSON() {
+    global $zm_request, $zm_products;
+
+        $categoryId = $zm_request->getParameter('categoryId', 0);
+        $activeOnly = true;
+        if (zm_setting('isAdmin')) {
+            $activeOnly = $zm_request->getParameter('active', true);
+        }
+
+        $flatObj = $this->flattenObject($zm_products->getProductsForCategoryId($categoryId, $active),
+            array('id', 'name', 'description', 'model', 
+                'attributes' => array('id', 'type', 'name',
+                    'values' => array('id', 'name', 'default')
+                )
+            )
+        );
+        $json = $this->toJSON($flatObj);
+        $this->setJSONHeader($json);
+    }
+
+}
+
+?>
