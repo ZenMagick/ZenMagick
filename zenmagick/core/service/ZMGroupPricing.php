1e5d632820ef5b239126ff0e7494786627cf981b
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-02-20 02:09:21 +0000

    add
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@754 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/core/service/ZMGroupPricing.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2008 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Group pricing.
+ *
+ * @author mano
+ * @package org.zenmagick.service
+ * @version $Id$
+ */
+class ZMGroupPricing extends ZMService {
+
+    /**
+     * Default c'tor.
+     */
+    function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Default c'tor.
+     */
+    function ZMGroupPricing() {
+        $this->__construct();
+    }
+
+    /**
+     * Default d'tor.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+
+    /**
+     * Get all price groups.
+     *
+     * @return array List of ZMPriceGroup objects.
+     */
+    function getPriceGroups() {
+        $db = $this->getDB();
+        $sql = "select * from " . TABLE_GROUP_PRICING;
+
+        $groups = array();
+        $results = $db->Execute($sql);
+        while (!$results->EOF) {
+            $groups[] = $this->_newPriceGroup($results->fields);
+            $results->MoveNext();
+        }
+
+        return $groups;
+    }
+
+    /**
+     * Get a price group for the given id.
+     *
+     * @param int priceGroupId The id.
+     * @return ZMPriceGroup The group or <code>null</code>.
+     */
+    function getPriceGroupForId($priceGroupId) {
+        $db = $this->getDB();
+        $sql = "select *
+                from " . TABLE_GROUP_PRICING . "
+                where  group_id = :priceGroupId";
+        $sql = $db->bindVars($sql, ":priceGroupId", $priceGroupId, "integer");
+
+        $results = $db->Execute($sql);
+        $priceGroup = null;
+        if (0 < $results->RecordCount()) {
+            $priceGroup = $this->_newPriceGroup($results->fields);
+        }
+        return $priceGroup;
+    }
+
+    /**
+     * Create new price group instance.
+     */
+    function &_newPriceGroup($fields) {
+        $priceGroup = $this->create("PriceGroup");
+        $priceGroup->id_ = $fields['group_id'];
+        $priceGroup->name_ = $fields['group_name'];
+        $priceGroup->discount_ = $fields['group_percent'];
+        $priceGroup->dateAdded_ = $fields['date_added'];
+        $priceGroup->lastModified_ = $fields['last_modified'];
+        return $priceGroup;
+    }
+
+}
+
+?>
