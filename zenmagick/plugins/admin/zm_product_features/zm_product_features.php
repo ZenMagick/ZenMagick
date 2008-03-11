53f2479305c1f45d0ce3741b745e1584954a83cc
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-03-11 21:27:25 +0000

    resolve before trying to create instance of ZMFeatures
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@850 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/admin/zm_product_features/zm_product_features.php
+++ zenmagick/plugins/admin/zm_product_features/zm_product_features.php
@@ -93,14 +93,14 @@ class zm_product_features extends ZMPlugin {
 
         parent::init();
 
+        // make ZMFeatures available by pre-loading it
+        ZMLoader::instance()->resolve("ZMFeatures");
+
         if (zm_setting('isLegacyAPI')) {
         global $zm_features;
 
             $zm_features = ZMFeatures::instance();
         }
-
-        // make ZMFeatures available by pre-loading it
-        ZMLoader::instance()->resolve("ZMFeatures");
 
         if (0 < ZMRequest::getProductId()) {
             // only available if product selected
