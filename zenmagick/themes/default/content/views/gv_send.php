87334747c402d911793e3d7677923ceeb553ef84
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-04-10 00:05:59 +0000

    fix the gv_send form validation rule name
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1023 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/themes/default/content/views/gv_send.php
+++ zenmagick/themes/default/content/views/gv_send.php
@@ -25,7 +25,7 @@
 ?>
 
 <p><?php zm_l10n("Current available balance: <strong>%s</strong>", zm_format_currency(ZMRequest::getAccount()->getVoucherBalance(), true, false)) ?></p>
-<?php zm_secure_form(FILENAME_GV_SEND, '', 'gv_send', "post", "return validate(this);") ?>
+<?php zm_secure_form(FILENAME_GV_SEND, '', 'gvreceiverObject', "post", "return validate(this);") ?>
     <fieldset>
         <legend><?php zm_l10n("EMail Gift Certificate") ?></legend>
         <label for="to_name"><?php zm_l10n("Receiver Name") ?></label><br />
