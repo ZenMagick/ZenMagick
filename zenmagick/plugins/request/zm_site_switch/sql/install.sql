ec661e57efe3205a73562dda1e0030d46c1a46a8
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-07-03 22:14:02 +0000

    add sql to track users and orders
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1255 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/request/zm_site_switch/sql/install.sql
@@ -0,0 +1,2 @@
+ALTER TABLE orders ADD store_id varchar(64) NOT NULL;
+ALTER TABLE customers ADD store_id varchar(64) NOT NULL;
---
+++ zenmagick/plugins/request/zm_site_switch/sql/uninstall.sql
@@ -0,0 +1,2 @@
+ALTER TABLE orders DROP store_id;
+ALTER TABLE customers DROP store_id;
