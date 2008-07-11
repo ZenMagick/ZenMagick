f632419b72dca106cd75b7b410e7b834022f3bc9
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-07-11 00:43:31 +0000

    play with tests
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1281 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/request/zm_tests/tests/zen-cart-pricing.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Test calculated prices.
+ *
+ * <p>This test requires a vanilla demo store database setup.</p>
+ *
+ * @package org.zenmagick.plugins.zm_tests.tests
+ * @author DerManoMann
+ * @version $Id$
+ */
+class TestZenCartPricing extends UnitTestCase {
+
+    /**
+     * Test product prices.
+     */
+    public function testProducts() {
+        $this->assertTrue(true);
+    }
+
+    //todo: remove
+    public function testFailed() {
+      throw new Exception();
+    }
+
+}
+
+?>
--- zenmagick/plugins/request/zm_tests/views/tests.php
+++ zenmagick/plugins/request/zm_tests/views/tests.php
@@ -1,2 +1,21 @@
 <h1>yo</h1>
 <?php echo $zm_tests->getPluginDir() ?>
+
+<?php
+
+    // todo:
+    // - move into controller
+    // - create ZenMagick suite class to collect tests
+    // - move zm tests into separate subfolder?
+
+    // load tests
+    $testsLoader = ZMLoader::make("Loader");
+    $testsLoader->addPath($zm_tests->getPluginDir().'tests/');
+    // tests are lower case for simplicity...
+    $testsLoader->loadStatic();
+
+    $suite = new TestSuite('ZenMagick Tests');
+    $suite->addTestCase(new TestZenCartPricing());
+    $suite->run(new HtmlReporter());
+
+?>
