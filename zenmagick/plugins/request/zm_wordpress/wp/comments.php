7bc04e0903ceec7b2e6d6074b494a41b3ec0bfc9
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-08-11 11:06:49 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1404 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_wordpress/wp/comments.php
+++ zenmagick/plugins/request/zm_wordpress/wp/comments.php
@@ -1,4 +1,3 @@
-YO - Comments+++++++++++++++++
 <?php if ($comments) : ?>
 	<h3 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses' );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
 
