10065c5083ef5f1afff0dffa7cf1f990e065fe83
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-08-11 02:25:42 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1400 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/request/zm_tests/tests/misc/TestCompareUrls.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Test url comparison.
+ *
+ * @package org.zenmagick.plugins.zm_tests.tests
+ * @author DerManoMann
+ * @version $Id$
+ */
+class TestCompareUrls extends UnitTestCase {
+
+    /**
+     * Test current.
+     */
+    public function testCurrent() {
+        $this->assertTrue(ZMTools::compareStoreUrl('index.php?main_page=tests&abc=def'));
+    }
+
+    /**
+     * Test two.
+     */
+    public function testTwo() {
+        $this->assertTrue(ZMTools::compareStoreUrl('index.php?main_page=tests&abc=def', 'index.php?main_page=tests'));
+        $this->assertFalse(ZMTools::compareStoreUrl('index.php?main_page=page&id=1', 'index.php?main_page=page'));
+        $this->assertTrue(ZMTools::compareStoreUrl('index.php?main_page=static&cat=foo', 'http://localhost/index.php?main_page=static&cat=foo'));
+    }
+
+}
+
+?>
