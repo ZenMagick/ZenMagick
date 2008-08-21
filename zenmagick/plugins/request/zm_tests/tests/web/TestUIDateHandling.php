6b7ba4579359151e55a92ad0a7d5504711760b4a
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-08-21 10:42:48 +0000

    test!
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1436 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_tests/tests/web/TestUIDateHandling.php
+++ zenmagick/plugins/request/zm_tests/tests/web/TestUIDateHandling.php
@@ -16,9 +16,7 @@ class TestUIDateHandling extends UnitTestCase {
         ZMRequest::setParameterMap(array('dob' => '09/08/1966'));
         $account = ZMLoader::make('Account');
         $account->populate();
-        echo $account->getDob()."<BR>";
-        echo ZM_DATETIME_FORMAT .'-> '. UI_DATE_FORMAT."<BR>";
-        echo ZMToolbox::instance()->locale->shortDate($account->getDob(), false)."<BR>";
+        $this->assertEqual('09/08/1966', ZMToolbox::instance()->locale->shortDate($account->getDob(), false));
 
     }
 
