dafa3c3a83c176042864c97674950e4ad089b74a
DerManoMann <DerManoMann@2e913b60-282b-11dc-bd19-815372732833>
2008-09-02 01:07:28 +0000

    uhuh
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1486 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_tests/tests/service/TestZMOrders.php
+++ zenmagick/plugins/request/zm_tests/tests/service/TestZMOrders.php
@@ -13,9 +13,15 @@ class TestZMOrders extends UnitTestCase {
      * Test create product.
      */
     public function testUpdateOrderStatus() {
-        $order = ZMOrders::instance()->getOrderForId(132);
+        $order = ZMOrders::instance()->getOrderForId(1);
         $order->setStatus(4);
         ZMOrders::instance()->updateOrder($order);
+        $order = ZMOrders::instance()->getOrderForId(1);
+        $this->assertEqual('Update', $order->getStatus());
+        $order->setStatus(2);
+        ZMOrders::instance()->updateOrder($order);
+        $order = ZMOrders::instance()->getOrderForId(1);
+        $this->assertEqual('Processing', $order->getStatus());
     }
 
 }
