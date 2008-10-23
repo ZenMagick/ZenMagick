9248e6c0510cddbb2cfcadd9c3cf1a8e1afd96d2
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2008-10-23 09:33:19 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1661 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_subscriptions/templates/subscription_request.php
+++ zenmagick/plugins/request/zm_subscriptions/templates/subscription_request.php
@@ -20,7 +20,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * $Id: contact_us.php 1320 2008-07-17 22:50:44Z DerManoMann $
+ * $Id$
  */
 ?>
 
