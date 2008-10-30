7bf456f210fb32747af0072c10259316574fa252
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2008-10-30 03:35:04 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1688 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_product_facets/views/facets.php
+++ zenmagick/plugins/request/zm_product_facets/views/facets.php
@@ -1,4 +1,5 @@
 <?php
+    $types = array('categories' => 'Categories', 'manufacturers' => 'Manufacturers', 'prices' => 'Price Range');
 
     function _show_cat_path($category) {
         $path = '';
@@ -9,11 +10,62 @@
         return $path;
     }
 
-    $category = ZMCategories::instance()->getCategoryForId(3);
+    function _req_parms($type, $id) {
+    global $types;
+
+        $params = array();
+        foreach ($types as $type => $name) {
+            $params[$type] = ZMRequest::getParameter($type);
+        }
+        $add = true;
+        foreach ($params[$type] as $rid) {
+            if ($rid == $id) {
+                $add = false;
+                break;
+            }
+        }
+        if ($add) {
+            $params[$type][] = $id;
+        }
+
+        $s = '';
+        foreach ($params as $type => $ids) {
+            foreach ($ids as $pid) {
+                $s .= '&'.$type.'[]='.$pid;
+            }
+        }
+        return $s;
+    }
+
+    $query = array();
+    foreach ($types as $type => $name) {
+        if (null !== ($value = ZMRequest::getParameter($type))) {
+            $query[$type] = $value;
+        }
+    }
+
+    //$category = ZMCategories::instance()->getCategoryForId(3);
     //$current = ZMFacets::instance()->filterWithTypes(array('manufacturers' => array(3)));
     //$current = ZMFacets::instance()->filterWithTypes(array('manufacturers' => array(4), 'categories' => $category->getChildIds()));
-    $current = ZMFacets::instance()->getFacets();
+    if (0 < count($query)) {
+        $current = ZMFacets::instance()->filterWithTypes($query);
+    } else {
+        $current = ZMFacets::instance()->getFacets();
+    }
 
+?>
+
+<?php foreach ($types as $type => $name) { $facet = $current[$type]; ?>
+  <div>
+    <h5><?php echo $name ?></h5>
+    <?php foreach ($facet as $id => $info) { ?>
+      <a href="<?php $net->url(null, _req_parms($type, $info['id']), false) ?>"><?php echo $info['name'] ?> (<?php echo count($info['entries']) ?>)</a><br>
+    <?php } ?>
+  </div>
+<?php } ?>
+
+<?php 
+if (false)
     foreach ($current as $type => $facet) {
         echo '<h3>'.$type.'</h3>';
         foreach ($facet as $id => $info) {
@@ -31,5 +83,4 @@
             }
         }
     }
-
 ?>
