17c206b1692434622d032b2faacbc1ab11a6db71
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2008-12-01 01:40:35 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1762 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/themes/brightsideoflife/content/boxes/search.php
+++ zenmagick/themes/brightsideoflife/content/boxes/search.php
@@ -33,9 +33,9 @@
                 <?php define('KEYWORD_DEFAULT', zm_l10n_get("enter search")); ?>
                 <?php $onfocus = "if(this.value=='" . KEYWORD_DEFAULT . "') this.value='';" ?>
                 <input type="text" id="keyword" name="keyword" value="<?php $html->encode(ZMRequest::getParameter('keyword', KEYWORD_DEFAULT)) ?>" onfocus="<?php echo $onfocus ?>" />
-                <input type="submit" class="btn" value="<?php zm_l10n("Go") ?>" />
+                <input type="submit" class="btn" value="<?php zm_l10n("Go") ?>" /><br />
+                <a class="clear" href="<?php $net->url(FILENAME_ADVANCED_SEARCH) ?>"><?php zm_l10n("Advanced Search") ?></a>
             </div>
         </form>
-        <a class="clear" href="<?php $net->url(FILENAME_ADVANCED_SEARCH) ?>"><?php zm_l10n("Advanced Search") ?></a>
     </div>
 <?php } ?>
