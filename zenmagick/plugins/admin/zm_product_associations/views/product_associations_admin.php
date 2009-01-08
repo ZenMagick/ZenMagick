3196079567765e656f4d1f1eebe017dbaea89ace
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-01-08 23:41:36 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1836 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/admin/zm_product_associations/views/product_associations_admin.php
+++ zenmagick/plugins/admin/zm_product_associations/views/product_associations_admin.php
@@ -43,6 +43,47 @@
   <a href="#TB_inline?height=355&amp;width=600&amp;inlineId=productPicker&amp;modal=true" class="thickbox">Show hidden modal content.</a>
 
   <div id="productPicker" style="display:none;">
-    <?php  echo zm_catalog_tree(ZMCategories::instance()->getCategoryTree(), '', false, true, 'pick-cat-tree'); ?>
-    <a href="#" onclick="tb_remove();return false;">OK</a>
+    <div style="float:left;width:35%;"><?php  echo zm_catalog_tree(ZMCategories::instance()->getCategoryTree(), '', false, false, 'picker-tree'); ?></div>
+    <div id="picker-prod-list" style="border:1px solid black;margin:0 5px 0 35%;;width:60%;min-height:200px;">
+    </div>
+    <div style="clear:both;text-align:right;padding:5px;">
+      <a class="btn" href="#" onclick="tb_remove();return false;">OK</a>
+    </div>
   </div>
+  <script type="text/javascript">
+      $(document).ready(function() {
+        $('#picker-tree a.tree-cat-url').each(function (i) {
+          var classes = this.className.split(' ');
+          for (var ii=0; ii<classes.length; ++ii) {
+            if (0 == classes[ii].indexOf('c:')) {
+              var token = classes[ii].split(':');
+              if (0 < token[1]) {
+                $(this).unbind().click(function() {
+                  var url = '<?php $toolbox->net->ajax('catalog', 'getProductsForCategoryId') ?>'+'&categoryId='+token[1];
+                  prodList = $('#picker-prod-list');
+                  prodList.html('Loading...');
+                  $.ajax({
+                      type: 'GET',
+                      url: '<?php $toolbox->net->ajax('catalog', 'getProductsForCategoryId') ?>'+'&categoryId='+token[1],
+                      success: function(msg) {
+                          prodList.html('');
+                          //XXX: fix
+                          var json = eval('(' + msg + ')');
+                          var html = '';
+                          for (var jj=0; jj < json.length; ++jj) {
+                              var item = json[jj];
+                              html += "Id: "+item.id+", Name: "+item.name  + '<br>';
+                          }
+                          prodList.html(html);
+                      }
+                  });
+                  return false;
+                });
+              }
+              break;
+            }
+          }
+        });
+      });
+  </script>
+
