40599ca49c01a662c12c2add3671faa1ad99bedb
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-01-20 03:03:09 +0000

    fix invalid label id
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1887 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/themes/KeepItSimple/content/default_layout.php
+++ zenmagick/themes/KeepItSimple/content/default_layout.php
@@ -35,7 +35,7 @@
         <label for="qsearch">Search:</label>
         <?php define('KEYWORD_DEFAULT', zm_l10n_get("search ...")); ?>
         <?php $onfocus = "if(this.value=='" . KEYWORD_DEFAULT . "') this.value='';" ?>
-        <input class="tbox" id="keyword" type="text" name="keyword" value="<?php $html->encode(ZMRequest::getParameter('keyword', KEYWORD_DEFAULT)) ?>" onfocus="<?php echo $onfocus ?>" title="Start typing and hit ENTER" />
+        <input class="tbox" id="qsearch" type="text" name="keyword" value="<?php $html->encode(ZMRequest::getParameter('keyword', KEYWORD_DEFAULT)) ?>" onfocus="<?php echo $onfocus ?>" title="Start typing and hit ENTER" />
         <input class="btn" alt="Search" type="image" name="searchsubmit" title="Search" src="<?php $zm_theme->themeURL("images/search.gif") ?>" />
         </p>
       </form>					
