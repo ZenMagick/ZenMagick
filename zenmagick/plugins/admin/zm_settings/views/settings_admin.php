076fcb577f6cafbb9c03ba1f45f34a6e8e35ddaf
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-02-05 09:20:05 +0000

    progress
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1942 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/admin/zm_settings/views/settings_admin.php
+++ zenmagick/plugins/admin/zm_settings/views/settings_admin.php
@@ -1,8 +1,28 @@
 <h1>Settings</h1>
 
-<?php foreach ($plugin->getConfigValues(false) as $value) { ?>
-    <?php var_dump($value) ?>
-<?php } ?>
+<script type="text/javascript">
+    var $widgetTemplates = [];
+    // use jquery to toggle value element
+</script>
 
-<?php echo 'setting some.other: '.ZMSettings::get('some.other'); ?>
- 
+<?php $toolbox->form->open('', 'fkt=settings_admin', false, array('id'=>'settings_form')) ?>
+    <fieldset>
+        <legend>Settings</legend>
+        <?php foreach ($plugin->getConfigValues(false) as $value) { ?>
+            <?php if ($value instanceof ZMWidget) { ?>
+                <p><?php echo $value->getTitle() ?>: <?php echo $value->render(); ?></p>
+            <?php } ?>
+        <?php } ?>
+    </fieldset>
+    <fieldset>
+        <legend>New Setting</legend>
+        <p>Type: <select name="type">
+            <option value="TextFormWidget">Text</option>
+            <option value="BooleanFormWidget">Boolean</option>
+        </select></p>
+        <p>Value: <input type="text" name="value"></p>
+        <p>Title: <input type="text" name="title"></p>
+        <p>Descritpion: <input type="text" name="description"></p>
+        <input type="submit" value="create">
+    </fieldset>
+</form>
