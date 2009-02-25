cc93815c949335cec724785a3d414c28d6dcfa4c
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-02-25 11:12:26 +0000

    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@1988 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/core/authentication/provider/ZMPhPassAuthentication.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 ZenMagick
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * PhPass authentication provider.
+ *
+ * <p>Uses <a href="http://www.openwall.com/phpass/">PHPass</a>.</p>
+ *
+ * @author DerManoMann
+ * @package org.zenmagick.authentication.provider
+ * @version $Id$
+ */
+class ZMPhPassAuthentication.php implements ZMAuthentication {
+    private $passwordHash_;
+
+    /**
+     * Create instance.
+     */
+    function __construct() {
+        $this->passwordHash_ = ZMLoader::make('PasswordHash', 8, false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function encryptPassword($plaintext, $salt=null) { 
+        return $this->passwordHash_->HashPassword($plaintext);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function validatePassword($plaintext, $encrypted) { 
+        return $this->passwordHash_->HashPassword($plaintext);
+        return phpbb_check_hash($plaintext, $encrypted);
+    }
+
+}
+
+?>
