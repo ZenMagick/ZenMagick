d8518facd66891652da2e0382fcd5bfe7137c4b5
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-03-18 09:50:22 +0000

    do more web testing
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2085 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_tests/tests/web/TestBrowsing.php
+++ zenmagick/plugins/request/zm_tests/tests/web/TestBrowsing.php
@@ -1,30 +1,41 @@
 <?php
+
 /**
- * Storefront testing.
+ * Storefront browsing testing.
  *
  * @package org.zenmagick.plugins.zm_tests.tests
  * @author DerManoMann
  * @version $Id$
  */
-
 class TestBrowsing extends ZMWebTestCase {
-    protected $secure_ = false;
-
     
     /**
-     * Test home.
+     * Test homepage.
      */
-    public function testHomepage() {
-        $this->get(ZMToolbox::instance()->net->url(FILENAME_DEFAULT, 'themeId=default', $this->secure_, false));
-        $this->assertText('ZenMagick');
+    public function testHome() {
+        $this->get(ZMToolbox::instance()->net->url(FILENAME_DEFAULT, '', false, false), array('themeId' => 'default'));
+        $this->assertResponse(200);
+        $this->assertTitle('ZenMagick');
+        $this->assertText('Welcome to your new ZenMagick powered online store!');
     }
 
     /**
      * Test product page.
      */
     public function testProduct() {
-        $this->get(str_replace('&amp;', '&', ZMToolbox::instance()->net->product(19, null, false).'&themeId=default'));
+        $this->get(ZMToolbox::instance()->net->url(FILENAME_PRODUCT_INFO, '', false, false), array('products_id' => '19', 'themeId' => 'default'));
+        $this->assertResponse(200);
         $this->assertTitle('There\'s Something About Mary Linked [DVD-TSAB] :: ZenMagick');
+    }
+
+    /**
+     * Test contact us page.
+     */
+    public function testContactUs() {
+        $this->get(ZMToolbox::instance()->net->url(FILENAME_CONTACT_US, '', false, false), array('themeId' => 'default'));
+        $this->assertResponse(200);
+        $this->assertTitle('Contact Us :: ZenMagick');
+        $this->assertText(' > Contact Us');
     }
 
 }
---
+++ zenmagick/plugins/request/zm_tests/tests/web/TestForms.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * Storefront forms testing.
+ *
+ * @package org.zenmagick.plugins.zm_tests.tests
+ * @author DerManoMann
+ * @version $Id: TestBrowsing.php 2081 2009-03-17 09:50:29Z dermanomann $
+ */
+class TestForms extends ZMWebTestCase {
+    
+    /**
+     * Test contact us page.
+     */
+    public function testContactUs() {
+        $this->get(ZMToolbox::instance()->net->url(FILENAME_CONTACT_US, '', false, false), array('themeId' => 'default'));
+        $this->assertResponse(200);
+        $this->assertTitle('Contact Us :: ZenMagick');
+        $this->assertText(' > Contact Us');
+    }
+
+}
+
+?>
+<?php
+/*
+assertTitle($title)	Pass if title is an exact match
+assertText($text)	Pass if matches visible and "alt" text
+assertNoText($text)	Pass if doesn't match visible and "alt" text
+assertPattern($pattern)	A Perl pattern match against the page content
+assertNoPattern($pattern)	A Perl pattern match to not find content
+assertLink($label)	Pass if a link with this text is present
+assertNoLink($label)	Pass if no link with this text is present
+assertLinkById($id)	Pass if a link with this id attribute is present
+assertNoLinkById($id)	Pass if no link with this id attribute is present
+assertField($name, $value)	Pass if an input tag with this name has this value
+assertFieldById($id, $value)	Pass if an input tag with this id has this value
+assertResponse($codes)	Pass if HTTP response matches this list
+assertMime($types)	Pass if MIME type is in this list
+assertAuthentication($protocol)	Pass if the current challenge is this protocol
+assertNoAuthentication()	Pass if there is no current challenge
+assertRealm($name)	Pass if the current challenge realm matches
+assertHeader($header, $content)	Pass if a header was fetched matching this value
+assertNoHeader($header)	Pass if a header was not fetched
+assertCookie($name, $value)	Pass if there is currently a matching cookie
+assertNoCookie($name)	Pass if there is currently no cookie of this name
+
+$this->setMaximumRedirects(2);
+
+
+ function testContact() {
+        $this->get('http://www.lastcraft.com/');
+        $this->clickLink('About');
+        $this->assertTitle(new PatternExpectation('/About Last Craft/'));
+    }
+
+
+
+        $this->get('http://www.my-site.com/');
+        $this->assertField('a', 'A default');
+        $this->setField('a', 'New value');
+        $this->click('Go');
+
+
+
+
+class SimpleFormTests extends WebTestCase {
+    ...
+    function testNoSuperuserChoiceAvailable() {
+        $this->get('http://www.lastcraft.com/form_testing_documentation.php');
+        $this->assertFalse($this->setField('type', 'Superuser'));
+    }
+}
+
+The current selection will not be changed if the new value is not an option.
+
+Here is the full list of widgets currently supported...
+
+    * Text fields, including hidden and password fields.
+    * Submit buttons including the button tag, although not yet reset buttons
+    * Text area. This includes text wrapping behaviour.
+    * Checkboxes, including multiple checkboxes in the same form.
+    * Drop down selections, including multiple selects.
+    * Radio buttons.
+    * Images.
+
+    *
+
+
+
+class SimpleFormTests extends WebTestCase {
+    function testMyJavascriptForm() {
+        $this->clickSubmit('OK', array('a_hidden_field'=>'123'));
+    }
+
+}
+
+Bear in mind that in doing this you're effectively stubbing out a part of your software (the javascript code in the form), and perhaps you might be better off using something like Selenium to ensure a complete test.
+Raw posting
+
+If you want to test a form handler, but have not yet written or do not have access to the form itself, you can create a form submission by hand.
+
+class SimpleFormTests extends WebTestCase {
+    ...    
+    function testAttemptedHack() {
+        $this->post(
+                'http://www.my-site.com/add_user.php',
+                array('type' => 'superuser'));
+        $this->assertNoText('user created');
+
+
+
+
+
+ */
+
+?>
