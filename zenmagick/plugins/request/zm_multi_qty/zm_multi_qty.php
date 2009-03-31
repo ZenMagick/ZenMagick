533fea06307333622b0e109f170a4af138576f8e
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-03-31 10:05:24 +0000

    oops
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2125 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/plugins/request/zm_multi_qty/zm_multi_qty.php
+++ zenmagick/plugins/request/zm_multi_qty/zm_multi_qty.php
@@ -74,8 +74,12 @@ class zm_multi_qty extends ZMPlugin {
         if (null != ZMRequest::getParameter(MULTI_QUANTITY_ID)) {
             // this is a multi qty add, so leave it to the custom controller to do so
             unset($_GET['action']);
-            // tweak the ZM_PAGE_KEY parameter to use our custom controller
-            ZMRequest::setParameter(ZM_PAGE_KEY, 'multi_qty_product_info');
+
+            // TODO: make nicer
+            // create mapping for lookup
+            ZMUrlMapper::instance()->setMappingInfo('product_info', array('controller' => 'MultiQtyProductInfoController'));
+            // tweak the main_page parameter as controller id is private!
+            ZMRequest::setParameter('main_page', 'multi_qty_product_info');
 
             // add url mappings
             if (ZMSettings::get('isShowCartAfterAddProduct')) {
