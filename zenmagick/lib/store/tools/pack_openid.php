016bfbe8ee419cd6f6d711e5ce621258ade35b8e
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-06-25 05:20:28 +0000

    moved
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2324 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/lib/store/tools/pack_openid.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 ZenMagick
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+    // load ZenMagick core
+    include dirname(dirname(dirname(__FILE__))) . '/external.php';
+    $coreDir = dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR;
+    ZMLoader::instance()->addPath($coreDir);
+
+    /**
+     * Custom class for OpenID specific dependency handling.
+     */
+    class OpenIDPacker extends ZMPhpPackagePacker {
+        /**
+         * {@inheritDoc}
+         */
+        public function finalizeDependencies($dependencies, $files) {
+            foreach ($dependencies as $class => $list) {
+                foreach ($list as $ii => $name) {
+                    if ('OpenID' == $name) {
+                        unset($dependencies[$class][$ii]);
+                    }
+                }
+            }
+            return $dependencies;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public function ignoreFile($file) {
+          return in_array(basename($file), 
+              array('Server.php', 'ServerRequest.php', 'DumbStore.php', 'FileStore.php', 'PostgreSQLStore.php', 'SQLiteStore.php', 'SQLStore.php'));
+        }
+    }
+
+
+    $dir = 'C:/Program Files/Apache Group/Apache2/htdocs/php-openid-2.1.2/Auth/';
+    $packer = new OpenIDPacker($dir, dirname($dir).'/openid.packed.php');
+    $packer->setDebug(false);
+    $packer->packFiles(true);
+
+?>
