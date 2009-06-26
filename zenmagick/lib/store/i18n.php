851236ef450bcd1531f88e8e53ebc445162bf456
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-06-26 04:24:48 +0000

    moved
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2335 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/lib/store/i18n.php
@@ -0,0 +1,111 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * $Id: i18n.php 1966 2009-02-14 10:52:50Z dermanomann $
+ */
+?>
+<?php
+
+if (!function_exists('zen_date_raw')) {
+
+    /**
+     * Convert UI date into a <em>raw date format</em> that zen-cart
+     * understands.
+     *
+     * <p>This generic implementation will work as long as <code>UI_DATE_FORMAT</code>
+     * is defined.<br>
+     * The function will honour <code>DD</code>, <code>MM</code>, <code>CC</code>, <code>YY</code>
+     * and <code>YYYY</code> in the format.</p>
+     *
+     * <p><strong>NOTE:</strong> The format is *not* case sensitive.</p>
+     *
+     * @package org.zenmagick.store.locale
+     * @param string date A date (usually part of a form submit by the user).
+     * @param boolean reverse If <code>true</code>, the returned data will be reversed.
+     * @return string The provided date converted into the format <code>YYYYDDMM</code> or <code>MMDDYYYY</code>, respectivley.
+     */
+    function zen_date_raw($date, $reverse=false) {
+        $da = ZMTools::parseDateString($date, UI_DATE_FORMAT);
+        $raw = $reverse ? $da['mm'].$da['dd'].$da['cc'].$da['yy'] : $da['cc'].$da['yy'].$da['mm'].$da['dd'];
+        return $raw;
+    }
+
+}
+
+    /**
+     * Lookup and echo a i18n specific setting.
+     *
+     * @package org.zenmagick.store.locale
+     * @param string name The setting name.
+     * @return string A value or null.
+     */
+    function zm_i18n($name) {
+        $i18n = array();
+        if (isset($GLOBALS['_zm_i18n_text'])) {
+            $i18n = $GLOBALS['_zm_i18n_text'];
+        }
+
+        return isset($i18n[$name]) ? $i18n[$name] : null;
+    }
+
+    /**
+     * Add i18n setting.
+     *
+     * @package org.zenmagick.store.locale
+     * @param mixed name The setting name or an array of settings.
+     * @param string value The value (will be ignored if <code>name</code> is of type array).
+     */
+    function zm_i18n_add($name, $value=null) {
+        // ensure we have an array to start with
+        if (!isset($GLOBALS['_zm_i18n_text'])) {
+            $GLOBALS['_zm_i18n_text'] = array();
+        }
+
+        if (is_array($name)) {
+            $GLOBALS['_zm_i18n_text'] = array_merge($GLOBALS['_zm_i18n_text'], $name);
+        } else {
+            $GLOBALS['_zm_i18n_text'][$name] = $value;
+        }
+    }
+
+    /**
+     * Finalise i18n settings.
+     *
+     * <p>This function will convert all i18n settings into corresponding <code>defines</code> and
+     * do all additional stuff that should be done just once.</p>
+     *
+     * @package org.zenmagick.store.locale
+     */
+    function zm_i18n_finalise() {
+        if (!isset($GLOBALS['_zm_i18n_text'])) {
+            return;
+        }
+        foreach ($GLOBALS['_zm_i18n_text'] as $name => $value) {
+            define($name, $value);
+        }
+
+        if (defined('LC_TIME_LOCALE')) {
+            @setlocale(LC_TIME, LC_TIME_LOCALE);
+        }
+    }
+
+?>
---
+++ zenmagick/lib/store/l10n.php
@@ -0,0 +1,141 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * $Id: l10n.php 2332 2009-06-26 04:05:17Z dermanomann $
+ */
+?>
+<?php
+
+    /**
+     * Lookup and echo a language specific text.
+     *
+     * @package org.zenmagick.store.locale
+     * @param string text The text.
+     * @param var args A variable number of arguments that will be used as arguments for
+     *  <code>vsprintf(..)</code> to insert variables into the localized text.
+     * @return string A localized version based on the current language, or the original text.
+     */
+    function zm_l10n($text) {
+        // get the remaining args
+        $args = func_get_args();
+        array_shift($args);
+        echo _zm_l10n_lookup($text, $text, $args);
+    }
+
+    /**
+     * Lookup a language specific text.
+     *
+     * @package org.zenmagick.store.locale
+     * @param string text The text.
+     * @param var args A variable number of arguments that will be used as arguments for
+     *  <code>vsprintf(..)</code> to insert variables into the localized text.
+     * @return string A localized version based on the current language, or the original text.
+     */
+    function zm_l10n_get($text) {
+        // get the remaining args
+        $args = func_get_args();
+        array_shift($args);
+        return _zm_l10n_lookup($text, $text, $args);
+    }
+
+    /**
+     * Lookup a language specific chunk.
+     *
+     * <p>Similar to <code>zm_l10n_get(..)</code>, except that the first argument is a chunk (file-)name, rather
+     * than a real localizable string.</p>
+     *
+     * @package org.zenmagick.store.locale
+     * @param string name The chunk name.
+     * @param var args A variable number of arguments that will be used as arguments for
+     *  <code>vsprintf(..)</code> to insert variables into the localized text.
+     * @return string A localized version based on the current language, or <code>null</code>.
+     */
+    function zm_l10n_chunk_get($name) {
+        $session = ZMRequest::getSession();
+        $language = $session->getLanguage();
+
+        $file = $language->getDirectory().'/'.$name.'.txt';
+        if (Runtime::getTheme()->themeFileExists($file, 'lang/')) {
+            $args = func_get_args();
+            array_shift($args);
+            $contents = file_get_contents(Runtime::getTheme()->themeFile($file, 'lang/'));
+            if (null == $args) {
+                // no need for expensive printf!
+                return $contents;
+            }
+            return vsprintf($contents, $args);
+        }
+
+        return null;
+    }
+
+    /**
+     * Add language mappings.
+     *
+     * @package org.zenmagick.store.locale
+     * @param array map The new/additional mappings.
+     */
+    function zm_l10n_add($map) {
+        // sanitiy check
+        if (!is_array($map)) {
+            return;
+        }
+
+        // ensure we have an array to start with
+        if (!isset($GLOBALS['_zm_l10n_text'])) {
+            $GLOBALS['_zm_l10n_text'] = array();
+        }
+
+        $GLOBALS['_zm_l10n_text'] = array_merge($GLOBALS['_zm_l10n_text'], $map);
+    }
+
+    /**
+     * The actual <code>l10n</code> workhorse.
+     *
+     * @package org.zenmagick.store.locale
+     * @param string text The text.
+     * @param string default A default text in case there is no localized version of the given text.
+     * @param var args A variable number of arguments that will be used as arguments for
+     *  <code>vsprintf(..)</code> to insert variables into the localized text.
+     * @return string A localized version based on the current language, or the original text.
+     */
+    function _zm_l10n_lookup($text, $default, $args=null) {
+        $l10n = array();
+        if (isset($GLOBALS['_zm_l10n_text'])) {
+            $l10n = $GLOBALS['_zm_l10n_text'];
+        }
+
+        // get localized text or default to provided default
+        $format = isset($l10n[$text]) ? $l10n[$text] : $default;
+        //!isset($l10n[$text]) && ZMLogging::instance()->log("can't resolve l10n: '".$text."'", ZMLogging::DEBUG);
+
+        if (null == $args) {
+            // no need for expensive printf!
+            return $format;
+            // preserve % in strings that do not have anything to replace
+            //$format = str_replace('%', '%%', $format);
+        }
+
+        return vsprintf($format, $args);
+    }
+
+?>
