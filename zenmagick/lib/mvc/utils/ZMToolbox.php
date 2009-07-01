b105717a137072c5925fa2c29ae98be4f1e9e631
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-07-01 11:37:42 +0000

    moved
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2379 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/lib/mvc/utils/ZMToolbox.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * ZenMagick Core - Another PHP framework.
+ * Copyright (C) 2006,2009 ZenMagick
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Container for template related utilities.
+ *
+ * @author DerManoMann
+ * @package org.zenmagick.mvc.utils
+ * @version $Id: ZMToolbox.php 2149 2009-04-13 22:59:14Z dermanomann $
+ */
+class ZMToolbox extends ZMObject {
+
+    /**
+     * Create new instance.
+     */
+    function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Destruct instance.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+    /**
+     * Get instance.
+     */
+    public static function instance() {
+        return ZMObject::singleton('Toolbox');
+    }
+
+
+    /**
+     * Get a map of all tools.
+     *
+     * @return array A map of all available tools.
+     */
+    public function getTools() {
+        return $this->properties_;
+    }
+
+}
+
+?>
