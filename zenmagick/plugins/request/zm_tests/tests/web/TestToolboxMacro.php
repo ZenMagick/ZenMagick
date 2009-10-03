c3e5b9f72c6584838d3a23288e71b82537c81f30
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-10-03 19:14:32 +0000

    oops
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2528 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/request/zm_tests/tests/web/TestToolboxMacro.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Test the toolbox macros.
+ *
+ * @package org.zenmagick.plugins.zm_tests.tests.web
+ * @author DerManoMann
+ * @version $Id$
+ */
+class TestToolboxMacro extends ZMTestCase {
+
+    /**
+     * Test format address.
+     */
+    public function testFormatAddress() {
+    }
+
+}
+
+?>
