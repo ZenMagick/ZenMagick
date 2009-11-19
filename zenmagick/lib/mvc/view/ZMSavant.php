e9eef059cd3bf43410b8f1d0d62327e11608c3ef
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-11-19 00:01:28 +0000

    phpdoc
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2607 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/lib/mvc/view/ZMSavant.php
+++ zenmagick/lib/mvc/view/ZMSavant.php
@@ -29,11 +29,43 @@
  * <p><strong>ATTENTION:</strong> These methods only make sense if called from
  * within a template.</p>
  *
+ * <p>Also, adds support for caching. The config map supports a key <em>cache</em> that
+ * is expected to be a class name that implements the following two methods:</p>
+ * <dl>
+ *   <dt><code>get($tpl)</code></dt>
+ *   <dd>Query the cache for the given template name and return the cached contents (if any).
+ *     If the template is not cached (yet), or is not allowed to be cached, <code>null</code>
+ *     should be returned.</dd>
+ *   <dt><code>save($tpl, $result)</code></dt>
+ *   <dd>Save the contents of the given template fetch in the cache (if allowed).</dd>
+ * </dl>
+ *
+ * <p>It should be noted that it is the reponsibility of the cache class to decide whether a given
+ * template can be cached or not.</p>
+ *
  * @author DerManoMann
  * @package org.zenmagick.mvc.view
  * @version $Id$
  */
 class ZMSavant extends Savant3 {
+
+    /**
+     * Create a new instance.
+     */
+    function __construct($config=null) {
+        parent::__construct($config);
+        if ($this->__config['cache'] && !is_object($this->__config['cache'])) {
+            $this->__config['cache'] = ZMLoader::make($this->__config['cache']);
+        }
+    }
+
+    /**
+     * Destruct instance.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
 
     /**
      * Check if the given resource file exists.
