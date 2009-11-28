bf5942e85b91ef3aad29c1f2df9a7408aa8d1644
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-11-28 10:05:39 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2654 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/themes/test/extra/ProductFinder.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Simple custom product search.
+ *
+ * @author DerManoMann
+ * @package org.zenmagick.store.utils
+ * @version $Id: ZMProductFinder.php 2308 2009-06-24 11:03:11Z dermanomann $
+ */
+class ProductFinder extends ZMProductFinder {
+
+    /**
+     * Build the search SQL.
+     *
+     * @param ZMSearchCriteria criteria Search criteria.
+     * @return ZMQueryDetails The search SQL.
+     */
+    protected function buildQuery($criteria) {
+        $select = "SELECT pd.products_id, ";
+        $from = " FROM " . TABLE_PRODUCTS_DESCRIPTION . " pd";
+        $sort = ' ORDER BY weight DESC';
+        $where = ' WHERE';
+
+        // get token
+        zen_parse_search_string(stripslashes($criteria->getKeywords()), $tokens);
+
+        $weight = '';
+        $weightTemplate = 'IF (products_name LIKE "@@@%", 20, IF (products_name LIKE "%@@@%", 10, 0)) + IF (products_description LIKE "%@@@%", 5, 0)';
+
+        $whereTemplate = '(products_name LIKE "%@@@%" OR products_description LIKE "%@@@%")';
+        foreach ($tokens as $token) {
+            switch ($token) {
+                case 'and':
+                case 'or':
+                    $weight .= " + ";
+                case '(':
+                case ')':
+                    $where .= " " . $token . " ";
+                    break;
+                default:
+                    $where .= str_replace('@@@', $token, $whereTemplate);
+                    $weight .= str_replace('@@@', $token, $weightTemplate);
+            }
+        }
+
+        $sql = $select . $weight . ' AS weight'. $from . $where . $sort;
+        $tables = array(TABLE_PRODUCTS_DESCRIPTION);
+        return new ZMQueryDetails(Runtime::getDatabase(), $sql, $args, $tables, null, 'pd.products_id');
+    }
+
+}
+
+?>
