6327ea40a8fc2d2d4d4f842d98b4309676f363d2
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-11-30 10:18:30 +0000

    add new plugin admin page controller base class
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2661 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/lib/store/services/plugins/ZMPluginAdminController.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Plugin admin controller base class.
+ *
+ * @author DerManoMann
+ * @package org.zenmagick.store.services.plugins
+ * @version $Id: ZMPluginPageController.php 2650 2009-11-27 03:53:30Z dermanomann $
+ */
+class ZMPluginAdminController extends ZMController {
+    private $title_;
+    private $plugin_;
+
+
+    /**
+     * Create a new instance.
+     *
+     * @param string id The id.
+     * @param string title The page title.
+     * @param mixed plugin The parent plugin.
+     */
+    function __construct($id, $title=null, $plugin) {
+        parent::__construct($id);
+        $this->title_ = null != $title ? $title : $id;
+        $this->plugin_ = $plugin;
+    }
+
+    /**
+     * Destruct instance.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+
+    /**
+     * Set the plugin.
+     *
+     * @param mixed plugin A <code>ZMPlugin</code> instance or plugin id.
+     */
+    public function setPlugin($plugin) { 
+        $this->plugin_ = $plugin;
+    }
+
+    /**
+     * Get the plugin.
+     *
+     * @return ZMPlugin The plugin.
+     */
+    protected function getPlugin() {
+        if (!is_object($this->plugin_)) {
+            $this->plugin_ = ZMPlugins::instance()->getPluginForId($this->plugin_);
+        }
+
+        return $this->plugin_;
+    }
+
+}
+
+?>
