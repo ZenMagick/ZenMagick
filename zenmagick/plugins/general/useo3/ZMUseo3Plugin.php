fdf38f5f3efc950ca5f32bf96b6e49a26755d572
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-12-03 02:46:53 +0000

    converted
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2687 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/general/useo3/ZMUseo3Plugin.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 ZenMagick
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+/**
+ * Plugin for Ultimate SEO 3.x support.
+ *
+ * @package org.zenmagick.plugins.useo3
+ * @author mano
+ * @version $Id: zm_useo3.php 2351 2009-06-29 05:05:02Z dermanomann $
+ */
+class ZMUseo3Plugin extends Plugin {
+
+    /**
+     * Create new instance.
+     */
+    function __construct() {
+        parent::__construct('Ultimate SEO3', 'Ultimate SEO 3.x for ZenMagick', '${plugin.version}');
+        $this->setLoaderPolicy(ZMPlugin::LP_ALL);
+    }
+
+    /**
+     * Destruct instance.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+}
+
+?>
