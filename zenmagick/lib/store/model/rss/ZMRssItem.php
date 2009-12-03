54dfd710129aebf3470e17ae4267e63740100603
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-12-03 22:32:18 +0000

    update copyright
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2694 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/lib/store/model/rss/ZMRssItem.php
+++ zenmagick/lib/store/model/rss/ZMRssItem.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * ZenMagick - Extensions for zen-cart
- * Copyright (C) 2006-2009 ZenMagick
+ * Copyright (C) 2006-2009 zenmagick.org
  *
  * Portions Copyright (c)      Vojtech Semecky, webmaster @ webdot . cz
  * Portions Copyright (c) 2003 The zen-cart developers
