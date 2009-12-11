8b79be3d582700e0a454f74d2e52d8e1220a5bb1
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-12-11 03:40:09 +0000

    oops
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2714 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/lib/mvc/controller/ZMController.php
+++ zenmagick/lib/mvc/controller/ZMController.php
@@ -238,7 +238,7 @@ class ZMController extends ZMObject {
     public function getFormData($request) {
         if (null == $this->formData_ && null !== ($mapping = ZMUrlManager::instance()->findMapping($this->id_))) {
             if (array_key_exists('model', $mapping)) {
-                $this->formData_ =  ZMBeanUtils::getBean($mapping['model'].(false === strpos($viewInfo['view'], '#') ? '#' : '&').'formId='.$mapping['formId']);
+                $this->formData_ =  ZMBeanUtils::getBean($mapping['model'].(false === strpos($mapping['view'], '#') ? '#' : '&').'formId='.$mapping['formId']);
                 if ($this->formData_ instanceof ZMFormData) {
                     $this->formData_->populate($request);
                 } else {
