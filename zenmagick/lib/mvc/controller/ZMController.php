7151ed68040d82cae1a6b4443a0894ef97ecad63
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-12-11 03:37:11 +0000

    more regressions
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2712 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/lib/mvc/controller/ZMController.php
+++ zenmagick/lib/mvc/controller/ZMController.php
@@ -89,7 +89,7 @@ class ZMController extends ZMObject {
         }
 
         // form validation (only if not already error view from session validation...)
-        $formData = $this->getModel($request);
+        $formData = $this->getFormData($request);
         if (null == $view && null != $formData && $this->isFormSubmit($request)) {
             // move to function
             if (null != ($view = $this->validateFormBean($request, $formData))) {
@@ -235,7 +235,7 @@ class ZMController extends ZMObject {
      * @param ZMRequest request The request to process.
      * @return ZMObject An object instance or <code>null</code>
      */
-    public function getModel($request) {
+    public function getFormData($request) {
         if (null == $this->formData_ && null !== ($mapping = ZMUrlManager::instance()->findMapping($this->id_))) {
             if (array_key_exists('model', $mapping)) {
                 $this->formData_ =  ZMBeanUtils::getBean($mapping['model'].(false === strpos($viewInfo['view'], '#') ? '#' : '&').'formId='.$mapping['formId']);
