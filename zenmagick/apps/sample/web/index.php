5c2a6b3400c792c07151f1ffb25ed3be28d64512
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-12-18 01:24:00 +0000

    .
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2760 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/apps/sample/web/index.php
+++ zenmagick/apps/sample/web/index.php
@@ -1,7 +1,7 @@
 <?php
 
   // app location relative to zenmagick installation (ZM_BASE_PATH)
-  define('ZM_APP_PATH', 'apps'.DIRECTORY_SEPARATOR.basename(dirname(dirname(__FILE__))));
+  define('ZM_APP_PATH', 'apps'.DIRECTORY_SEPARATOR.basename(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR);
 
   require '../../../bootstrap.php';
   require '../../../mvc.php';
