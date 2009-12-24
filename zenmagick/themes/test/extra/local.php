02c71201e1d5af65b64637c3e18971cd2258176d
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2009-12-24 00:54:53 +0000

    some haml testing
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2780 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/themes/test/extra/local.php
+++ zenmagick/themes/test/extra/local.php
@@ -52,4 +52,7 @@
     }
     ZMEvents::instance()->attach(new RestrictCategory());
 
+    ZMUrlManager::instance()->setMapping(null, array('template' => 'haml1', 'view' => 'SavantView#layout=ref::null&config='.urlencode('compiler=ref::SavantHamlCompiler')));
+    ZMSettings::set('zenmagick.mvc.templates.ext', '.haml');
+
 ?>
