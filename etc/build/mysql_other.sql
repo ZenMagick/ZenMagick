92fd43e7696b175ce7f6f3c90a8fb3d7eef66c81
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2010-01-07 02:12:08 +0000

    add SQL to hide some zen cart config options in the full install
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2817 2e913b60-282b-11dc-bd19-815372732833

--- etc/build/mysql_other.sql
+++ etc/build/mysql_other.sql
@@ -8,3 +8,17 @@ UPDATE configuration set configuration_value = 'Off' WHERE configuration_key = '
 
 ## remove zen cart banners
 DELETE FROM banners;
+
+## hide some unused options
+#  8 = Configuration - Product Listing
+# 10 = Configuration - Logging
+# 14 = Configuration - GZip Compression
+# 18 = Configuration - Product Info
+# 21 = Configuration - New Listing
+# 22 = Configuration - Featured Listing
+# 23 = Configuration - All Listing
+# 24 = Configuration - Index Listing
+# 25 = Configuration - Define Page Status
+# 30 = Configuration - EZ-Pages Settings
+UPDATE configuration_group SET visible = 0 WHERE configuration_group_id in (8, 10, 14, 18, 21, 22, 23, 24, 25, 30)
+
