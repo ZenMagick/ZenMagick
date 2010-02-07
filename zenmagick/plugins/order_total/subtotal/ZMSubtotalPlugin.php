a7440985ae2f24e9ee7c8614eed347bbf577f348
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2010-02-07 09:09:37 +0000

    Initial version
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2886 2e913b60-282b-11dc-bd19-815372732833

---
+++ zenmagick/plugins/order_total/subtotal/ZMSubtotalPlugin.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * ZenMagick - Extensions for zen-cart
+ * Copyright (C) 2006-2009 zenmagick.org
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+/**
+ * ZenMagick order total module to display the subtotal.
+ *
+ * @package org.zenmagick.plugins.subtotal
+ * @author DerManoMann
+ * @version $Id$
+ */
+class ZMSubtotalPlugin extends ZMOrderTotalPlugin {
+
+    /**
+     * Create new instance.
+     */
+    function __construct() {
+        parent::__construct('Subtotal', 'Display subtotal', '${plugin.version}');
+        $this->setLoaderPolicy(ZMPlugin::LP_FOLDER);
+        $this->setPreferredSortOrder(100);
+    }
+
+    /**
+     * Destruct instance.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function calculate($request, $shoppingCart) {
+        $output = array($output[] = ZMLoader::make('OrderTotalDetails', 'Sub-Total', $shoppingCart->getSubtotal()));
+        return $output;
+    }
+
+}
+
+?>
