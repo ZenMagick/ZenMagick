7c96820e0620fa1eb12995b02af52e73cd2ac489
dermanomann <dermanomann@2e913b60-282b-11dc-bd19-815372732833>
2010-02-15 08:55:09 +0000

    add similar orders example, add limit support to override the default limit of 6
    git-svn-id: https://zenmagick.svn.sourceforge.net/svnroot/zenmagick/trunk@2896 2e913b60-282b-11dc-bd19-815372732833

--- zenmagick/lib/store/provider/ZMSimilarOrderProductAssociationHandler.php
+++ zenmagick/lib/store/provider/ZMSimilarOrderProductAssociationHandler.php
@@ -27,6 +27,8 @@
 /**
  * Product association handler for <em>also purchased</em> products.
  *
+ * <p>Supports <em>limit</em> parameter in the <code>$args</code> map.</p>
+ *
  * @author DerManoMann
  * @package org.zenmagick.store.provider
  * @version $Id$
@@ -44,6 +46,10 @@ class ZMSimilarOrderProductAssociationHandler implements ZMProductAssociationHan
      * {@inheritDoc}
      */
     public function getProductAssociationsForProductId($productId, $args=null, $all=false) {
+        $limit = 6;
+        if (is_array($args) && array_key_exists('limit', $args)) {
+            $limit = (int)$args['limit'];
+        }
         $sql = "SELECT p.products_id
                 FROM " . TABLE_ORDERS_PRODUCTS . " opa, " . TABLE_ORDERS_PRODUCTS . " opb, " . TABLE_ORDERS . " o, " . TABLE_PRODUCTS ." p
                 WHERE opa.products_id = :productId AND opa.orders_id = opb.orders_id
@@ -51,7 +57,7 @@ class ZMSimilarOrderProductAssociationHandler implements ZMProductAssociationHan
                     AND opb.orders_id = o.orders_id and p.products_status = 1
                     GROUP BY p.products_id
                     ORDER BY o.date_purchased DESC
-                    LIMIT 6";
+                    LIMIT ".$limit;
         $args = array('productId' => $productId);
 
         $assoc = array();
