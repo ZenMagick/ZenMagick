24d4ec61fd6974d376b5cfcd9aea36771f3973e9
DerManoMann <mano@zenmagick.org>
2010-03-30 12:11:42 +1300

    make the order form URL secure if using the default url

--- zenmagick/lib/store/model/checkout/ZMShoppingCart.php
+++ zenmagick/lib/store/model/checkout/ZMShoppingCart.php
@@ -369,7 +369,7 @@ class ZMShoppingCart extends ZMObject {
      */
     function getOrderFormURL() {
     global $$_SESSION['payment'];
-        $url = ZMRequest::instance()->url(FILENAME_CHECKOUT_PROCESS);
+        $url = ZMRequest::instance()->url(FILENAME_CHECKOUT_PROCESS, null, true);
         if (isset($$_SESSION['payment']->form_action_url)) {
             $url = $$_SESSION['payment']->form_action_url;
         }
