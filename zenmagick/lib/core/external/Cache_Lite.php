c788ce5d2be08fab942367de2ac00bb178df01d4
DerManoMann <mano@zenmagick.org>
2010-04-17 21:38:49 +1200

    prefix xxx_magic_quotes_runtime with '@'

--- zenmagick/lib/core/external/Cache_Lite.php
+++ zenmagick/lib/core/external/Cache_Lite.php
@@ -710,8 +710,8 @@ class Cache_Lite
         if ($fp) {
             clearstatcache();
             $length = @filesize($this->_file);
-            $mqr = get_magic_quotes_runtime();
-            set_magic_quotes_runtime(0);
+            $mqr = @get_magic_quotes_runtime();
+            @set_magic_quotes_runtime(0);
             if ($this->_readControl) {
                 $hashControl = @fread($fp, 32);
                 $length = $length - 32;
@@ -721,7 +721,7 @@ class Cache_Lite
             } else {
                 $data = '';
             }
-            set_magic_quotes_runtime($mqr);
+            @set_magic_quotes_runtime($mqr);
             if ($this->_fileLocking) @flock($fp, LOCK_UN);
             @fclose($fp);
             if ($this->_readControl) {
