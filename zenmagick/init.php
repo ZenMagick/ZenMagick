3f8d1bb65d66bf78059d1847d3442cb8cd7fad40
DerManoMann <mano@zenmagick.org>
2010-05-04 11:21:50 +1200

    re-enable temp. code to make zmIndex.php admin code work

--- zenmagick/init.php
+++ zenmagick/init.php
@@ -32,10 +32,10 @@
 
     include 'bootstrap.php';
 
-//TODO: remove again
-//ZMLoader::instance()->addPath(ZMFileUtils::mkPath(array(ZMRuntime::getInstallationPath(), 'apps', 'admin', 'lib')));
-//ZMLoader::instance()->loadStatic();
-//ZMSettings::append('zenmagick.mvc.toolbox.tools', 'admin:ToolboxAdmin');
+//TODO: used for zmIndex.php admin pages; remove once apps/admin is stable enough
+ZMLoader::instance()->addPath(ZMFileUtils::mkPath(array(ZMRuntime::getInstallationPath(), 'apps', 'admin', 'lib')));
+ZMLoader::instance()->loadStatic();
+ZMSettings::append('zenmagick.mvc.toolbox.tools', 'admin:ToolboxAdmin');
 
     // allow seo rewriters to fiddle with the request
     $_zm_request->seoDecode();
