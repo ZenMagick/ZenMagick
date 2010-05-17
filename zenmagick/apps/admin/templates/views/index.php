9255a4912999c931cf28fbf803ff124d21691c62
DerManoMann <mano@zenmagick.org>
2010-05-17 12:08:23 +1200

    add empty orders page

--- zenmagick/apps/admin/templates/views/index.php
+++ zenmagick/apps/admin/templates/views/index.php
@@ -1,7 +1,14 @@
 <h1>Dashboard...</h1>
 
 <!-- TODO: allow to filter status -->
-<?php foreach (ZMOrders::instance()->getOrderStatusList($session->getValue('languages_id')) as $status) { ?>
-  <?php $result = ZMRuntime::getDatabase()->querySingle("SELECT count(*) AS count FROM " . TABLE_ORDERS . " where orders_status = :orderStatusId", array('orderStatusId' => $status->getOrderStatusId()), TABLE_ORDERS); ?>
-<p><?php echo $status->getStatusName() ?>: <?php echo $result['count'] ?></p>
-<?php } ?>
+<div class="dbox" style="float:left;border:2px solid #aaa;padding:2px 6px;">
+  <h3>Order Stats</h3>
+  <p>
+  <?php foreach (ZMOrders::instance()->getOrderStatusList($session->getValue('languages_id')) as $status) { ?>
+    <?php $result = ZMRuntime::getDatabase()->querySingle("SELECT count(*) AS count FROM " . TABLE_ORDERS . " where orders_status = :orderStatusId", array('orderStatusId' => $status->getOrderStatusId()), TABLE_ORDERS); ?>
+    <a href="<?php echo $admin2->url('orders', 'orderStatusId='.$status->getOrderStatusId()) ?>"><?php echo $status->getStatusName() ?>: <?php echo $result['count'] ?></a><br>
+  <?php } ?>
+  </p>
+</div>
+
+<br clear="left">
