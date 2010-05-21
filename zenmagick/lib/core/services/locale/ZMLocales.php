65c995d8c7865df79b86ef2cc5bb8fab31eb6e2c
DerManoMann <mano@zenmagick.org>
2010-05-21 16:36:40 +1200

    add locale service

---
+++ zenmagick/lib/core/services/locale/ZMLocales.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * ZenMagick - Another PHP framework.
+ * Copyright (C) 2006-2010 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Locale service.
+ *
+ * <p>Delegates translations to an instance of <code>ZMLocale</code>.</p>
+ *
+ * <p>The implementation used can be configured via the setting 'zenmagick.core.locale.provider'. If none is
+ *  configured, a default echo implementation will be used.</p>
+ *
+ * @author DerManoMann
+ * @package org.zenmagick.core.services.locale
+ * @version $Id$
+ */
+class ZMLocales extends ZMObject implements ZMLocale {
+    private $locale_;
+
+
+    /**
+     * Create new instance.
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->locale_ = null;
+    }
+
+    /**
+     * Destruct instance.
+     */
+    public function __destruct() {
+        parent::__destruct();
+    }
+
+    /**
+     * Get instance.
+     */
+    public static function instance() {
+        return ZMObject::singleton('Locales');
+    }
+
+
+    /**
+     * Get the locale to be used.
+     *
+     * @return ZMLocale The locale.
+     */
+    public function getLocale() {
+        if (null == $this->locale_) {
+            $this->locale_ = ZMBeanUtils::getBean(ZMSettings::get('zenmagick.core.locale.provider', 'EchoLocale'));
+        }
+
+        return $this->locale_;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function translate($text, $context=null, $domain=ZMLocale::DEFAULT_DOMAIN) {
+        return $this->getLocale()->translate($text, $context, $domain);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function translatePlural($single, $number, $plural=null, $context=null, $domain=ZMLocale::DEFAULT_DOMAIN) {
+        return $this->getLocale()->translatePlural($single, $number, $plural, $context, $domain);
+    }
+
+}
---
+++ zenmagick/lib/core/services/locale/_zm.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * ZenMagick - Another PHP framework.
+ * Copyright (C) 2006-2010 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Translate the given text.
+ *
+ * @param string text The text to translate.
+ * @param mixed context Optional translation context; default is <code>null</code>.
+ * @param string domain The translation domain; default is <code>ZMLocale::DEFAULT_DOMAIN</code>.
+ * @return string The translated text or, if no translation found, the original text.
+ * @package org.zenmagick.core.services.locale
+ */
+function _zm($text, $context=null, $domain=ZMLocale::DEFAULT_DOMAIN) {
+    return ZMLocales::instance()->translate($text, $context, $domain);
+}
+
+
+/**
+ * Translate the given text with plural option.
+ *
+ * @param string single The text to translate for single case.
+ * @param int number The number.
+ * @param string plural The text to translate for plural case; default is <code>null</code> to default to the single case.
+ * @param mixed context Optional translation context; default is <code>null</code>.
+ * @param string domain The translation domain; default is <code>ZMLocale::DEFAULT_DOMAIN</code>.
+ * @return string The translated text or, if no translation found, the original text.
+ * @package org.zenmagick.core.services.locale
+ */
+function _zmn($single, $number, $plural=null, $context=null, $domain=ZMLocale::DEFAULT_DOMAIN) {
+    return ZMLocales::instance()->translatePlural($single, $number, $plural, $context, $domain);
+}
