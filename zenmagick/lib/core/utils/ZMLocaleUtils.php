90077768b0d9c6e7594c182d689c69425afc4471
DerManoMann <mano@zenmagick.org>
2010-06-15 10:34:00 +1200

    fix logic

--- zenmagick/lib/core/utils/ZMLocaleUtils.php
+++ zenmagick/lib/core/utils/ZMLocaleUtils.php
@@ -27,7 +27,7 @@
  * @package org.zenmagick.core.utils
  */
 class ZMlocaleUtils {
-    const L10N_PATTERNS = 'zm_l10n,_zmn,_zm,_vzm';
+    const L10N_PATTERNS = 'zm_l10n_get,zm_l10n,_zmn,_zm,_vzm';
 
     /**
      * Build a language map for all found l10n strings in the given directory tree.
@@ -42,83 +42,79 @@ class ZMlocaleUtils {
         foreach (ZMLoader::findIncludes($baseDir.DIRECTORY_SEPARATOR, $ext, true) as $filename) {
             $strings = array();
             $contents = file_get_contents($filename);
-            $pos = 0;
-            while (-1 < $pos) {
-                // search for multiple patterns
-                foreach ($patterns as $pattern) {
-                    $next = strpos($contents, $pattern, $pos);
-                    if ($next !== false) {
+            foreach ($patterns as $pattern) {
+                $pos = 0;
+                while (-1 < $pos) {
+                    // search for multiple patterns
+                    $pos = strpos($contents, $pattern, $pos);
+                    if (false === $pos || -1 == $pos) {
+                        // nothing more to do
+                        break;
+                    }
+
+                    $ob = strpos($contents, '(', $pos+1);
+                    // allow for 10 chars between pattern and '('
+                    if ($pos < $ob && ($ob-$pos) < (strlen($pattern) + 10)) {
+                        // found something
+                        // examine first non whitespace char to figure out which quote to look for
+                        $quote = '';
+                        $qi = $ob+1;
+                        while (true) {
+                            $quote = trim(substr($contents, $qi, 1));
+                            if ("'" == $quote || '"' == $quote) {
+                                break;
+                            }
+                            if ('' != $quote) {
+                                // not a string
+                                $quote = null;
+                                break;
+                            }
+
+                            ++$qi;
+                            // sanity check
+                            if ($qi-$ob > 10)
+                              break;
+                        }
+
+                        if ('' != $quote) {
+                            // have a quote
+                            $pos += $qi-$ob+1;
+                            $text = '';
+                            $lastChar = '';
+                            $start = $qi+1;
+                            $len = 0;
+                            $char = '';
+                            while (true) {
+                                $char = substr($contents, $start+$len, 1);
+                                $len++;
+                                if ($char == $quote && $lastChar != '\\') {
+                                    break;
+                                }
+                                $lastChar = $char;
+                                $text .= $char;
+                                // sanity check
+                                if ($len > 1000) {
+                                    ZMLogging::instance()->log('unbound string in '.$filename.' around char '.$pos.'; skipping', ZMLogging::WARN);
+                                    ++$pos;
+                                    break;
+                                }
+                            }
+                            $strings[$text] = $text;
+                        } else {
+                            // found something, but not a string
+                            ZMLogging::instance()->log('found something: '.substr($contents, $qi-10, 20), ZMLogging::TRACE);
+                            ++$pos; //avoid getting stuck
+                        }
+                    } else {
+                        ++$pos; //avoid getting stuck
                         break;
                     }
                 }
-                if ($next === false || -1 == $next) {
-                    // nothing more to do
-                    break;
-                }
-
-                $pos = $next;
-
-                $ob = strpos($contents, '(', $pos+1);
-                // allow for 10 chars between pattern and '('
-                if ($pos < $ob && ($ob-$pos) < (strlen($pattern) + 10)) {
-                    // found something
-                    // examine first non whitespace char to figure out which quote to look for
-                    $quote = '';
-                    $qi = $ob+1;
-                    while (true) {
-                        $quote = trim(substr($contents, $qi, 1));
-                        if ("'" == $quote || '"' == $quote) {
-                            break;
-                        }
-                        if ('' != $quote) {
-                            // not a string
-                            $quote = null;
-                            break;
-                        }
-
-                        ++$qi;
-                        // sanity check
-                        if ($qi-$ob > 10)
-                          break;
-                    }
-
-                    if ('' != $quote) {
-                        // have a quote
-                        $pos += $qi-$ob+1;
-                        $text = '';
-                        $lastChar = '';
-                        $start = $qi+1;
-                        $len = 0;
-                        $char = '';
-                        while (true) {
-                            $char = substr($contents, $start+$len, 1);
-                            $len++;
-                            if ($char == $quote && $lastChar != '\\') {
-                                break;
-                            }
-                            $lastChar = $char;
-                            $text .= $char;
-                            // sanity check
-                            if ($len > 1000) {
-                                ZMLogging::instance()->log('unbound string in '.$filename.' around char '.$pos.'; skipping', ZMLogging::WARN);
-                                ++$pos;
-                                break;
-                            }
-                        }
-                        $strings[$text] = $text;
-                    } else {
-                        // found something, but not a string
-                        ZMLogging::instance()->log('found something: '.substr($contents, $qi-10, 20), ZMLogging::TRACE);
-                        ++$pos; //avoid getting stuck
-                    }
-                } else {
-                    ++$pos; //avoid getting stuck
-                    break;
+                if (0 < count($strings)) {
+                    $map[$filename] = $strings;
                 }
             }
-            if (0 < count($strings)) {
-                $map[$filename] = $strings;
-            }
+
         }
 
         return $map;
