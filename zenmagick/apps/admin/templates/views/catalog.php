306f8a04f54153c9cd99bafd5de1a054d011a3e1
DerManoMann <mano@zenmagick.org>
2010-07-06 13:49:58 +1200

    wip

--- zenmagick/apps/admin/templates/views/catalog.php
+++ zenmagick/apps/admin/templates/views/catalog.php
@@ -21,6 +21,57 @@
 
 <h1>Catalog</h1>
 
-<?php foreach (ZMPlugins::instance()->getPluginsForGroup('catalog') as $plugin) { ?>
+<?php foreach (ZMPlugins::instance()->getPluginsForGroup('catalog_manager') as $plugin) { ?>
   xx<?php echo $plugin ?>
 <?php } ?>
+
+<?php
+
+  // peel fkt parameter from url string
+  function get_fkt($url) {
+      $urlToken = parse_url($url); 
+      parse_str(str_replace('&amp;', '&', $urlToken['query']), $query); 
+      return $query['fkt'];
+  }
+
+  // active fkt
+  $selectedFkt = $request->getParameter('fkt', '');
+  $defaultUrlParams = '';
+
+  $tabInfo = array();
+  foreach (ZMAdminMenu::getItemsForParentId(ZMAdminMenu::MENU_CATALOG_MANAGER_TAB) as $item) {
+      $fkt = get_fkt($item->getURL());
+      $view = $toolbox->admin->getViewForFkt($request, $fkt, $selectedFkt != $fkt);
+      // export default url params as determined here
+      $view->setVar('defaultUrlParams', $defaultUrlParams);
+      $tabInfo[] = array('item' => $item, 'view' => $view, 'fkt' => $fkt);
+  }
+
+
+?>
+
+      <ul>
+        <?php foreach ($tabInfo as $info) { ?>
+          <li><a href="#<?php echo $info['fkt'] ?>"><span><?php echo $info['item']->getTitle() ?></span></a></li>
+        <?php } ?>
+      </ul>
+
+<?php $activeTab = 1; ?>
+      <?php foreach ($tabInfo as $index => $info) { 
+        if ($info['fkt'] == $selectedFkt) { $activeTab = ($index+1); }
+        ?>
+        <div id="<?php echo $info['fkt'] ?>" style="position:relative;">
+            <?php if (ZMMessages::instance()->hasMessages()) { ?>
+                <ul id="messages" style="margin-left:0">
+                <?php foreach (ZMMessages::instance()->getMessages() as $message) { ?>
+                    <li class="<?php echo $message->getType() ?>"><?php echo $message->getText() ?></li>
+                <?php } ?>
+                </ul>
+            <?php } ?>
+            <?php 
+            if (null != $info['view']) {
+                echo $info['view']->generate($request);
+            } ?>
+        </div>
+      <?php } ?>
+
