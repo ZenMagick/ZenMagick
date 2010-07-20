c630a4cc29aa45b3f1904b5f992a88f6e2a161f7
DerManoMann <mano@zenmagick.org>
2010-07-20 23:16:19 +1200

    use png, add favicon

--- zenmagick/apps/admin/templates/default_layout.php
+++ zenmagick/apps/admin/templates/default_layout.php
@@ -40,6 +40,7 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=<?php echo ZMSettings::get('zenmagick.mvc.html.charset') ?>">
     <title><?php _vzm('ZenMagick Admin') ?></title>
+    <link rel="shortcut icon" href="favicon.ico"> 
     <?php $resources->cssFile('style/zenmagick.css') ?>
     <?php $resources->cssFile('style/jquery-ui/jquery-ui-1.8.2.custom.css') ?>
     <?php $resources->cssFile('style/jquery.cluetip.css') ?>
--- zenmagick/apps/admin/templates/footer.php
+++ zenmagick/apps/admin/templates/footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
+  <a href="<?php echo $admin2->url('index') ?>"><img src="<?php echo $this->asUrl('images/logo-small.png', ZMView::RESOURCE) ?>" alt="ZenMagick" title="ZenMagick"></a>
   <p>
-    <?php echo sprintf(_zm('Copyright &copy; 2006-2010 %s.'), '<a target="_blank" href="http://www.zenmagick.org/">ZenMagick</a>') ?>
+    <?php echo sprintf(_zm('Copyright &copy; 2006-2010 %s.'), '<a target="_blank" href="http://www.zenmagick.org/">ZenMagick</a>') ?><br>
     <?php echo sprintf(_zm('Design by %s.'), '<a target="_blank" href="http://www.rubikintegration.com/">Rubik Integration</a>') ?>
   </p>
-  <p><?php _vzm('Zen Cart&trade; is a trademark of Zen Ventures, LLC.') ?></p>
 </div>
