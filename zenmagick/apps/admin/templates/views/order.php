4d2ea1d3122bae3238648ac04b32cf9dd5fea09e
DerManoMann <mano@zenmagick.org>
2010-08-02 12:44:37 +1200

    wip

--- zenmagick/apps/admin/templates/views/order.php
+++ zenmagick/apps/admin/templates/views/order.php
@@ -20,4 +20,63 @@
 ?>
 <?php zm_title($this, sprintf(_zm('Order: %s'), $order->getId())) ?>
 
-<?php var_dump($order) ?>
+<h3><?php _vzm("Item Details") ?></h3>
+<table class="grid">
+    <tbody>
+    <?php foreach ($order->getOrderItems() as $orderItem) { ?>
+        <tr>
+            <td class="qty"><?php echo $orderItem->getQty() ?> x </td>
+            <td class="itm">
+                <?php echo $html->encode($orderItem->getName()) ?>
+                <?php if ($orderItem->hasAttributes()) { ?>
+                    <br/>
+                    <?php foreach ($orderItem->getAttributes() as $attribute) { ?>
+                        <p><span class="attr"><?php echo $html->encode($attribute->getName()) ?>:</span>
+                        <?php $first = true; foreach ($attribute->getValues() as $attributeValue) { ?>
+                            <?php if (!$first) { ?>, <?php } ?>
+                            <span class="atval"><?php echo $html->encode($attributeValue->getName()) ?></span>
+                        <?php $first = false; } ?>
+                        </p>
+                    <?php } ?>
+                <?php } ?>
+            </td>
+            <td class="price"><?php echo $utils->formatMoney($orderItem->getCalculatedPrice()) ?></td>
+        </tr>
+    <?php } ?>
+    <?php foreach ($order->getOrderTotalLines() as $orderTotalLine) { ?>
+        <tr>
+            <td colspan="2" class="total"><?php echo $html->encode($orderTotalLine->getName()) ?></td>
+            <td class="price"><?php echo $orderTotalLine->getValue() ?></td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+
+<h3><?php _vzm("Order History") ?></h3>
+<table cellpadding="0" cellspacing="0">
+    <tbody>
+    <?php foreach ($order->getOrderStatusHistory($session->getLanguageId()) as $orderStatus) { ?>
+        <tr>
+            <td><?php echo $locale->shortDate($orderStatus->getDateAdded()) ?></td>
+            <td><?php echo $html->encode($orderStatus->getName()) ?></td>
+            <td class="cmt"><?php echo $html->encode($orderStatus->getComment()) ?></td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+
+<h3><?php _vzm("Address Details") ?></h3>
+<div id="addr">
+    <div id="daddr">
+        <h4><?php _vzm("Shipping Address") ?></h4>
+        <?php if (!$order->hasShippingAddress()) { ?>
+            <?php _vzm("N/A") ?>
+        <?php } else { ?>
+            <?php echo $macro->formatAddress($order->getShippingaddress()) ?>
+        <?php } ?>
+    </div>
+    <div id="baddr">
+        <h4><?php _vzm("Billing Address") ?></h4>
+        <?php echo $macro->formatAddress($order->getBillingAddress()) ?>
+    </div>
+</div>
