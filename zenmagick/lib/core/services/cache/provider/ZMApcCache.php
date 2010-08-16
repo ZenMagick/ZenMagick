ef85da4f2cdfdeea0143b0e055179a6c6061033f
DerManoMann <mano@zenmagick.org>
2010-08-16 15:39:15 +1200

    initial version

---
+++ zenmagick/lib/core/services/cache/provider/ZMApcCache.php
@@ -0,0 +1,111 @@
+<?php
+/*
+ * ZenMagick - Another PHP framework.
+ * Copyright (C) 2006-2010 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * APC caching.
+ *
+ * <p>Persistent caching using <code>APC</code>.</p>
+ *
+ * @author DerManoMann
+ * @package org.zenmagick.core.services.cache.provider
+ */
+class ZMApcCache extends ZMObject implements ZMCache {
+    private $group_;
+    private $lifetime_;
+    private $lastModified_;
+
+
+    /**
+     * Create new instance.
+     */
+    function __construct() {
+        parent::__construct();
+        $this->lifetime_ = 0;
+        $this->lastModified_ = time();
+        $this->compress_ = 0;
+    }
+
+    /**
+     * Destruct instance.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function init($group, $config) {
+        $this->group_ = $group;
+        $this->lifetime_ = $config['cacheTTL'];
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isAvailable() {
+        return function_exists('apc_cache_info');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function clear() {
+        $this->lastModified_ = time();
+        //TODO: clear by group
+		    return apc_clear_cache('user');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function lookup($id) {
+		    return apc_fetch($this->group_.'/'.$id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function remove($id) {
+        $this->lastModified_ = time();
+		    return apc_delete($this->group_.'/'.$id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function save($data, $id) {
+        $this->lastModified_ = time();
+        return apc_store($this->group_.'/'.$id, $data, $this->lifetime_);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function lastModified() {
+        return $this->lastModified_;
+    }
+
+}
