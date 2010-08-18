341bcea29ca87bf2f0a129723b84ececc8236ec5
DerManoMann <mano@zenmagick.org>
2010-08-18 16:22:19 +1200

    enable short tags and keep simple savant template cache

--- zenmagick/lib/mvc/view/filter/ZMShortTagFilter.php
+++ zenmagick/lib/mvc/view/filter/ZMShortTagFilter.php
@@ -35,7 +35,7 @@ class ZMShortTagFilter {
      * {@inheritDoc}
      */
     public function filter($text) {
-      return str_replace(array('<%=', '<?='), '<?php echo ', $text);
+        return str_replace(array('<%=', '<?='), '<?php echo ', $text);
     }
 
 }
