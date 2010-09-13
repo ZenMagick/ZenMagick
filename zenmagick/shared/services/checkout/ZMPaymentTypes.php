4d71f98f1d853442db608cb4ff866315bb80a100
DerManoMann <mano@zenmagick.org>
2010-09-13 14:08:51 +1200

    add own payments.php code

---
+++ zenmagick/shared/services/checkout/ZMPaymentTypes.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2010 zenmagick.org
+ *
+ * Portions Copyright (c) 2003 The zen-cart developers
+ * Portions Copyright (c) 2003 osCommerce
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+
+/**
+ * Payments.
+ *
+ * @author DerManoMann
+ * @package zenmagick.store.shared.services.checkout
+ */
+class ZMPaymentTypes extends ZMObject {
+
+    /**
+     * Create new instance.
+     */
+    function __construct() {
+    }
+
+    /**
+     * Destruct instance.
+     */
+    function __destruct() {
+        parent::__destruct();
+    }
+
+    /**
+     * Get instance.
+     */
+    public static function instance() {
+        return ZMObject::singleton('PaymentTypes');
+    }
+
+
+    /**
+     * Get all (available) payment types.
+     *
+     * @param boolean all Optional flag to return all installed; default is <code>false</code> to return enabled only.
+     * @return array List of <code>ZMPaymentType</code> instances.
+     */
+    public function getPaymentTypes($all=false) {
+        $paymentTypes = array();
+
+        if (defined('MODULE_PAYMENT_INSTALLED') && !ZMLangUtils::isEmpty(MODULE_PAYMENT_INSTALLED)) {
+            // get a list of modules and stuff
+            $moduleInfos = array();
+            foreach (explode(';', MODULE_PAYMENT_INSTALLED) as $filename) {
+                if (file_exists(DIR_FS_CATALOG . DIR_WS_MODULES . '/payment/' . $filename)) {
+                    $class = substr($filename, 0, strrpos($filename, '.'));
+                    $moduleInfos[] = array('class' => $class, 'filename' => $filename);
+                }
+            }
+
+            foreach ($moduleInfos as $info) {
+                $lang_file = DIR_WS_LANGUAGES.$_SESSION['language'].'/modules/payment/'.$info['filename'];
+                if (@file_exists($lang_file)) {
+                    include_once $lang_file;
+                }
+                include_once DIR_WS_MODULES.'payment/'.$info['filename'];
+                $module = new $info['class'];
+                if ($module->enabled) {
+                    $paymentTypes[$module->code] = $module;
+                }
+            }
+        }
+
+        return $paymentTypes;
+    }
+
+}
