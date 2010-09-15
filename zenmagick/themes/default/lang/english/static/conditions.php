d02114157f77ca990951fc7d7c0cc450508a9f8c
DerManoMann <mano@zenmagick.org>
2010-09-15 22:26:30 +1200

    fix bad char

--- zenmagick/themes/default/lang/english/static/conditions.php
+++ zenmagick/themes/default/lang/english/static/conditions.php
@@ -1,5 +1,5 @@
 <p>
-	<strong>Conditions of Use Sample TextÂ ...</strong></p>
+	<strong>Conditions of Use Sample Text ...</strong></p>
 <p>
 	This section of text is from the <code>Static Page Editor</code> located under ZenMagick in the Admin.</p>
 <p>
