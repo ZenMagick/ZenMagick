2ef57f1ff8447767ba1581693d5df130bb2f8321
DerManoMann <mano@zenmagick.org>
2011-01-06 11:36:32 +1300

    change input submit to button submit and adjust all relevant CSS; this allows to change the button to us an image rather than text (no localization yet)

--- themes/default/content/views/password_forgotten.php
+++ themes/default/content/views/password_forgotten.php
@@ -29,5 +29,5 @@
     <label for="email_address"><?php _vzm("E-Mail Address") ?></label>
     <input type="text" id="email_address" name="email_address" <?php echo $form->fieldLength(TABLE_CUSTOMERS, 'customers_email_address') ?> /> 
   </fieldset>
-  <div class="btn"><input type="submit" class="btn" value="<?php _vzm("Submit") ?>" /></div>
+  <div class="btn"><button type="submit" class="btn submit"><span><?php _vzm("Submit") ?></span></button></div>
 </form>
