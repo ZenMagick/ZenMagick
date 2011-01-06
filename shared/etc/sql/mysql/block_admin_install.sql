ea5cb6d816066ba774d2a70d8fd178c3fbb4f734
DerManoMann <mano@zenmagick.org>
2011-01-07 09:30:22 +1300

    add basic block widget properties

--- shared/etc/sql/mysql/block_admin_install.sql
+++ shared/etc/sql/mysql/block_admin_install.sql
@@ -16,6 +16,9 @@ CREATE TABLE blocks_to_groups (
   blocks_to_groups_id int(11) NOT NULL auto_increment,
   block_group_id int(11),
   block_name varchar(32) NOT NULL,
+  sort_order int(11) NOT NULL default 0,
+  template varchar(48),
+  format varchar(64),
   PRIMARY KEY (blocks_to_groups_id),
   FOREIGN KEY (block_group_id) REFERENCES block_groups (block_group_id) ON DELETE CASCADE
 ) TYPE=MyISAM;
