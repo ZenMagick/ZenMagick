f7f266e46047c2e94ac3903d323cdc30cd6cd753
DerManoMann <mano@zenmagick.org>
2011-01-07 16:24:26 +1300

    add initial drag&drop to assign blocks to block groups

--- apps/admin/templates/views/block_group_admin.php
+++ apps/admin/templates/views/block_group_admin.php
@@ -21,13 +21,61 @@
 <?php $admin2->title() ?>
 
 <div class="col3" style="float:left;width:32%;border:1px solid gray;padding:5px;margin:1px;">
-  <h2>Block Group Setup</h2>
+  <h2>Blocks</h2>
+  <ul id="blockList">
+    <?php foreach ($blocks as $def => $title) { ?>
+      <li><span class="clean <?php echo $def ?>"><?php echo $title ?></span></li>
+    <?php } ?>
+  </ul>
 </div>
 
 <div class="col3" style="float:left;width:32%;border:1px solid gray;padding:5px;margin:1px;">
-  <h2>Blocks</h2>
-  <?php foreach ($blocks as $def => $title) { ?>
-    <?php echo $title."<BR>" ?>
-  <?php } ?>
+  <h2>Block Group Setup: <?php echo $groupId ?></h2>
+  <ul id="groupBlockList" class="sortable" style="min-height:5em;min-width:25em;border:1px solid gray;">
+  </ul>
 </div>
 
+
+
+<script>
+$(function() {
+		$("#groupBlockList").sortable({
+			revert: true
+		});
+		$("#blockList li").draggable({
+			connectToSortable: "#groupBlockList",
+			helper: "clone",
+      snap: true,
+			revert: "invalid"
+		});
+		$("ul, li").disableSelection();
+	});
+$( ".draggable" ).draggable({
+  stop: function(event, ui) { 
+    if (this.parentNode && this.parentNode.id == 'blockList') {
+        //alert('yup');
+    }
+  }
+});
+$("#groupBlockList").droppable({
+  drop: function(event, ui) {
+   var span = $('span.clean', ui.draggable);
+   if (span) {
+      // TODO: first show spinner until ajax call is done
+      // TODO: move style in css file
+      var icons = '<div class="icons" style="float:right"><span class="ui-icon ui-icon-circle-close"></span></div>';
+      $(icons).insertAfter(span);
+      span.removeClass('clean');
+      // add close handler
+      $('span.ui-icon-circle-close', ui.draggable).click(function() {
+          $(this.parentNode.parentNode).remove();
+      });
+
+      // TODO: ajax call to create blocks_to_groups entry
+      // once complete, add edit icon
+      return span;
+    }
+  }
+});
+
+</script>
