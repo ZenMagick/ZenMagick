b390cb5280802dba4003a16e9d4ef136805e8fbe
DerManoMann <mano@zenmagick.org>
2011-02-08 12:41:00 +1300

    add new Settings class

---
+++ lib/base/zenmagick/base/Settings.php
@@ -0,0 +1,125 @@
+<?php
+/*
+ * ZenMagick - Another PHP framework.
+ * Copyright (C) 2006-2010 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+namespace zenmagick\base;
+
+/**
+ * A hierarchical settings repository.
+ *
+ * @author DerManoMann
+ * @package org.zenmagick.base
+ */
+class Settings {
+    protected $settings_ = array();
+
+
+    /**
+     * Lookup a given path.
+     *
+     * @param string path The path.
+     * @return mixed Either an <code>array</code> with value, element name (the last path element) and parent container in it, or <code>null</code>.
+     */
+    private function lookup($path) {
+        $current = &$this->settings_;
+        foreach (explode('.', $path) as $element) {
+            if (empty($element)) {
+                throw new \RuntimeException(sprintf('invalid path: %s', $path));
+            }
+            if (!array_key_exists($element, $current)) {
+                return null;
+            }
+            $last = &$current;
+            $current = &$current[$element];
+        }
+        return array($current, $element, $last);
+    }
+
+    /**
+     * Check if a given path exists.
+     *
+     * @param string path The path.
+     * @return boolean <code>true</code> if the path exists.
+     */
+    public function exists($path) {
+        return null !== $this->lookup($path);
+    }
+
+    /**
+     * Get the value for the given path.
+     *
+     * @param string path The path.
+     * @param mixed default Optional default value to be returned if the path doesn't exist; default is <code>null</code>.
+     * @return mixed The value, the default value or <code>null</code>.
+     */
+    public function get($path, $default=null) {
+        if (null !== ($lookup = $this->lookup($path))) {
+            return $lookup[0];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Set the value for the given path.
+     *
+     * @param string path The path.
+     * @param mixed value The (new) value.
+     * @return mixed The old value or <code>null</code>.
+     */
+    public function set($path, $value) {
+        if (null !== ($lookup = $this->lookup($path))) {
+            $lookup[2][$lookup[1]] = $value;
+            return $lookup[0];
+        }
+
+        // create path
+        $current = &$this->settings_;
+        foreach (explode('.', $path) as $element) {
+            if (empty($element)) {
+                throw new \RuntimeException(sprintf('invalid path: %s', $path));
+            }
+            if (!array_key_exists($element, $current)) {
+                $current[$element] = array();
+            }
+            $last = &$current;
+            $current = &$current[$element];
+        }
+        $last[$element] = $value;
+        return null;
+    }
+
+    /**
+     * Append to an existing value.
+     *
+     * @param string path The path to append to.
+     * @param mixed value The value to append.
+     * @param string delim Optional delimiter to be used if the value exists and is not empty; default is <em>','</em>.
+     * @return mixed The old value or <code>null</code>.
+     */
+    public function append($path, $value, $delim=',') {
+        if (null !== ($lookup = $this->lookup($path))) {
+            $lookup[2][$lookup[1]] .= $delim.$value;
+            return $lookup[0];
+        }
+        return $this->set($path, $value);
+    }
+
+}
---
+++ plugins/unitTests/tests/base/TestSettings.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2010 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+
+use zenmagick\base\Settings;
+
+/**
+ * Test settings
+ *
+ * @package org.zenmagick.plugins.unitTests.tests
+ * @author DerManoMann
+ */
+class TestSettings extends ZMTestCase {
+
+    /**
+     * Test append.
+     */
+    public function testAppendNew() {
+        $settings = new Settings();
+        $key = 'a.b.n@@';
+        $value = 'doh';
+        $oldValue = $settings->append($key, $value);
+        $this->assertNull($oldValue);
+        $this->assertEqual($value, $settings->get($key));
+
+        // and with delim
+        $key = 'b.c.n@@@';
+        $delim = '!';
+        $old = null;
+        $oldValue = $settings->append($key, $value, $delim);
+        $this->assertEqual($old, $oldValue);
+        $this->assertEqual($value, $settings->get($key));
+    }
+
+    /**
+     * Test append old.
+     */
+    public function testAppendOld() {
+        $settings = new Settings();
+        $key = 'a.b.o@@';
+        $old = 'yo';
+        $value = 'doh';
+        $settings->set($key, $old);
+        $this->assertEqual($old, $settings->get($key));
+        $oldValue = $settings->append($key, $value);
+        $this->assertEqual($old, $oldValue);
+        // ',' is the default delimiter
+        $this->assertEqual($old.','.$value, $settings->get($key));
+
+        // and with delim
+        $key = 'o@@@';
+        $delim = '!';
+        ZMSettings::set($key, $old);
+        $oldValue = ZMSettings::append($key, $value, $delim);
+        $this->assertEqual($old, $oldValue);
+        $this->assertEqual($old.$delim.$value, ZMSettings::get($key));
+    }
+
+    /**
+     * Test append multiple.
+     */
+    public function testAppendMultiple() {
+        $settings = new Settings();
+        $key = 'a.b.c.o@@@@';
+        $old = null;
+
+        $oldValue = $settings->append($key, 'yo', ',');
+        $this->assertEqual(null, $oldValue);
+        $this->assertEqual('yo', $settings->get($key));
+
+        $oldValue = $settings->append($key, 'yo', ',');
+        $this->assertEqual('yo', $oldValue);
+        $this->assertEqual('yo,yo', $settings->get($key));
+
+        $oldValue = $settings->append($key, 'yo', ',');
+        $this->assertEqual('yo,yo', $oldValue);
+        $this->assertEqual('yo,yo,yo', $settings->get($key));
+    }
+
+    /**
+     * Test new value.
+     */
+    public function testNewValue() {
+        $settings = new Settings();
+        $key = 'a.b.n@@';
+        $pkey = 'a.b';
+        $value = 'doh';
+
+        $this->assertFalse($settings->exists($key));
+        $oldValue = $settings->set($key, $value);
+        $this->assertNull($oldValue);
+        $this->assertEqual($value, $settings->get($key));
+        $this->assertEqual(array('n@@' => $value), $settings->get($pkey));
+    }
+
+    /**
+     * Test update value.
+     */
+    public function testUpdateValue() {
+        $settings = new Settings();
+        $key = 'a.b.n@@';
+        $value = 'doh';
+        $newValue = 'deng';
+
+        $settings->set($key, $value);
+        $this->assertEqual($value, $settings->get($key));
+        $oldValue = $settings->set($key, $newValue);
+        $this->assertEqual($value, $oldValue);
+        $this->assertEqual($newValue, $settings->get($key));
+    }
+
+}
