3456efa6b65ec81b719db6a9353e37a19562d2f0
DerManoMann <mano@zenmagick.org>
2011-03-22 22:19:23 +1300

    do not extend ZMSession!

--- apps/store/lib/mvc/Session.php
+++ apps/store/lib/mvc/Session.php
@@ -34,7 +34,7 @@ use zenmagick\base\events\Event;
  * @author DerManoMann
  * @package zenmagick.store.sf.mvc
  */
-class Session extends ZMSession {
+class Session extends ZMObject {
 
     /**
      * Create new instance.
