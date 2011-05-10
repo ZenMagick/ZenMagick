84fbb55ef43b37df86180bf9172d8432bdded511
DerManoMann <mano@zenmagick.org>
2011-05-10 12:28:55 +1200

    resolve Beans namespace

--- apps/admin/templates/views/index.php
+++ apps/admin/templates/views/index.php
@@ -17,6 +17,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
+use zenmagick\base\Beans;
 ?>
 <?php $resources->cssFile('style/dashboard.css'); ?>
 <?php $resources->jsFile('js/dashboard.js'); ?>
