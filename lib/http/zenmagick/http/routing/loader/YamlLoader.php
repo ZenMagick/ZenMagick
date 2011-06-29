e7040a721d08cf3f993463760e4aa40d3abcb011
DerManoMann <mano@zenmagick.org>
2011-06-29 15:06:59 +1200

    refs #211; add custom loader and add component to class loader

---
+++ lib/http/zenmagick/http/routing/loader/YamlLoader.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * ZenMagick - Another PHP framework.
+ * Copyright (C) 2006-2011 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+namespace zenmagick\http\routing\loader;
+
+use Symfony\Component\Config\FileLocatorInterface;
+
+use zenmagick\base\config\EchoFileLocator;
+use zenmagick\base\Toolbox;
+
+/**
+ * Yaml loader for routing mappings.
+ *
+ * <p>Assume <code>$file</code> to be the actual content.</p>
+ *
+ * @author DerManoMann
+ * @package zenmagick.http.routing.loader
+ */
+class YamlLoader extends \Symfony\Component\Routing\Loader\YamlFileLoader {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct(FileLocatorInterface $locator=null) {
+        parent::__construct(null != $locator ? $locator : new EchoFileLocator());
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load($file, $type=null) {
+        return parent::load($file, $type);
+    }
+
+}
