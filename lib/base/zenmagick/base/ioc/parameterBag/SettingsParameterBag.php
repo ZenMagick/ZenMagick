7112893db86e6894110b3f154a90283882eaed54
DerManoMann <mano@zenmagick.org>
2011-06-30 16:00:20 +1200

    follow symfony changes

--- lib/base/zenmagick/base/ioc/parameterBag/SettingsParameterBag.php
+++ lib/base/zenmagick/base/ioc/parameterBag/SettingsParameterBag.php
@@ -24,7 +24,6 @@ namespace zenmagick\base\ioc\parameterBag;
 use zenmagick\base\Runtime;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
-use Symfony\Component\DependencyInjection\Exception\NonExistentParameterException;
 
 /**
  * Parameter bag that can also resolve against settings.
@@ -36,52 +35,18 @@ class SettingsParameterBag extends ParameterBag {
 
     /**
      * {@inheritDoc}
-     *
-     * <p>Will also resolve values against application settings.</p>
      */
-    public function resolveValue($value) {
-        if (is_array($value)) {
-            $args = array();
-            foreach ($value as $k => $v) {
-                $args[$this->resolveValue($k)] = $this->resolveValue($v);
-            }
-
-            return $args;
-        }
-
-        if (!is_string($value)) {
-            return $value;
-        }
-
+    public function resolveString($value, array $resolving=array()) {
+        // we do this to deal with non string values (Boolean, integer, ...)
+        // as the preg_replace_callback throw an exception when trying
+        // a non-string in a parameter value
         if (preg_match('/^%([^%]+)%$/', $value, $match)) {
-            // we do this to deal with non string values (Boolean, integer, ...)
-            // the preg_replace_callback converts them to strings
-            if ($this->has(strtolower($match[1]))) {
-                return $this->get(strtolower($match[1]));
-            } else if (Runtime::getSettings()->exists($match[1])) {
+            $key = strtolower($match[1]);
+            if (Runtime::getSettings()->exists($match[1])) {
                 return Runtime::getSettings()->get($match[1]);
             }
-            throw new NonExistentParameterException($name);
         }
-
-        return str_replace('%%', '%', preg_replace_callback(array('/(?<!%)%([^%]+)%/'), array($this, 'resolveValueCallback'), $value));
-    }
-
-    /**
-     * Value callback
-     *
-     * @see resolveValue
-     *
-     * @param array $match
-     * @return string
-     */
-    private function resolveValueCallback($match) {
-        if ($this->has(strtolower($match[1]))) {
-            return $this->get(strtolower($match[1]));
-        } else if (Runtime::getSettings()->exists($match[1])) {
-            return Runtime::getSettings()->get($match[1]);
-        }
-        throw new NonExistentParameterException($name);
+        return parent::resolveString($value, $resolving);
     }
 
 }
