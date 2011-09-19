44ad35fbc896bec9410f9cda7530314fb5dce2de
DerManoMann <mano@zenmagick.org>
2011-09-20 10:32:14 +1200

    fix front controller setting key

--- lib/http/zenmagick/http/request/rewriter/DefaultUrlRewriter.php
+++ lib/http/zenmagick/http/request/rewriter/DefaultUrlRewriter.php
@@ -45,7 +45,7 @@ class DefaultUrlRewriter implements UrlRewriter {
      * Create new instance.
      */
     public function __construct() {
-        $this->index_ = Runtime::getSettings()->get('zenmagick.http.request.index', 'index.php');
+        $this->index_ = Runtime::getSettings()->get('zenmagick.http.request.handler', 'index.php');
         // resolve once only
         $this->requestIdKey_ = Runtime::getSettings()->get('zenmagick.http.request.idName', \ZMRequest::DEFAULT_REQUEST_ID);
         $type = Runtime::getSettings()->get('zenmagick.http.request.urlType', 'default');
