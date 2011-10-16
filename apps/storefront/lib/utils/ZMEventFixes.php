fab1c3b8c6157f5253c76d493a97440a5f7ad1fa
John Robeson <johnny@localmomentum.net>
2011-10-16 04:10:10 -0700

    typofix: s/ogoff/logoff in checkAuthorization

--- apps/storefront/lib/utils/ZMEventFixes.php
+++ apps/storefront/lib/utils/ZMEventFixes.php
@@ -356,7 +356,7 @@ class ZMEventFixes extends ZMObject {
     protected function checkAuthorization($request) {
         $account = $request->getAccount();
         if (null != $account && ZMAccounts::AUTHORIZATION_PENDING == $account->getAuthorization()) {
-            if (!in_array($request->getRequestId(), array('customers_authorization', 'login', 'ogoff', 'contact_us', 'privacy'))) {
+            if (!in_array($request->getRequestId(), array('customers_authorization', 'login', 'logoff', 'contact_us', 'privacy'))) {
                 $request->redirect($request->url('customers_authorization'));
             }
         }
