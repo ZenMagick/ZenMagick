1a8af07db6e61d6ae2d6914d695d0fa15a14b9dc
DerManoMann <mano@zenmagick.org>
2011-11-19 19:18:26 +1300

    refs #313;

--- apps/storefront/lib/mvc/controller/ZMAccountHistoryInfoController.php
+++ apps/storefront/lib/mvc/controller/ZMAccountHistoryInfoController.php
@@ -33,27 +33,12 @@
 class ZMAccountHistoryInfoController extends ZMController {
 
     /**
-     * Create new instance.
-     */
-    function __construct() {
-        parent::__construct();
-    }
-
-    /**
-     * Destruct instance.
-     */
-    function __destruct() {
-        parent::__destruct();
-    }
-
-
-    /**
      * Process a HTTP GET request.
      *
      * @return ZMView A <code>ZMView</code> that handles presentation or <code>null</code>
      * if the controller generates the contents itself.
      */
-    function processGet($request) {
+    public function processGet($request) {
         $request->getToolbox()->crumbtrail->addCrumb("Account", $request->url('account', '', true));
         $request->getToolbox()->crumbtrail->addCrumb($request->getToolbox()->utils->getTitle(), $request->url('account_history', '', true));
         $request->getToolbox()->crumbtrail->addCrumb("Order # ".$request->getOrderId());
