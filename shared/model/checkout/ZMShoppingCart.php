11bc3278db8bfdf400ed0e9d90ab08db2908617b
John Robeson <johnny@localmomentum.net>
2011-12-03 03:52:31 -0800

    ZMShoppingCart: instantiate shoppingCart if we do not already have it

--- shared/model/checkout/ZMShoppingCart.php
+++ shared/model/checkout/ZMShoppingCart.php
@@ -50,8 +50,10 @@ class ZMShoppingCart extends ZMObject {
      */
     function __construct() {
         parent::__construct();
-        $this->cart_ = $_SESSION['cart'];
         $this->session = Runtime::getContainer()->get('session');
+        $cart = $this->session->getValue('cart');
+        // TODO: quick fix using 'new' until we drop zencart's shopping cart class altogether
+        $this->cart_ = (null != $cart) ? $cart : new shoppingCart;
         // TODO: remove
         $comments = $this->session->getValue('comments');
         $this->setComments(null !== $comments ? $comments : '');
