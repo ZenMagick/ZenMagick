92004b164f79db885cb0bbdb640c285831b27a01
DerManoMann <mano@zenmagick.org>
2011-12-09 13:53:28 +1300

    fixed regression: namespace for class loader changed

--- lib/mvc/validation/ZMValidator.php
+++ lib/mvc/validation/ZMValidator.php
@@ -21,7 +21,7 @@
 <?php
 
 use zenmagick\base\Beans;
-use zenmagick\base\ClassLoader;
+use zenmagick\base\classloader\ClassLoader;
 use zenmagick\base\Runtime;
 use zenmagick\base\ZMException;
 use zenmagick\base\ZMObject;
