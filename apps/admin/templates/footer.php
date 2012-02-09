44eeb0777c2a6a66a85d87e02fc6fc95ca546106
DerManoMann <mano@zenmagick.org>
2012-02-09 17:10:09 +1300

    bump (c) year

--- apps/admin/templates/footer.php
+++ apps/admin/templates/footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
   <a href="<?php echo $admin2->url('index') ?>"><img src="<?php echo $this->asUrl('images/logo-small.png', $view::RESOURCE) ?>" alt="ZenMagick" title="ZenMagick"></a>
   <p>
-    <?php echo sprintf(_zm('Copyright &copy; 2006-2010 %s'), '<a target="_blank" href="http://www.zenmagick.org/">ZenMagick</a>') ?><br>
+    <?php echo sprintf(_zm('Copyright &copy; 2006-2012 %s'), '<a target="_blank" href="http://www.zenmagick.org/">ZenMagick</a>') ?><br>
     <?php echo sprintf(_zm('Design by %s'), '<a target="_blank" href="http://www.rubikintegration.com/">Rubik Integration</a>') ?>
   </p>
 </div>
