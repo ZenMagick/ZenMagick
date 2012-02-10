783432ecfa207ac38ad690bf67a02cc0c5c89b07
DerManoMann <mano@zenmagick.org>
2012-02-10 14:52:53 +1300

    fixes #385

--- apps/admin/lib/dashboard/widgets/StoreStatusDashboardWidget.php
+++ apps/admin/lib/dashboard/widgets/StoreStatusDashboardWidget.php
@@ -22,6 +22,7 @@
 namespace zenmagick\apps\store\admin\dashboard\widgets;
 
 use DateTime;
+use zenmagick\base\Beans;
 use zenmagick\base\Runtime;
 use zenmagick\apps\store\admin\dashboard\DashboardWidget;
 
@@ -31,9 +32,6 @@ use zenmagick\apps\store\admin\dashboard\DashboardWidget;
  * @author DerManoMann <mano@zenmagick.org>
  */
 class StoreStatusDashboardWidget extends DashboardWidget {
-    const ACTIVITY_LOG_RECORD_THRESHOLD = 50000;
-    const ACTIVITY_LOG_DATE_THRESHOLD = 60;
-    const NEW_SIGNUP_GV_EXPIRY_THRESHOLD = 21;
 
     /**
      * Create new user.
@@ -48,90 +46,13 @@ class StoreStatusDashboardWidget extends DashboardWidget {
      */
     public function getContents($request) {
         $contents = _zm('Nothing to report.');
+
+        // query all status checker
         $messages = array();
-
-        $languageId = Runtime::getSettings()->get('storeDefaultLanguageId');
-        $configService = $this->container->get('configService');
-
-        // build status list
-        $installDir = realpath(dirname(Runtime::getInstallationPath()).'/zc_install');
-        if (is_dir($installDir)) { $messages[] = array(self::STATUS_NOTICE, sprintf(_zm('Installation directory exists at: %s. Please remove this directory for security reasons.'), $installDir)); }
-
-        $configure = realpath(dirname(Runtime::getInstallationPath()).'/includes/configure.php');
-        if (file_exists($configure) && is_writeable($configure)) {
-            $messages[] = array(self::STATUS_WARN, sprintf(_zm('Store configuration file: %s should be read-only.'), $configure));
+        foreach ($this->container->findTaggedServiceIds('apps.store.admin.dashboard.widget.statusCheck') as $id => $args) {
+            $statusCheck = $this->container->get($id);
+            $messages = array_merge($messages, $statusCheck->getStatusMessages());
         }
-        $configure = realpath(dirname(Runtime::getInstallationPath()).'/'.Runtime::getSettings()->get('apps.store.zencart.admindir').'/includes/configure.php');
-        if (file_exists($configure) && is_writeable($configure)) {
-            $messages[] = array(self::STATUS_WARN, sprintf(_zm('Admin configuration file: %s should be read-only.'), $configure));
-        }
-
-        if (null != ($value = $configService->getConfigValue('MODULE_PAYMENT_INSTALLED'))) {
-            $value = $value->getValue();
-            if (empty($value)) {
-                $messages[] = array(self::STATUS_NOTICE, _zm('You have no payment modules activated. Please go to Configuration->Modules->Payment to configure.'));
-            }
-        }
-        if (null != ($value = $configService->getConfigValue('MODULE_SHIPPING_INSTALLED'))) {
-            $value = $value->getValue();
-            if (empty($value)) {
-                $messages[] = array(self::STATUS_NOTICE, _zm('You have no shipping modules activated. Please go to Configuration->Modules->Shipping to configure.'));
-            }
-        }
-
-        $result = \ZMRuntime::getDatabase()->querySingle('SELECT COUNT(log_id) AS counter from '. DB_PREFIX . 'admin_activity_log', array(), 'admin_activity_log');
-        if (0 < $result['counter']) {
-            $reset = null;
-            if (self::ACTIVITY_LOG_RECORD_THRESHOLD < $result['counter']) {
-                $reset = sprintf(_zm('The Admin Activity Log table has over %s records and should be cleaned ... '), self::ACTIVITY_LOG_RECORD_THRESHOLD);
-            } else {
-                $sql = 'SELECT MIN(access_date) AS access_date FROM ' . DB_PREFIX . 'admin_activity_log WHERE access_date < DATE_SUB(CURDATE(), INTERVAL '.self::ACTIVITY_LOG_DATE_THRESHOLD.' DAY)';
-                $result = \ZMRuntime::getDatabase()->querySingle($sql);
-                if ($result && null != $result['access_date']) {
-                    $reset = sprintf(_zm('The Admin Activity Log table has records more than %s days old and should be cleaned ... '), self::ACTIVITY_LOG_DATE_THRESHOLD);
-                }
-            }
-            if ($reset) {
-                $messages[] = array(self::STATUS_NOTICE, $reset);
-            }
-        }
-
-        if (null != ($value = $configService->getConfigValue('NEW_SIGNUP_DISCOUNT_COUPON'))) {
-            $value = $value->getValue();
-            if (!empty($value)) {
-              if (null != ($coupon = $this->container->get('couponService')->getCouponForId($value, $languageId))) {
-                  $expiryDate = $coupon->getExpiryDate();
-                  $diff = $expiryDate->diff(new DateTime(), true);
-                  $interval = (int)$diff->format('%r%a');
-                  if ($interval > 0 && $interval < self::NEW_SIGNUP_GV_EXPIRY_THRESHOLD) {
-                      $messages[] = array(self::STATUS_NOTICE, sprintf(_zm('Welcome Email Discount Coupon expires in %s days.'), $interval));
-                  }
-              }
-            }
-        }
-
-        if (null != ($results = \ZMRuntime::getDatabase()->fetchAll('SELECT * FROM ' . TABLE_COUPON_GV_QUEUE . ' where release_flag = "N"')) && 0 < count($results)) {
-            $url = '<a href="'.$request->url('zc_admin', 'zpid=gv_queue').'">'._zm('gift queue').'</a>';
-            $messages[] = array(self::STATUS_NOTICE, sprintf(_zm('%s item(s) in %s waiting for approval.'), count($results), $url));
-        }
-
-        // payment module test modes
-        if (defined('MODULE_PAYMENT_PAYPAL_IPN_DEBUG') && defined('MODULE_PAYMENT_PAYPAL_TESTING') && (MODULE_PAYMENT_PAYPAL_IPN_DEBUG == 'true' || MODULE_PAYMENT_PAYPAL_TESTING == 'Test')) {
-            $messages[] = array(self::STATUS_NOTICE, _zm('PayPal is in testing mode.'));
-        }
-        if ((defined('MODULE_PAYMENT_AUTHORIZENET_AIM_STATUS') && MODULE_PAYMENT_AUTHORIZENET_AIM_STATUS == 'True'
-          && defined('MODULE_PAYMENT_AUTHORIZENET_AIM_TESTMODE') && MODULE_PAYMENT_AUTHORIZENET_AIM_TESTMODE == 'Test')
-          || (defined('MODULE_PAYMENT_AUTHORIZENET_STATUS') && MODULE_PAYMENT_AUTHORIZENET_STATUS == 'True'
-              && defined('MODULE_PAYMENT_AUTHORIZENET_TESTMODE') && MODULE_PAYMENT_AUTHORIZENET_TESTMODE =='Test' ) ) {
-            $messages[] = array(self::STATUS_NOTICE, _zm('AuthorizeNet is in testing mode.'));
-        }
-        if (defined('MODULE_SHIPPING_USPS_SERVER') && MODULE_SHIPPING_USPS_SERVER == 'test' ) {
-            $messages[] = array(self::STATUS_NOTICE, _zm('USPS is in testing mode.'));
-        }
-        if (!defined('DEFAULT_CURRENCY')) { $messages[] = array(self::STATUS_WARN, _zm('Please set a default currency.')); }
-        if (!defined('DEFAULT_LANGUAGE') || DEFAULT_LANGUAGE=='') { $messages[] = array(self::STATUS_NOTICE, _zm('Please set a default language.')); }
-        if (DOWN_FOR_MAINTENANCE == 'true') { $messages[] = array(self::STATUS_WARN, _zm('Your site is currently down for Maintenance.')); }
-
 
         // figure out the status and generate contents
         $status = self::STATUS_DEFAULT;
---
+++ apps/admin/lib/dashboard/widgets/status/InstallationStatusCheck.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2011 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<?php
+namespace zenmagick\apps\store\admin\dashboard\widgets\status;
+
+use DateTime;
+use zenmagick\base\Runtime;
+use zenmagick\apps\store\admin\dashboard\widgets\StatusCheck;
+use zenmagick\apps\store\admin\dashboard\DashboardWidget;
+
+/**
+ * Installation status check.
+ *
+ * @author DerManoMann <mano@zenmagick.org>
+ */
+class InstallationStatusCheck implements StatusCheck {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getStatusMessages() {
+        $messages = array();
+
+        $installDir = realpath(dirname(Runtime::getInstallationPath()).'/zc_install');
+        if (is_dir($installDir)) { $messages[] = array(DashboardWidget::STATUS_NOTICE, sprintf(_zm('Installation directory exists at: %s. Please remove this directory for security reasons.'), $installDir)); }
+
+        $configure = realpath(dirname(Runtime::getInstallationPath()).'/includes/configure.php');
+        if (file_exists($configure) && is_writeable($configure)) {
+            $messages[] = array(DashboardWidget::STATUS_WARN, sprintf(_zm('Store configuration file: %s should be read-only.'), $configure));
+        }
+        $configure = realpath(dirname(Runtime::getInstallationPath()).'/'.Runtime::getSettings()->get('apps.store.zencart.admindir').'/includes/configure.php');
+        if (file_exists($configure) && is_writeable($configure)) {
+            $messages[] = array(DashboardWidget::STATUS_WARN, sprintf(_zm('Admin configuration file: %s should be read-only.'), $configure));
+        }
+
+        return $messages;
+    }
+
+}
