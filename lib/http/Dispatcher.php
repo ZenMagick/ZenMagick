0a5d22cdb636585f7680a4c2992c957b06d750f6
DerManoMann <mano@zenmagick.org>
2012-02-21 15:31:40 +1300

    refs #319: - obsolete (and remove) ZMRoutingController - wrap controller in Executor and let the dispatcher do the custom parameter mapping

--- lib/http/Dispatcher.php
+++ lib/http/Dispatcher.php
@@ -21,19 +21,23 @@
 <?php
 namespace zenmagick\http;
 
+use ReflectionClass;
+use ZMRequest;
 use zenmagick\base\Beans;
 use zenmagick\base\Runtime;
 use zenmagick\base\ZMException;
 use zenmagick\base\ZMObject;
 use zenmagick\base\events\Event;
 use zenmagick\base\logging\Logging;
+use zenmagick\base\utils\Executor;
+use zenmagick\base\utils\ParameterMapper;
 
 /**
  * ZenMagick MVC request dispatcher.
  *
  * @author DerManoMann <mano@zenmagick.org>
  */
-class Dispatcher extends ZMObject {
+class Dispatcher extends ZMObject implements ParameterMapper {
 
     /**
      * Dispatch a request.
@@ -43,26 +47,28 @@ class Dispatcher extends ZMObject {
     public function dispatch($request) {
         ob_start();
 
-        // load saved messages
-        Runtime::getContainer()->get('messageService')->loadMessages($request->getSession());
+        $messageService = $this->container->get('messageService');
+        $eventDispatcher = $this->container->get('eventDispatcher');
 
-        Runtime::getEventDispatcher()->dispatch('dispatch_start', new Event(null, array('request' => $request)));
-        $view = self::handleRequest($request);
-        Runtime::getEventDispatcher()->dispatch('dispatch_done', new Event(null, array('request' => $request)));
+        // load saved messages
+        $messageService->loadMessages($request->getSession());
+
+        $eventDispatcher->dispatch('dispatch_start', new Event(null, array('request' => $request)));
+        $view = $this->handleRequest($request);
+        $eventDispatcher->dispatch('dispatch_done', new Event(null, array('request' => $request)));
 
         // allow plugins and event subscribers to filter/modify the final contents; corresponds with ob_start() in init.php
         $event = new Event(null, array('request' => $request, 'view' => $view, 'content' => ob_get_clean()));
-        Runtime::getEventDispatcher()->dispatch('finalise_content', $event);
+        $eventDispatcher->dispatch('finalise_content', $event);
 
         echo $event->get('content');
 
         // if we get to here all messages have been displayed
-        $messageService = Runtime::getContainer()->get('messageService');
         $messageService->clear();
         $messageService->saveMessages($request->getSession());
 
         // all done
-        Runtime::getEventDispatcher()->dispatch('all_done', new Event(null, array('request' => $request, 'view' => $view, 'content' => $event->get('content'))));
+        $eventDispatcher->dispatch('all_done', new Event(null, array('request' => $request, 'view' => $view, 'content' => $event->get('content'))));
         $request->closeSession();
     }
 
@@ -73,14 +79,12 @@ class Dispatcher extends ZMObject {
      * @return View The view or <code>null</code>.
      */
     public function handleRequest($request) {
-        $controller = $this->getControllerForRequest($request);
-        $view = null;
+        $eventDispatcher = $this->container->get('eventDispatcher');
 
         try {
-            // execute controller
-            $view = $controller->process($request);
+            $view = $this->getControllerExecutor($request)->execute();
         } catch (Exception $e) {
-            Runtime::getLogging()->dump($e, sprintf('controller::process failed: %s', $e->getMessage()), Logging::ERROR);
+            $this->container->get('loggingService')->dump($e, sprintf('controller::process failed: %s', $e->getMessage()), Logging::ERROR);
             $controller = $this->container->get('defaultController');
             $view = $controller->findView('error', array('exception' => $e));
             $request->setController($controller);
@@ -97,51 +101,92 @@ class Dispatcher extends ZMObject {
                     }
                     header($s);
                 }
-                Runtime::getEventDispatcher()->dispatch('view_start', new Event(null, array('request' => $request, 'view' => $view)));
+                $eventDispatcher->dispatch('view_start', new Event(null, array('request' => $request, 'view' => $view)));
                 // generate response
                 echo $view->generate($request);
-                Runtime::getEventDispatcher()->dispatch('view_done', new Event(null, array('request' => $request, 'view' => $view)));
+                $eventDispatcher->dispatch('view_done', new Event(null, array('request' => $request, 'view' => $view)));
             } catch (ZMException $e) {
-                Runtime::getLogging()->dump($e, sprintf('view::generate failed: %s', $e), Logging::ERROR);
+                $this->container->get('loggingService')->dump($e, sprintf('view::generate failed: %s', $e), Logging::ERROR);
             } catch (Exception $e) {
-                Runtime::getLogging()->dump($e, sprintf('view::generate failed: %s', $e->getMessage()), Logging::ERROR);
+                $this->container->get('loggingService')->dump($e, sprintf('view::generate failed: %s', $e->getMessage()), Logging::ERROR);
                 //TODO: what to do?
             }
         } else {
-            Runtime::getLogging()->debug('null view, skipping $view->generate()');
+            $this->container->get('loggingService')->debug('null view, skipping $view->generate()');
         }
 
         return $view;
     }
 
     /**
-     * Get a controller instance for the given request.
-     *
-     * @return ZMController A controller instance or <code>null</code>.
+     * {@inheritDoc}
      */
-    public function getControllerForRequest($request) {
-        $controller = null;
+    public function mapParameter($callback, array $parameter) {
+        if (!is_array($callback)) {
+            return $parameter;
+        }
+
+        $rc = new ReflectionClass($callback[0]);
+        $method = $rc->getMethod($callback[1]);
+        // we always put that in
+        $request = $parameter['request'];
+        $mapped = array();
+        foreach ($method->getParameters() as $rp) {
+            $value = null;
+            if (array_key_exists($rp->name, $parameter)) {
+                $value = $parameter[$rp->name];
+            } else {
+                // check for known types
+                $hintClass = $rp->getClass();
+                if ($hintClass) {
+                    // check for special classes/interfaces
+                    // TODO: this is expected to grow a bit, so make the code look nicer
+                    if ('ZMFormData' == $hintClass->name || $hintClass->isSubclassOf('ZMFormData')) {
+                        $value = Beans::getBean($hintClass->name);
+                        $value->populate($request);
+                    } else if ('ZMRequest' == $hintClass->name || $hintClass->isSubclassOf('ZMRequest')) {
+                        $value = $request;
+                    } else if ('zenmagick\http\messages\Messages' == $hintClass->name || $hintClass->isSubclassOf('zenmagick\http\messages\Messages')) {
+                        $value = $this->container->get('messageService');
+                    } else {
+                        // last choice - assume a model class that does not extend/implement FormData
+                        $value = Beans::getBean($hintClass->name);
+                        Beans::setAll($value, $request->getParameterMap(), null);
+                    }
+                }
+            }
+            $mapped[] = $value;
+        }
+        return $mapped;
+    }
+
+    /**
+     * Get an executor for an controller to handle the given request.
+     *
+     * @param ZMRequest request The request.
+     * @return Executor The executor.
+     */
+    public function getControllerExecutor(ZMRequest $request) {
         if ($routerMatch = $this->container->get('routeResolver')->getRouterMatch($request->getUri())) {
             // class:method ?
             $token = explode(':', $routerMatch['_controller']);
             if (1 == count($token)) {
-                // expect a ZMController instance with traditional processing
+                // traditional controller
                 $controller = Beans::getBean($routerMatch['_controller']);
+                return new Executor(array($controller, 'process'), array($request));
             } else {
-                // wrap to allow custom method with variable parameters
+                // wrap to allow custom method with variable parameter
                 // TODO: remove once all controller use type hints for $request
                 if (!array_key_exists('request', $routerMatch)) {
                     // allow $request as mappable parameter too
                     $routerMatch['request'] = $request;
                 }
-                $controller_ = new \ZMRoutingController(Beans::getBean($token[0]), $token[1], $routerMatch);
-                $controller_->setContainer($this->container);
+                return new Executor(array(Beans::getBean($token[0]), $token[1]), $routerMatch, $this);
             }
         } else {
             $controller = \ZMUrlManager::instance()->findController($request->getRequestId());
+            return new Executor(array($controller, 'process'), array($request));
         }
-
-        return $controller;
     }
 
 }
