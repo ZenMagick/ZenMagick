0d1f8f81281bb692c35e9c46f4f8e1874efd34ed
John Robeson <johnny@localmomentum.net>
2012-02-28 02:03:20 -0800

    ZenCartBundle: add classes to the class loader. add {get|set}BaseDirectories

--- shared/store/bundles/ZenCartBundle/ZenCartClassLoader.php
+++ shared/store/bundles/ZenCartBundle/ZenCartClassLoader.php
@@ -41,16 +41,50 @@ class ZenCartClassLoader extends ClassLoader {
         parent::__construct($namespaces);
         $this->baseDirectories = array(dirname(__FILE__).'/bridge/includes/classes', dirname(Runtime::getInstallationPath()).'/includes/classes');
         $this->classFileMap = array(
-            'httpClient' => 'http_client',
-            'messageStack' => 'message_stack',
-            'navigationHistory' => 'navigation_history',
-            'shoppingCart' => 'shopping_cart',
+            // ZenMagick
             'base' => 'class.base',
+            'httpClient' => 'http_client',
+            'messageStack' => 'message_stack', // admin overrides storefront
+            'navigationHistory' => 'navigation_history',
             'notifier' => 'class.notifier',
-            'queryFactory' => 'db/mysql/query_factory'
+            'queryFactory' => 'db/mysql/query_factory',
+            'queryFactoryResult' => 'db/mysql/query_factory',
+            'shoppingCart' => 'shopping_cart',
+            // ZenCart admin/storefront
+            'breadcrumb' => 'breadcrumb',
+            'category_tree' => 'category_tree',
+            'language' => 'language',
+            'products' => 'products',
+            'sniffer' => 'sniffer',
+            'splitPageResults' => 'split_page_results', // admin overrides storefront
+            'template_func' => 'template_func',
+            'PHPMailer' => 'class.phpmailer', // @todo remove legacy mailer support
+            'SMTP' => 'class.smtp',
+            // ZenCart Admin
+            'box' => 'box',
+            'objectInfo' => 'object_info',
+            'tableBlock' => 'table_block',
+            'upload' => 'upload',
         );
     }
 
+    /**
+     * Get the base directories used to find classes.
+     *
+     * @return array
+     */
+    public function getBaseDirectories() {
+        return $this->baseDirectories;
+    }
+
+    /**
+     * Set base directories to search for classes.
+     *
+     * @param array baseDirectories
+     */
+    public function setBaseDirectories($baseDirectories) {
+        $this->baseDirectories = $baseDirectories;
+    }
 
     /**
      * {@inheritDoc}
