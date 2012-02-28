96775d4b135c5c97573593d16c25a9aad3996d88
DerManoMann <mano@zenmagick.org>
2012-02-28 17:00:06 +1300

    refs #319: remove now obsolete methodController plugin

--- plugins/methodController/content/views/xform.php
+++
@@ -1,5 +0,0 @@
-<h1>xform</h1>
-<?php echo $form->open('xform', '', true, array('id'=>'xform')) ?>
-  <p><label for="name"><?php _vzm('Name') ?></label><br><input type="text" id="name" name="name" value=""></p>
-  <p><input type="submit" value="<?php _vzm('Submit') ?>">
-</form>
--- plugins/methodController/content/views/xform_success.php
+++
@@ -1,5 +0,0 @@
-<h1>xform success</h1>
-
-<p><?php _vzm('The name entered was: <strong>%s</strong>', $enteredName) ?></p>
-
-<p><a href="<?php echo $net->url('xform', '', true) ?>"><?php _vzm('Try again') ?></a>.</p>
