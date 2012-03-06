9e8280f220c082038706fa25db71b3f47f03b959
John Robeson <johnny@localmomentum.net>
2012-03-06 07:16:20 -0800

    clarify application_top.php

--- shared/store/bundles/ZenCartBundle/bridge/includes/application_top.php
+++ shared/store/bundles/ZenCartBundle/bridge/includes/application_top.php
@@ -43,8 +43,7 @@ if (!$contaminated) {
   }
 }
 unset($paramsToCheck, $paramsToAvoid, $key);
-if ($contaminated)
-{
+if ($contaminated) {
   header('HTTP/1.1 406 Not Acceptable');
   exit(0);
 }
@@ -54,30 +53,19 @@ unset($contaminated);
 define('IS_ADMIN_FLAG', false);
 define('PAGE_PARSE_START_TIME', microtime());
 define('DEBUG_AUTOLOAD', false);
-/**
- * set the level of error reporting
- *
- * Note STRICT_ERROR_REPORTING should never be set to true on a production site. <br />
- * It is mainly there to show php warnings during testing/bug fixing phases.<br />
- */
-//if (defined('STRICT_ERROR_REPORTING') && STRICT_ERROR_REPORTING == true) {
+
+/if (defined('STRICT_ERROR_REPORTING') && STRICT_ERROR_REPORTING == true) {
   @ini_set('display_errors', TRUE);
   error_reporting(version_compare(PHP_VERSION, 5.3, '>=') ? E_ALL & ~E_DEPRECATED & ~E_NOTICE : version_compare(PHP_VERSION, 5.4, '>=') ? E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_STRICT : E_ALL & ~E_NOTICE);
-/*} else {
+} else {
   error_reporting(0);
-}*/
+}
 
 
 // set php_self in the local scope
 if (!isset($PHP_SELF)) $PHP_SELF = $_SERVER['PHP_SELF'];
 
-/**
- * check for and include load application parameters
- */
 if (file_exists('includes/configure.php')) {
-  /**
-   * load the main configure file.
-   */
   include('includes/configure.php');
 } else {
   $problemString = 'includes/configure.php not found';
@@ -85,59 +73,22 @@ if (file_exists('includes/configure.php')) {
   exit;
 }
 
-/**
- * if main configure file doesn't contain valid info (ie: is dummy or doesn't match filestructure, display assistance page to suggest running the installer)
- */
-if (!defined('DIR_FS_CATALOG') || !is_dir(DIR_FS_CATALOG.'/includes/classes')) {
-  $problemString = 'includes/configure.php file contents invalid.  ie: DIR_FS_CATALOG not valid or not set';
-  require('includes/templates/template_default/templates/tpl_zc_install_suggested_default.php');
-  exit;
-}
-
-/**
- * include the list of extra configure files
- */
-if ($za_dir = @dir(DIR_WS_INCLUDES . 'extra_configures')) {
-  while ($zv_file = $za_dir->read()) {
-    if (preg_match('/\.php$/', $zv_file) > 0) {
-      /**
-       * load any user/contribution specific configuration files.
-       */
-      include(DIR_WS_INCLUDES . 'extra_configures/' . $zv_file);
-    }
-  }
-  $za_dir->close();
-  unset($za_dir);
+foreach(glob(DIR_FS_CATALOG.'includes/extra_configures/*.php') as $file) {
+    include $file;
 }
 /**
- * Admin only.. destroy laer
+ * Admin only.. destroy later
 $template_dir = '';
 define('DIR_WS_TEMPLATES', DIR_WS_INCLUDES . 'templates/');
 */
 
 
 $autoLoadConfig = array();
-if (isset($loaderPrefix)) {
- $loaderPrefix = preg_replace('/[^a-z_]/', '', $loaderPrefix);
-} else {
-  $loaderPrefix = 'config';
-}
+$loaderPrefix = isset($loaderPrefix) ? $loaderPrefix : 'config';
 $loader_file = $loaderPrefix . '.core.php';
 require('includes/initsystem.php');
-/**
- * determine install status
- */
-if (( (!file_exists('includes/configure.php') && !file_exists('includes/local/configure.php')) ) || (DB_TYPE == '') || (!file_exists('includes/classes/db/' .DB_TYPE . '/query_factory.php')) || !file_exists('includes/autoload_func.php')) {
-  $problemString = 'includes/configure.php file empty or file not found, OR wrong DB_TYPE set, OR cannot find includes/autoload_func.php which suggests paths are wrong or files were not uploaded correctly';
-  require('includes/templates/template_default/templates/tpl_zc_install_suggested_default.php');
-  header('location: zc_install/index.php');
-  exit;
-}
-
-/**
- * load the autoloader interpreter code.
-*/
 require('includes/autoload_func.php');
+
 if ($spider_flag == false) {
 // counter and counter history
   require(DIR_WS_INCLUDES . 'counter.php');
