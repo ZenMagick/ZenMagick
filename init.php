e71e287c0305e0d658ec2601482f10db2f16bbbe
John Robeson <johnny@localmomentum.net>
2012-03-07 01:43:24 -0800

    cleaning indenting, copyright in store.php,init.php

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * ZenMagick - Another PHP framework.
- * Copyright (C) 2006-2011 zenmagick.org
+ * Copyright (C) 2006-2012 zenmagick.org
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,51 +17,50 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-?>
-<?php
+
 use zenmagick\base\Runtime;
 use zenmagick\http\HttpApplication;
 
-  // avoid including twice
-  if (class_exists('zenmagick\base\Application')) {
-      return;
-  }
+// avoid including twice
+if (class_exists('zenmagick\base\Application')) {
+    return;
+}
 
-  $rootDir = __DIR__;
-  include_once $rootDir.'/lib/base/Application.php';
-  include_once $rootDir.'/lib/http/HttpApplication.php';
+$rootDir = __DIR__;
+include_once $rootDir.'/lib/base/Application.php';
+include_once $rootDir.'/lib/http/HttpApplication.php';
 
-  try {
-      $config = array('appName' => 'storefront', 'environment' => (isset($_SERVER['ZM_ENVIRONMENT']) ? $_SERVER['ZM_ENVIRONMENT'] : 'prod'));
-      $application = new HttpApplication($config);
-      $application->bootstrap();
+try {
+    $config = array('appName' => 'storefront', 'environment' => (isset($_SERVER['ZM_ENVIRONMENT']) ? $_SERVER['ZM_ENVIRONMENT'] : 'prod'));
+    $application = new HttpApplication($config);
+    $application->bootstrap();
 
-      $container = Runtime::getContainer();
-      $_zm_request = $request = $container->get('request');
-      // allow seo rewriters to fiddle with the request
-      $request->urlDecode();
+    $container = Runtime::getContainer();
+    $_zm_request = $request = $container->get('request');
+    // allow seo rewriters to fiddle with the request
+    $request->urlDecode();
 
-      // make sure we use the appropriate protocol (HTTPS, for example) if required
-      $container->get('sacsManager')->ensureAccessMethod($request);
+    // make sure we use the appropriate protocol (HTTPS, for example) if required
+    $container->get('sacsManager')->ensureAccessMethod($request);
 
-      // form validation
-      $validationConfig = Runtime::getApplicationPath().'/config/validation.yaml';
-      if ($container->has('validator') && file_exists($validationConfig)) {
-          $container->get('validator')->load(file_get_contents(Toolbox::resolveWithEnv($validationConfig)));
-      }
-
-      // load stuff that really needs to be global!
-      $settingsService = $container->get('settingsService');
-      if ($settingsService->get('zenmagick.base.plugins.enabled', true)) {
-          foreach (Runtime::getContainer()->get('pluginService')->getAllPlugins($settingsService->get('zenmagick.base.context')) as $plugin) {
-              foreach ($plugin->getGlobal($request) as $file) {
-                  include_once $file;
-              }
-          }
-      }
-    } catch (Exception $e) {
-        echo '<pre>';
-        echo $e->getTraceAsString();
-        echo '</pre>';
-        die(sprintf('init storefront failed: %s', $e->getMessage()));
+    // form validation
+    $validationConfig = Runtime::getApplicationPath().'/config/validation.yaml';
+    if ($container->has('validator') && file_exists($validationConfig)) {
+        $container->get('validator')->load(file_get_contents(Toolbox::resolveWithEnv($validationConfig)));
     }
+
+    // load stuff that really needs to be global!
+    $settingsService = $container->get('settingsService');
+    if ($settingsService->get('zenmagick.base.plugins.enabled', true)) {
+        foreach (Runtime::getContainer()->get('pluginService')->getAllPlugins($settingsService->get('zenmagick.base.context')) as $plugin) {
+            foreach ($plugin->getGlobal($request) as $file) {
+                include_once $file;
+            }
+        }
+    }
+} catch (Exception $e) {
+    echo '<pre>';
+    echo $e->getTraceAsString();
+    echo '</pre>';
+    die(sprintf('init storefront failed: %s', $e->getMessage()));
+}
