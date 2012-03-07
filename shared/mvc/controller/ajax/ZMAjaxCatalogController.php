a31d9ff07f8bebc339be9fd59f7ff177ee7a2d00
DerManoMann <mano@zenmagick.org>
2012-03-08 10:04:11 +1300

    fix regression and add method to lookup products for manufacturer

--- shared/mvc/controller/ajax/ZMAjaxCatalogController.php
+++ shared/mvc/controller/ajax/ZMAjaxCatalogController.php
@@ -61,8 +61,9 @@ class ZMAjaxCatalogController extends ZMAjaxController {
      */
     public function getProductForIdJSON($request) {
         $productId = $request->getProductId();
+        $languageId = $request->getParameter('languageId', $request->getSession()->getLanguageId());
 
-        $flatObj = ZMAjaxUtils::flattenObject($this->container->get('productService')->getProductForId($productId, $request->getSession()->getLanguageId()), $this->get('ajaxProductMap'));
+        $flatObj = ZMAjaxUtils::flattenObject($this->container->get('productService')->getProductForId($productId, $languageId), $this->get('ajaxProductMap'));
         $json = $this->toJSON($flatObj);
         $this->setJSONHeader($json);
     }
@@ -73,6 +74,7 @@ class ZMAjaxCatalogController extends ZMAjaxController {
      * <p>Request parameter:</p>
      * <ul>
      *  <li>categoryId - The category id</li>
+     *  <li>languageId - The language id</li>
      *  <li>active - Admin only parameter to allow to also retrieve inactive products</li>
      * </ul>
      *
@@ -81,6 +83,7 @@ class ZMAjaxCatalogController extends ZMAjaxController {
      */
     public function getProductsForCategoryIdJSON($request) {
         $categoryId = $request->getParameter('categoryId', 0);
+        $languageId = $request->getParameter('languageId', $request->getSession()->getLanguageId());
         $activeOnly = true;
         if (Runtime::isContextMatch('admin')) {
             $activeOnly = $request->getParameter('active', true);
@@ -88,11 +91,52 @@ class ZMAjaxCatalogController extends ZMAjaxController {
 
         if (null === ($page = $request->getParameter('page'))) {
             // return all
-            $flatObj = ZMAjaxUtils::flattenObject($this->container->get('productService')->getProductsForCategoryId($categoryId, $activeOnly), $this->get('ajaxProductMap'));
+            $flatObj = ZMAjaxUtils::flattenObject($this->container->get('productService')->getProductsForCategoryId($categoryId, $activeOnly, $languageId), $this->get('ajaxProductMap'));
         } else {
             // use result list to paginate
-            $args = array($categoryId, $activeOnly);
+            $args = array($categoryId, $activeOnly, $languageId);
             $resultSource = new ZMObjectResultSource('ZMProduct', 'productService', "getProductsForCategoryId", $args);
+            $resultList = Runtime::getContainer()->get('ZMResultList');
+            $resultList->setResultSource($resultSource);
+            $resultList->setPageNumber($page);
+            if (null !== ($pagination = $request->getParameter('pagination'))) {
+                $resultList->setPagination($pagination);
+            }
+            $flatObj = ZMAjaxUtils::flattenObject($resultList, $this->get('ajaxResultListMap'));
+        }
+
+        $json = $this->toJSON($flatObj);
+        $this->setJSONHeader($json);
+    }
+
+    /**
+     * Get products for the given manufacturer id.
+     *
+     * <p>Request parameter:</p>
+     * <ul>
+     *  <li>manufacturerId The manufacturer id</li>
+     *  <li>languageId - The language id</li>
+     *  <li>active - Admin only parameter to allow to also retrieve inactive products</li>
+     * </ul>
+     *
+     * @param ZMRequest request The current request.
+     * @return void
+     */
+    public function getProductsForManufacturerIdJSON($request) {
+        $manufacturerId = $request->getParameter('manufacturerId', 0);
+        $languageId = $request->getParameter('languageId', $request->getSession()->getLanguageId());
+        $activeOnly = true;
+        if (Runtime::isContextMatch('admin')) {
+            $activeOnly = $request->getParameter('active', true);
+        }
+
+        if (null === ($page = $request->getParameter('page'))) {
+            // return all
+            $flatObj = ZMAjaxUtils::flattenObject($this->container->get('productService')->getProductsForManufacturerId($manufacturerId, $activeOnly, $languageId), $this->get('ajaxProductMap'));
+        } else {
+            // use result list to paginate
+            $args = array($manufacturerId, $activeOnly, $languageId);
+            $resultSource = new ZMObjectResultSource('ZMProduct', 'productService', "getProductsForManufacturerId", $args);
             $resultList = Runtime::getContainer()->get('ZMResultList');
             $resultList->setResultSource($resultSource);
             $resultList->setPageNumber($page);
