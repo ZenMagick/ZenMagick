820329ca538e1705c97064baf981fee8e13f4712
John Robeson <johnny@localmomentum.net>
2012-03-08 13:44:38 -0800

    bring in autoload_func and initial core.config.php

---
+++ shared/store/bundles/ZenCartBundle/bridge/includes/auto_loaders/config.core.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * autoloader array for catalog application_top.php
+ * see  {@link  http://www.zen-cart.com/wiki/index.php/Developers_API_Tutorials#InitSystem wikitutorials} for more details.
+ *
+ * @package initSystem
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: config.core.php 4271 2006-08-26 01:21:02Z drbyte $
+ */
+ 
+/**
+ * Changes: 
+ *
+ * dropped classes already available via ZenCartClassLoader
+ * dropped init_sefu, init_gzip
+ * dropped db cache
+ * dropped phpbb3
+ */
+  $autoLoadConfig[0][] = array('autoType'=>'classInstantiate',
+                                'className'=>'notifier',
+                                'objectName'=>'zco_notifier');
+/**
+ * Breakpoint 10.
+ * 
+ * require('includes/init_includes/init_database.php');
+ * require('includes/version.php');
+ * 
+ */
+  $autoLoadConfig[10][] = array('autoType'=>'init_script',
+                                'loadFile'=> 'init_file_db_names.php');
+  $autoLoadConfig[10][] = array('autoType'=>'init_script',
+                                'loadFile'=>'init_database.php');
+/**
+ * Breakpoint 20.
+ * 
+ * require('includes/init_includes/init_file_db_names.php');
+ * 
+ */
+  $autoLoadConfig[20][] = array('autoType'=>'include',
+                                'loadFile'=> DIR_WS_INCLUDES . 'version.php');
+/**
+ * Breakpoint 30.
+ * 
+ * $zc_cache = new cache(); 
+ * 
+ */
+
+/**
+ * Breakpoint 40.
+ * 
+ * require('includes/init_includes/init_db_config_read.php');
+ * 
+ */
+  $autoLoadConfig[40][] = array('autoType'=>'init_script',
+                                'loadFile'=> 'init_db_config_read.php');
+/**
+ * Breakpoint 50.
+ * 
+ * $sniffer = new sniffer();
+ * require('includes/init_includes/init_gzip.php'); 
+ * require('includes/init_includes/init_sefu.php'); 
+ * $phpBB = new phpBB();
+ */
+  $autoLoadConfig[50][] = array('autoType'=>'classInstantiate',
+                                'className'=>'sniffer',
+                                'objectName'=>'sniffer');
+
+/**
+ * Breakpoint 60.
+ * 
+ * require('includes/init_includes/init_general_funcs.php'); 
+ * require('includes/init_includes/init_tlds.php'); 
+ * 
+ */
+  $autoLoadConfig[60][] = array('autoType'=>'init_script',
+                                'loadFile'=> 'init_general_funcs.php');
+  $autoLoadConfig[60][] = array('autoType'=>'init_script',
+                                'loadFile'=> 'init_tlds.php');
+/**
+ * Breakpoint 70.
+ * 
+ * require('includes/init_includes/init_sessions.php'); 
+ * 
+ */
+  $autoLoadConfig[70][] = array('autoType'=>'init_script',
+                                'loadFile'=> 'init_sessions.php');
+/**
+ * Breakpoint 80.
+ * 
+ * if(!$_SESSION['cart']) $_SESSION['cart'] = new shoppingCart();
+ * if(!$_SESSION['navigaton']) $_SESSION['navigation'] = new navigaionHistory();
+ * 
+ */
+  $autoLoadConfig[80][] = array('autoType'=>'classInstantiate',
+                                'className'=>'shoppingCart',
+                                'objectName'=>'cart',
+                                'checkInstantiated'=>true,
+                                'classSession'=>true);
+  $autoLoadConfig[80][] = array('autoType'=>'classInstantiate',
+                                'className'=>'navigationHistory',
+                                'objectName'=>'navigation',
+                                'checkInstantiated'=>true,
+                                'classSession'=>true);
+/**
+ * Breakpoint 90.
+ * 
+ * currencies = new currencies();
+ * 
+ */
+  $autoLoadConfig[90][] = array('autoType'=>'classInstantiate',
+                                'className'=>'currencies',
+                                'objectName'=>'currencies');
+/**
+ * Breakpoint 100.
+ * 
+ * require('includes/init_includes/init_sanitize.php'); 
+ * $template = new template_func();
+ * 
+ */
+  $autoLoadConfig[100][] = array('autoType'=>'classInstantiate',
+                                 'className'=>'template_func',
+                                 'objectName'=>'template');
+  $autoLoadConfig[100][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_sanitize.php');
+/**
+ * Breakpoint 110.
+ * 
+ * require('includes/init_includes/init_languages.php'); 
+ * require('includes/init_includes/init_templates.php'); 
+ * 
+ */
+  $autoLoadConfig[110][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_languages.php');
+  $autoLoadConfig[110][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_templates.php');
+/**
+ * Breakpoint 120.
+ * 
+ * $_SESSION['navigation']->add_current_page();
+ * require('includes/init_includes/init_currencies.php'); 
+ * 
+ */
+  $autoLoadConfig[120][] = array('autoType'=>'objectMethod',
+                                'objectName'=>'navigation',
+                                'methodName' => 'add_current_page');
+  $autoLoadConfig[120][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_currencies.php');
+/**
+ * Breakpoint 130.
+ * 
+ * require('includes/init_includes/init_customer_auth.php'); 
+ * messageStack = new messageStack();
+ * 
+ */
+  $autoLoadConfig[130][] = array('autoType'=>'classInstantiate',
+                                 'className'=>'messageStack',
+                                 'objectName'=>'messageStack');
+  $autoLoadConfig[130][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_customer_auth.php');
+/**
+ * Breakpoint 140.
+ * 
+ * require('includes/init_includes/init_cart_handler.php'); 
+ * 
+ */
+  $autoLoadConfig[140][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_cart_handler.php');
+/**
+ * Breakpoint 150.
+ * 
+ * require('includes/init_includes/init_special_funcs.php'); 
+ * 
+ */
+  $autoLoadConfig[150][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_special_funcs.php');
+/**
+ * Breakpoint 160.
+ * 
+ * require('includes/init_includes/init_category_path.php'); 
+ * $breadcrumb = new breadcrumb();
+ */
+  $autoLoadConfig[160][] = array('autoType'=>'classInstantiate',
+                                 'className'=>'breadcrumb',
+                                 'objectName'=>'breadcrumb');
+  $autoLoadConfig[160][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_category_path.php');
+/**
+ * Breakpoint 170.
+ * 
+ * require('includes/init_includes/init_add_crumbs.php'); 
+ * 
+ */
+  $autoLoadConfig[170][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_add_crumbs.php');
+/**
+ * Breakpoint 180.
+ * 
+ * require('includes/init_includes/init_header.php'); 
+ * 
+ */
+  $autoLoadConfig[180][] = array('autoType'=>'init_script',
+                                 'loadFile'=> 'init_header.php');
+?>
---
+++ shared/store/bundles/ZenCartBundle/bridge/includes/autoload_func.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+use zenmagick\base\Runtime;
+use zenmagick\apps\store\bundles\ZenCartBundle\ZenCartBundle;
+
+/**
+ * Cleaned up version of the Zen Cart auto loader
+ *
+ * @author Johnny Robeson
+ */
+reset($autoLoadConfig);
+ksort($autoLoadConfig);
+foreach ($autoLoadConfig as $actionPoint => $row) {
+    foreach($row as $entry) {
+        var_dump($entry);
+        $files = array();
+        $require = false;
+        switch($entry['autoType']) {
+            case 'classInstantiate':
+                if(!isset($entry['checkInstantiated'])) $entry['checkInstantiated'] = false;
+                if(!isset($entry['classSession'])) $entry['classSession'] = false;
+                $objectName = $entry['objectName'];
+                $className = $entry['className'];
+                if ($entry['classSession']) {
+                    if (!isset($_SESSION[$objectName]) || !$entry['checkInstantiated']) {
+                        $_SESSION[$objectName] = new $className();
+                    }
+                } else {
+                    $$objectName = new $className();
+                }
+            break;
+            case 'objectMethod':
+                $objectName = $entry['objectName'];
+                $methodName = $entry['methodName'];
+                $object = $_SESSION[$objectName];
+                if (is_object($object)) {
+                    $object->$methodName();
+                } else {
+                    $$objectName->$methodName();
+                }
+            break;
+            case 'init_script':
+                $files = ZenCartBundle::resolveFiles('includes/init_includes/'.$entry['loadFile']);
+            break;
+            case 'class':
+                $files = ZenCartBundle::resolveFiles('includes/classes/'.$entry['loadFile']);
+            break;
+            case 'require':
+                $require = true;
+            case 'include':
+            case 'include_glob':
+                $files = ZenCartBundle::resolveFiles($entry['loadFile']);
+            break;
+        }
+        foreach ($files as $file) {
+            if ($require) {
+                require $file;
+            } else {
+                include $file;
+            }
+        }
+    }
+}
