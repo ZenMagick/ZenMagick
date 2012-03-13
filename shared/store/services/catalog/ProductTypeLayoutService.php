8a670491299853b6a2c242db3c50b157038e9a7d
John Robeson <johnny@localmomentum.net>
2012-03-13 02:54:21 -0700

    add entity and service for ProductTypeLayout table
    This just implements loadAll() in the same way as
    ZMConfig::loadAll()

---
+++ shared/store/services/catalog/ProductTypeLayoutService.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2012 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+namespace zenmagick\apps\store\services\catalog;
+
+use zenmagick\base\Runtime;
+use zenmagick\base\ZMObject;
+
+/**
+ * Product Type Layout  service.
+ *
+ * @todo reuse most of the config service (ZMConfig)
+ */
+class ProductTypeLayoutService extends ZMObject {
+
+    /**
+     * Load all configuration values.
+     *
+     * @return array Map of all configuration values.
+     */
+    public function loadAll() {
+        $map = array();
+        $sql = "SELECT configuration_key, configuration_value FROM " . DB_PREFIX . "product_type_layout";
+        foreach (\ZMRuntime::getDatabase()->fetchAll($sql) as $result) {
+            $map[$result['configuration_key']] = $result['configuration_value'];
+        }
+
+        return $map;
+    }
+
+    /**
+     * Define all configuration keys
+     *
+     * @param bool check Check if the defines exist.
+     */
+    public function defineAll($check = false) {
+        foreach ($this->loadAll() as $key => $value) {
+            if ($check && !defined($key)) continue;
+            define($key, $value);
+        }
+    }
+}
