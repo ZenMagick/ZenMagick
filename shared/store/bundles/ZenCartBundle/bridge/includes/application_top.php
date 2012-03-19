ad648a8c2f9c169c451de7419e52e61687974fed
John Robeson <johnny@localmomentum.net>
2012-03-19 12:55:23 -0700

    ZenCartBundle: move app top inoculation code to init_begin

--- shared/store/bundles/ZenCartBundle/bridge/includes/application_top.php
+++ shared/store/bundles/ZenCartBundle/bridge/includes/application_top.php
@@ -17,44 +17,6 @@ use zenmagick\http\HttpApplication;
 use zenmagick\base\Runtime;
 use zenmagick\apps\store\bundles\ZenCartBundle\ZenCartBundle;
 
-/**
- * inoculate against hack attempts which waste CPU cycles
- * @todo does this help anything?
- */
-$contaminated = (isset($_FILES['GLOBALS']) || isset($_REQUEST['GLOBALS'])) ? true : false;
-$paramsToAvoid = array('GLOBALS', '_COOKIE', '_ENV', '_FILES', '_GET', '_POST', '_REQUEST', '_SERVER', '_SESSION', 'HTTP_COOKIE_VARS', 'HTTP_ENV_VARS', 'HTTP_GET_VARS', 'HTTP_POST_VARS', 'HTTP_POST_FILES', 'HTTP_RAW_POST_DATA', 'HTTP_SERVER_VARS', 'HTTP_SESSION_VARS');
-$paramsToAvoid[] = 'autoLoadConfig';
-$paramsToAvoid[] = 'mosConfig_absolute_path';
-$paramsToAvoid[] = 'hash';
-$paramsToAvoid[] = 'main';
-foreach($paramsToAvoid as $key) {
-  if (isset($_GET[$key]) || isset($_POST[$key]) || isset($_COOKIE[$key])) {
-    $contaminated = true;
-    break;
-  }
-}
-$paramsToCheck = array('main_page', 'cPath', 'products_id', 'language', 'currency', 'action', 'manufacturers_id', 'pID', 'pid', 'reviews_id', 'filter_id', 'zenid', 'sort', 'number_of_uploads', 'notify', 'page_holder', 'chapter', 'alpha_filter_id', 'typefilter', 'disp_order', 'id', 'key', 'music_genre_id', 'record_company_id', 'set_session_login', 'faq_item', 'edit', 'delete', 'search_in_description', 'dfrom', 'pfrom', 'dto', 'pto', 'inc_subcat', 'payment_error', 'order', 'gv_no', 'pos', 'addr', 'error', 'count', 'error_message', 'info_message', 'cID', 'page', 'credit_class_error_code');
-if (!$contaminated) {
-  foreach($paramsToCheck as $key) {
-    if (isset($_GET[$key]) && !is_array($_GET[$key])) {
-      if (substr($_GET[$key], 0, 4) == 'http' || strstr($_GET[$key], '//')) {
-        $contaminated = true;
-        break;
-      }
-      if (isset($_GET[$key]) && strlen($_GET[$key]) > 43) {
-        $contaminated = true;
-        break;
-      }
-    }
-  }
-}
-unset($paramsToCheck, $paramsToAvoid, $key);
-if ($contaminated) {
-  header('HTTP/1.1 406 Not Acceptable');
-  exit(0);
-}
-unset($contaminated);
-/* *** END OF INNOCULATION *** */
 
 if (!class_exists('zenmagick\base\Application')) {
 $rootDir = 'zenmagick';
--- shared/store/bundles/ZenCartBundle/bridge/includes/auto_loaders/config.core.php
+++ shared/store/bundles/ZenCartBundle/bridge/includes/auto_loaders/config.core.php
@@ -17,6 +17,8 @@
  * dropped db cache
  * dropped phpbb3
  */
+  $autoLoadConfig[0][] = array('autoType'=>'init_script',
+                                'loadFile'=> 'init_begin.php');
   $autoLoadConfig[0][] = array('autoType'=>'classInstantiate',
                                 'className'=>'notifier',
                                 'objectName'=>'zco_notifier');
