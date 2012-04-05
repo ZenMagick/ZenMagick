593ead70286f6988f5d77e5a4378e21c83db7e44
John Robeson <johnny@localmomentum.net>
2012-04-05 10:08:13 -0700

    ZenCartBundle: clean out auto loader comments

--- shared/store/bundles/ZenCartBundle/bridge/admin/includes/auto_loaders/config.core.php
+++ shared/store/bundles/ZenCartBundle/bridge/admin/includes/auto_loaders/config.core.php
@@ -19,46 +19,13 @@
 // +----------------------------------------------------------------------+
 // $Id: config.core.php 17921 2010-10-10 11:58:15Z wilt $
 //
-/**
- * autoloader array for catalog application_top.php
- *
- * @package admin
- * @copyright Copyright 2003-2005 zen-cart Development Team
-**/
-/**
- * 
- * require(DIR_WS_CLASSES . 'class.base.php'); 
- * require(DIR_WS_CLASSES . 'class.notifier.php'); 
- * $zco_notifier = new notifier()'
- * require(DIR_WS_CLASSES . 'sniffer.php'); 
- * require(DIR_WS_CLASSES . 'logger.php'); 
- * require(DIR_WS_CLASSES . 'shopping_cart.php'); 
- * require(DIR_WS_CLASSES . 'products.php'); 
- * require(DIR_WS_CLASSES . 'table_block.php');
- * require(DIR_WS_CLASSES . 'box.php'); 
- * require(DIR_WS_CLASSES . 'message_stack.php'); 
- * require(DIR_WS_CLASSES . 'split_page_results.php'); 
- * require(DIR_WS_CLASSES . 'object_info.php'); 
- * require(DIR_WS_CLASSES . 'class.phpmailer.php');
- * require(DIR_WS_CLASSES . 'class.smtp.php'); 
- * require(DIR_WS_CLASSES . 'upload.php'); 
- * 
- */
-
-
+//
   $autoLoadConfig[0][] = array('autoType'=>'include',
                                 'loadFile'=> '../includes/configure.php');
   $autoLoadConfig[0][] = array('autoType'=>'classInstantiate',
                                 'className'=>'notifier',
                                 'objectName'=>'zco_notifier');
-/**
- * Breakpoint 10.
- * 
- * require('includes/init_includes/init_file_db_names.php');
- * require('includes/init_includes/init_database.ph');
- * require('includes/version.php');
- * 
- */
+
   $autoLoadConfig[10][] = array('autoType'=>'include_glob',
                                 'loadFile'=>'includes/extra_configures/*.php');
   $autoLoadConfig[10][] = array('autoType'=>'require',
@@ -72,51 +39,22 @@
                                 'className'=>'queryFactory',
                                 'objectName'=> 'db');
   $autoLoadConfig[10][] = array('autoType'=>'require',
-                                'loadFile'=> '../includes/version.php');
-/**
- * Breakpoint 20.
- * 
- * require('includes/init_includes/init_db_config_read.php');
- * 
- */
+                               'loadFile'=> '../includes/version.php');
   $autoLoadConfig[20][] = array('autoType'=>'service',
                                 'name'=>'productTypeLayoutService',
                                 'method'=>'defineAll');
-/**
- * Breakpoint 30.
- * 
- * require('includes/init_includes/init_gzip.php');
- * $sniffer = new sniffer();
- * 
- */
+
   $autoLoadConfig[30][] = array('autoType'=>'classInstantiate',
                                 'className'=>'sniffer',
                                 'objectName'=>'sniffer');
-/**
- * Breakpoint 40.
- * 
- * require('includes/init_includes/init_general_funcs.php');
- * require('includes/init_includes/init_tlds.php');
- * 
- */
+
   $autoLoadConfig[40][] = array('autoType'=>'include_glob',
                                 'loadFile'=>'includes/functions/{general.php,database.php,functions_customers.php,functions_metatags.php,functions_prices.php,html_output.php,localization.php,password_funcs.php}');
   $autoLoadConfig[40][] = array('autoType'=>'include_glob',
                                 'loadFile'=> '../includes/functions/{audience.php,functions_email.php,sessions.php,zen_mail.php}');
   $autoLoadConfig[40][] = array('autoType'=>'include_glob',
                                 'loadFile'=> 'includes/functions/extra_functions/*.php');
-/**
- * Breakpoint 60.
- * 
- * require('includes/init_includes/init_sessions.php');
- * 
- */
-/**
- * Breakpoint 70.
- * 
- * require('includes/init_includes/init_languages.php');
- * 
- */
+
   $autoLoadConfig[70][] = array('autoType'=>'service',
                                 'name'=>'themeService',
                                 'method'=>'initThemes');
@@ -131,80 +69,19 @@
   $autoLoadConfig[70][] = array('autoType'=>'include_glob',
                                 'loadFile'=>'includes/languages/%language%/extra_definitions/*.php');
 
-/**
- * Breakpoint 80.
- * 
- * require('includes/init_includes/init_templates.php');
- * 
- */
   $autoLoadConfig[90][] = array('autoType'=>'classInstantiate',
                                 'className'=>'template_func',
                                 'objectName'=>'template');
-/**
- * Breakpoint 90.
- * 
- * $zc_products = new products();
- * require(DIRWS_FUNCTIONS . 'localization.php');
- * 
- */
+
   $autoLoadConfig[90][] = array('autoType'=>'classInstantiate',
                                 'className'=>'products',
                                 'objectName'=>'zc_products');
-/**
- * Breakpoint 100.
- * 
- * $messageStack = new messageStack();
- * 
- */
+
   $autoLoadConfig[100][] = array('autoType'=>'classInstantiate',
                                  'className'=>'messageStack',
                                  'objectName'=>'messageStack');
-/**
- * Breakpoint 120.
- * 
- * require('includes/init_includes/init_special_funcs.php');
- * 
- */
+
   $autoLoadConfig[120][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_special_funcs.php');
 
-/**
- * Breakpoint 130.
- * 
- * require('includes/init_includes/init_category_path.php');
- * 
- */
 
-/**
- * Breakpoint 140.
- * 
- * require('includes/init_includes/init_errors.php');
- * 
- */
-
-/**
- * Breakpoint 150.
- * 
- * require('includes/init_includes/init_admin_auth.php');
- * 
- */
-
-/**
- * Breakpoint 160.
- * 
- * require(DIR_FS_CATALOG . DIR_WS_FUNCTIONS . 'audience.php');
- * 
- */
-
-/**
- * Breakpoint 170.
- * 
- * require('includes/init_includes/init_admin_history.php');
- * 
- */
-/**
- * Breakpoint 180.
- * 
- * require('includes/init_includes/init_html_editor.php);
- * 
- */
--- shared/store/bundles/ZenCartBundle/bridge/includes/auto_loaders/config.core.php
+++ shared/store/bundles/ZenCartBundle/bridge/includes/auto_loaders/config.core.php
@@ -9,14 +9,6 @@
  * @version $Id: config.core.php 4271 2006-08-26 01:21:02Z drbyte $
  */
  
-/**
- * Changes: 
- *
- * dropped classes already available via ZenCartClassLoader
- * dropped init_sefu, init_gzip
- * dropped db cache
- * dropped phpbb3
- */
   $autoLoadConfig[0][] = array('autoType'=>'include',
                                'loadFile'=> 'includes/configure.php');
   $autoLoadConfig[0][] = array('autoType'=>'init_script',
@@ -24,13 +16,7 @@
   $autoLoadConfig[0][] = array('autoType'=>'classInstantiate',
                                 'className'=>'notifier',
                                 'objectName'=>'zco_notifier');
-/**
- * Breakpoint 10.
- * 
- * require('includes/init_includes/init_database.php');
- * require('includes/version.php');
- * 
- */
+
   $autoLoadConfig[10][] = array('autoType'=>'include_glob',
                                 'loadFile'=>'includes/extra_configures/*.php');
   $autoLoadConfig[10][] = array('autoType'=>'require',
@@ -44,77 +30,27 @@
   $autoLoadConfig[10][] = array('autoType'=>'classInstantiate',
                                 'className'=>'queryFactory',
                                 'objectName'=> 'db');
-/**
- * Breakpoint 20.
- * 
- * require('includes/init_includes/init_file_db_names.php');
- * 
- */
+
   $autoLoadConfig[20][] = array('autoType'=>'include', // actually used by the paypal modules!
                                 'loadFile'=> 'includes/version.php');
-/**
- * Breakpoint 30.
- * 
- * $zc_cache = new cache(); 
- * 
- */
 
-/**
- * Breakpoint 40.
- * 
- * require('includes/init_includes/init_db_config_read.php');
- * 
- */
-/**
- * Breakpoint 50.
- * 
- * $sniffer = new sniffer();
- * require('includes/init_includes/init_gzip.php'); 
- * require('includes/init_includes/init_sefu.php'); 
- * $phpBB = new phpBB();
- */
   $autoLoadConfig[50][] = array('autoType'=>'classInstantiate',
                                 'className'=>'sniffer',
                                 'objectName'=>'sniffer');
 
-/**
- * Breakpoint 60.
- * 
- * require('includes/init_includes/init_general_funcs.php'); 
- * require('includes/init_includes/init_tlds.php'); 
- * 
- */
   $autoLoadConfig[60][] = array('autoType'=>'include_glob',
                                 'loadFile'=> 'includes/functions/{functions_email.php,functions_general.php,html_output.php,functions_ezpages.php,sessions.php,zen_mail.php}');
   $autoLoadConfig[60][] = array('autoType'=>'include_glob',
                                 'loadFile'=> 'includes/functions/extra_functions/*.php');
-/**
- * Include PayPal-specific functions
- *  require('includes/modules/payment/paypal/paypal_functions.php');
- */
+
   $autoLoadConfig[60][] = array('autoType'=>'include',
                                 'loadFile'=> DIR_WS_MODULES . 'payment/paypal/paypal_functions.php',
                                 'loaderPrefix'=>'paypal_ipn');
 
-/**
- * Breakpoint 70.
- * 
- * require('includes/init_includes/init_sessions.php'); 
- * 
- */
   $autoLoadConfig[70][] = array('autoType'=>'service',
-                                'name'=>'session',
-                                'method'=>'getToken',
-                                'session' => true,
+                                'name'=>'session', 'method'=>'getToken', 'session' => true,
                                 'resultVar'=>'securityToken');
 
-/**
- * Breakpoint 80.
- * 
- * if(!$_SESSION['cart']) $_SESSION['cart'] = new shoppingCart();
- * if(!$_SESSION['navigaton']) $_SESSION['navigation'] = new navigaionHistory();
- * 
- */
   $autoLoadConfig[80][] = array('autoType'=>'classInstantiate',
                                  'className'=>'messageStack',
                                  'objectName'=>'messageStack');
@@ -133,34 +69,17 @@
  $autoLoadConfig[81][] = array('autoType'=>'init_script',
                                 'loadFile'=> 'init_paypal_ipn_sessions.php',
                                 'loaderPrefix'=>'paypal_ipn');
-/**
- * Breakpoint 90.
- * 
- * currencies = new currencies();
- * 
- */
+
   $autoLoadConfig[90][] = array('autoType'=>'classInstantiate',
                                 'className'=>'currencies',
                                 'objectName'=>'currencies');
-/**
- * Breakpoint 100.
- * 
- * require('includes/init_includes/init_sanitize.php'); 
- * $template = new template_func();
- * 
- */
+
   $autoLoadConfig[100][] = array('autoType'=>'classInstantiate',
                                  'className'=>'template_func',
                                  'objectName'=>'template');
   $autoLoadConfig[100][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_sanitize.php');
-/**
- * Breakpoint 110.
- * 
- * require('includes/init_includes/init_languages.php'); 
- * require('includes/init_includes/init_templates.php'); 
- * 
- */
+
   $autoLoadConfig[110][] = array('autoType'=>'include',
                                 'loadFile'=>'includes/classes/db/mysql/define_queries.php');
   $autoLoadConfig[110][] = array('autoType'=>'classInstantiate',
@@ -181,13 +100,6 @@
                                               'includes/languages/%language%/extra_definitions/%template_dir%/*.php',
                                               'includes/languages/%language%/extra_definitions/*.php'));
 
-/**
- * Breakpoint 120.
- * 
- * $_SESSION['navigation']->add_current_page();
- * require('includes/init_includes/init_currencies.php'); 
- * 
- */
   $autoLoadConfig[120][] = array('autoType'=>'service',
                                 'name'=>'productTypeLayoutService',
                                 'method'=>'defineAll');
@@ -197,49 +109,23 @@
                                 'loaderPrefix'=>'config');
   $autoLoadConfig[120][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_currencies.php');
-/**
- * Breakpoint 130.
- * 
- * require('includes/init_includes/init_customer_auth.php'); 
- * messageStack = new messageStack();
- * 
- */
+
   $autoLoadConfig[130][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_customer_auth.php',
                                  'loaderPrefix'=>'config');
-/**
- * Breakpoint 140.
- * 
- * require('includes/init_includes/init_cart_handler.php'); 
- * 
- */
+
   $autoLoadConfig[140][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_cart_handler.php');
-/**
- * Breakpoint 150.
- * 
- * require('includes/init_includes/init_special_funcs.php'); 
- * 
- */
+
   $autoLoadConfig[150][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_special_funcs.php');
-/**
- * Breakpoint 160.
- * 
- * require('includes/init_includes/init_category_path.php'); 
- * $breadcrumb = new breadcrumb();
- */
+
   $autoLoadConfig[160][] = array('autoType'=>'classInstantiate',
                                  'className'=>'breadcrumb',
                                  'objectName'=>'breadcrumb');
   $autoLoadConfig[160][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_category_path.php');
-/**
- * Breakpoint 170.
- * 
- * require('includes/init_includes/init_add_crumbs.php'); 
- * 
- */
+
   $autoLoadConfig[170][] = array('autoType'=>'init_script',
                                  'loadFile'=> 'init_add_crumbs.php',
                                  'loaderPrefix'=>'config');
@@ -249,12 +135,7 @@
                                                    'includes/languages/%language%/%template_dir%/checkout_process.php',
                                                    'includes/languages/%language%/checkout_process.php'),
                                  'loaderPrefix'=>'paypal_ipn');
-/**
- * Breakpoint 180.
- * 
- * require('includes/init_includes/init_header.php'); 
- * 
- */
+
   $autoLoadConfig[180][] = array('autoType'=>'init_script',
                                 'loadFile'=> 'init_header.php',
                                 'loaderPrefix'=>'config');
