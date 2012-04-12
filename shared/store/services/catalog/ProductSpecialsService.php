dcaac0769a5269e2f0003c13247c526beca88cb0
John Robeson <johnny@localmomentum.net>
2012-04-12 04:11:25 -0700

    refs #282:add ProductSpecialsService with scheduleSpecials()

---
+++ shared/store/services/catalog/ProductSpecialsService.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2012 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace zenmagick\apps\store\services\catalog;
+
+use DateTime;
+use zenmagick\base\Runtime;
+use zenmagick\base\ZMObject;
+
+/**
+ * Products on special.
+ *
+ */
+class ProductSpecialsService extends ZMObject {
+
+    public function runTasks() {
+        $this->scheduleSpecials();
+    }
+
+    /**
+     * Start/stop all specials.
+     *
+     * Stops all specials scheduled for expiration
+     * and starts all specials scheduled to be started.
+     *
+     * @todo finish product price retrieval that takes specials into account
+     */
+    public function scheduleSpecials() {
+        $sql = "SELECT specials_id, products_id, status, expires_date, specials_date_available
+                FROM " . TABLE_SPECIALS;
+        foreach (\ZMRuntime::getDatabase()->fetchAll($sql, array(), TABLE_SPECIALS, 'ZMSpecial') as $special) {
+            $availableDate = $special->getAvailableDate();
+            $expiryDate = $special->getExpiryDate();
+            $active = $special->getStatus();
+            if (!$active && null != $availableDate && new DateTime() >= $availableDate) {
+                $special->setStatus(true);
+                // @todo update product prices via product price sorter for $special?
+                \ZMRuntime::getDatabase()->updateModel(TABLE_SPECIALS, $special);
+                zen_update_products_price_sorter($special->getProductId());
+            }
+            // @todo the original code also disabled specials tht haven't started yet. is that something we should worry about?
+            if ($active && null != $expiryDate && new DateTime() >= $expiryDate) {
+                $special->setStatus(false);
+                // @todo update product prices via product price sorter for $special?
+                \ZMRuntime::getDatabase()->updateModel(TABLE_SPECIALS, $special);
+                zen_update_products_price_sorter($special->getProductId());
+            }
+        }
+    }
+}
