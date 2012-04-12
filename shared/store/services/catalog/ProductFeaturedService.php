8ff7204159549461723a521aff644a5e44185327
John Robeson <johnny@localmomentum.net>
2012-04-12 04:08:53 -0700

    refs #282:add ProductFeaturedService with scheduleFeatured()

---
+++ shared/store/services/catalog/ProductFeaturedService.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2012 zenmagick.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace zenmagick\apps\store\services\catalog;
+
+use DateTime;
+use zenmagick\base\Runtime;
+use zenmagick\base\ZMObject;
+
+/**
+ * Featured Products.
+ */
+class ProductFeaturedService extends ZMObject {
+
+    public function runTasks() {
+        $this->scheduleFeatured();
+    }
+
+    /**
+     * Start/stop all featured products.
+     *
+     * Stops all featured products scheduled for expiration
+     * and starts all featured products scheduled to be started.
+     */
+    public function scheduleFeatured() {
+        $sql = "SELECT featured_id, status, expires_date, featured_date_available
+                FROM " . TABLE_FEATURED;
+        foreach (\ZMRuntime::getDatabase()->fetchAll($sql, array(), TABLE_FEATURED, 'ZMFeature') as $feature) {
+            $availableDate = $feature->getAvailableDate();
+            $expiryDate = $feature->getExpiryDate();
+            $active = $feature->getStatus();
+            if (!$active && null != $availableDate && new DateTime() >= $availableDate) {
+                $feature->setStatus(true);
+                \ZMRuntime::getDatabase()->updateModel(TABLE_FEATURED, $feature);
+            }
+            // @todo the original code also disabled features tht haven't started yet. is that something we should worry about?
+            if ($active && null != $expiryDate && new DateTime() >= $expiryDate) {
+                $feature->setStatus(false);
+                \ZMRuntime::getDatabase()->updateModel(TABLE_FEATURED, $feature);
+            }
+        }
+    }
+}
