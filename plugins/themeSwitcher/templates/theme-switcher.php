1b301aa502cf04519ecc119466e09aa39fd779ea
DerManoMann <mano@zenmagick.org>
2012-07-06 20:58:35 +1200

    set z-index to avoid having the form covered

--- plugins/themeSwitcher/templates/theme-switcher.php
+++ plugins/themeSwitcher/templates/theme-switcher.php
@@ -17,8 +17,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */ if (0 < count($themeList)) { ?>
-  <div id="theme-switcher" style="text-align:right;padding:2px 8px;position:absolute;top:3px;right:4px;color:black;background-color:#abc;text-align:left;border:1px solid #ddd;border-radius:.5em;">
-    <label for="selected-theme"><?php _vzm('Themes:') ?> </label><br>
+  <div id="theme-switcher" style="z-index:1000;text-align:right;padding:2px 8px;position:absolute;top:3px;right:4px;color:black;background-color:#abc;text-align:left;border:1px solid #ddd;border-radius:.5em;">
+    <label for="selected-theme"><?php _vzm('Select Theme:') ?> </label><br>
     <select id="selected-theme" name="selectedTheme" onchange="window.location=this.value;" style="color:black;background:white;">
       <?php foreach ($themeList as $info) { ?>
       <option value="<?php echo $info['url'] ?>"<?php echo ($info['active'] ? ' selected="selected"' : '') ?>><?php echo $info['name'] ?></option>
