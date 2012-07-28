0a12cb06cc855f37d879552b90c748406722e26b
John Robeson <johnny@localmomentum.net>
2012-07-28 16:59:57 -0400

    shrink theme namespaces to zenmagick\themes\foo

--- themes/brightsideoflife/lib/EventListener.php
+++ themes/brightsideoflife/lib/EventListener.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-namespace zenmagick\apps\store\themes\brightsideoflife;
+namespace zenmagick\themes\brightsideoflife;
 
 use zenmagick\base\Runtime;
 use zenmagick\apps\store\themes\ThemeEventListener;
--- themes/test/lib/EventListener.php
+++ themes/test/lib/EventListener.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-namespace zenmagick\apps\store\themes\test;
+namespace zenmagick\themes\test;
 
 use zenmagick\base\Runtime;
 use zenmagick\apps\store\themes\ThemeEventListener;
