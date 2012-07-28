7b94fc65f8233159bbcf59ff303df6ad47a7146c
John Robeson <johnny@localmomentum.net>
2012-07-28 16:08:13 -0400

    Add a class loader to map other dirs under the zenmagick ns
    This is just the initial draft that prepares us for taking advantage
    of the new composer classmap generation code when it lands. It is meant
    to supplement the existing composer classloader during this
    transistionary period.
    
    It also makes clear some of the assumptions we make about directory
    structures and gives us a chance to remedy them.
    
    It supports all the current app name spaces (hardcoded), plugins,
    ,themes and shared.
    
    I expect that we will remove the lib directories under apps, plugins,
    and themes which would really simplify the implementation.

--- autoload.php
+++ autoload.php
@@ -9,6 +9,46 @@ if (!function_exists('intl_get_error_code')) {
     $loader->add('', __DIR__.'/vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 /**
+ * ZenMagick modified PSR-0 class loader.
+ *
+ * This is to support classes grouped under the zenmagick
+ * namespace.
+ * Example:
+ *   zenmagick\plugins\foo\controller\FooController => zmroot/plugins/foo/lib/controller/FooController
+ *   where the basename of zmroot is not zenmagick.
+ *
+ * @todo fix up the melded app\store namespace
+ * @todo remove regexes
+ * @todo probably remove plugin and theme lib directories
+ * @todo maybe remove app/appName/lib too?
+ * @todo revaluate once the classmap generation pull request is merged
+ *       <link>https://github.com/composer/composer/pull/811</link>
+ */
+spl_autoload_register(function ($class) {
+    if (0 === strpos($class, 'zenmagick')) {
+        $class = substr($class, 10);
+        if (false !== $pos = strrpos($class, '\\')) {
+            $classPath = str_replace('\\', DIRECTORY_SEPARATOR, substr($class, 0, $pos)) . DIRECTORY_SEPARATOR;
+            $className = substr($class, $pos + 1);
+            $classPath .= $className.'.php';
+            $fix = array( // @todo having the melded apps\store makes things difficult.
+                '|^apps/sample/|' => 'apps/sample/lib/',
+                '|^apps/store-installer/|' => 'apps/store-installer/lib/',
+                '|^apps/store/storefront/|' => 'apps/storefront/lib/',
+                '|^apps/store/admin/|' => 'apps/admin/lib/',
+                '|^apps/store/themes/(\w+)/|' => 'themes/${1}/lib/',
+                '|^apps/store/|' => 'shared/store/',
+                '|^plugins/(\w+)/|' => 'plugins/${1}/lib/',
+            );
+            $classPath = preg_replace(array_keys($fix), array_values($fix), $classPath);
+            $file = __DIR__.DIRECTORY_SEPARATOR.$classPath;
+            if (file_exists($file)) {
+                require_once $file;
+            }
+        }
+    }
+});
+/**
  * Preload Locales class to make translation functions available.
  */
 class_exists('zenmagick\base\locales\handler\pomo\POMO');
