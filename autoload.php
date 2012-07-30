490d7446be0fd282967737be3dd788916aabc13e
John Robeson <johnny@localmomentum.net>
2012-07-30 09:38:56 -0400

    move stuff from apps/storefront/lib to apps/storefront

--- autoload.php
+++ autoload.php
@@ -34,7 +34,6 @@ spl_autoload_register(function ($class) {
             $fix = array( // @todo having the melded apps\store makes things difficult.
                 '|^apps/sample/|' => 'apps/sample/lib/',
                 '|^apps/store-installer/|' => 'apps/store-installer/lib/',
-                '|^apps/storefront/|' => 'apps/storefront/lib/',
                 '|^apps/admin/|' => 'apps/admin/lib/',
                 '|^apps/store/|' => 'shared/store/',
             );
