56b4148f90890124abea99a28313c7e45a90eab7
John Robeson <johnny@localmomentum.net>
2012-08-30 23:56:08 -0400

    remove store-installer app until we can actually do one

--- apps/store-installer/StoreInstallerBundle.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace zenmagick\apps\store-installer;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StoreInstallerBundle extends Bundle {
-}
--- apps/store-installer/templates/views/index.php
+++
@@ -1,1 +0,0 @@
-<h1><?php _vzm('Hello world!') ?></h1>
