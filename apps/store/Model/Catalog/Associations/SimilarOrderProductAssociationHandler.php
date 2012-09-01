e24c7c897f039918320dcf8c001176d0d895b66b
John Robeson <johnny@localmomentum.net>
2012-09-01 14:55:17 -0400

    CamelCase errant associations namespace

--- apps/store/Model/Catalog/Associations/SimilarOrderProductAssociationHandler.php
+++ apps/store/Model/Catalog/Associations/SimilarOrderProductAssociationHandler.php
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-namespace ZenMagick\apps\store\Model\Catalog\associations;
+namespace ZenMagick\apps\store\Model\Catalog\Associations;
 
 use ZMRuntime;
 use ZenMagick\apps\store\Model\Catalog\ProductAssociation;
