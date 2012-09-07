019f6aae03eb454d3afded5166c5c38849e5573f
John Robeson <johnny@localmomentum.net>
2012-09-07 15:26:14 -0400

    rename StoreEventListener

--- lib/ZenMagick/StoreBundle/EventListener/StoreListener.php
+++ lib/ZenMagick/StoreBundle/EventListener/StoreListener.php
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
-namespace ZenMagick\StoreBundle;
+namespace ZenMagick\StoreBundle\EventListener;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -35,7 +35,7 @@ use ZenMagick\StoreBundle\Widgets\StatusCheck;
  *
  * @author DerManoMann
  */
-class StoreEventListener extends ZMObject {
+class StoreListener extends ZMObject {
     /**
      * {@inheritDoc}
      */
