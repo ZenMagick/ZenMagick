99a37bc9ae70641a9692ffd845f6f7c48e730b4e
John Robeson <johnny@localmomentum.net>
2012-09-09 08:50:35 -0400

    remove manual application route loading

--- lib/ZenMagick/ZenCartBundle/ZenCartListener.php
+++ lib/ZenMagick/ZenCartBundle/ZenCartListener.php
@@ -100,7 +100,6 @@ class ZenCartListener implements EventSubscriberInterface {
         $settingsService = $this->container->get('settingsService');
         if (Runtime::isContextMatch('admin')) {
             $settingsService->add('apps.store.admin.menus', 'lib/ZenMagick/ZenCartBundle/Resources/config/admin/menu.yaml');
-            $settingsService->add('zenmagick.http.routing.addnRouteFiles', __DIR__.'/Resources/config/admin/routing.xml');
         }
     }
 
