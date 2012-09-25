12dd1304495fc8a72191f9b80f7797b673edc9af
John Robeson <johnny@localmomentum.net>
2012-09-25 08:58:53 -0400

    add BlockConfig entity

---
+++ lib/ZenMagick/StoreBundle/Entity/Blocks/BlockConfig.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2012 zenmagick.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace ZenMagick\StoreBundle\Entity\Blocks;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="block_config")
+ * @ORM\Entity
+ */
+class BlockConfig {
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="block_config_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string $value
+     *
+     * @ORM\Column(name="value", type="text", nullable=true)
+     */
+    private $value;
+
+    /**
+     * @var string $definition
+     *
+     * @ORM\Column(name="definition", type="text", nullable=true)
+     */
+    private $definition;
+
+    /**
+     * @var BlocksToGroups
+     *
+     * @ORM\ManyToOne(targetEntity="Block")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="blocks_to_groups_id", referencedColumnName="blocks_to_groups_id")
+     * })
+     */
+    private $block;
+
+
+}
