cd0c672b560b9548696910c1d5df80f3f40b955e
John Robeson <johnny@localmomentum.net>
2012-09-26 04:27:55 -0400

    add meta tag entities

---
+++ lib/ZenMagick/StoreBundle/Entity/Catalog/CategoryMetaTagDetails.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * ZenMagick - Smart e-commerce
+ * Copyright (C) 2006-2012 zenmagick.org
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace ZenMagick\StoreBundle\Entity\Catalog;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="meta_tags_categories_description")
+ * @ORM\Entity
+ */
+class CategoryMetaTagDetails {
+    /**
+     * @var integer $categoryId
+     *
+     * @ORM\Column(name="categories_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $categoryId;
+
+    /**
+     * @var integer $languageId
+     *
+     * @ORM\Column(name="language_id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $languageId;
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="metatags_title", type="string", length=255, nullable=false)
+     */
+    private $title;
+
+    /**
+     * @var string $keywords
+     *
+     * @ORM\Column(name="metatags_keywords", type="text", nullable=true)
+     */
+    private $keywords;
+
+    /**
+     * @var string $description
+     *
+     * @ORM\Column(name="metatags_description", type="text", nullable=true)
+     */
+    private $description;
+
+
+}
