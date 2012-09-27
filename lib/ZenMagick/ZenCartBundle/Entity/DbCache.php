bad39567917630e86f8681e742d652cd2cd672f0
John Robeson <johnny@localmomentum.net>
2012-09-27 00:41:57 -0400

    add DbCache entity

---
+++ lib/ZenMagick/ZenCartBundle/Entity/DbCache.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ZenMagick\ZenCartBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ZenMagick\ZenCartBundle\Entity\DbCache
+ *
+ * @ORM\Table(name="db_cache")
+ * @ORM\Entity
+ */
+class DbCache {
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="cache_entry_name", type="string", length=64, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     */
+    private $name;
+
+    /**
+     * @var string $data
+     *
+     * @ORM\Column(name="cache_data", type="blob", nullable=true)
+     */
+    private $data;
+
+    /**
+     * @var integer $created
+     *
+     * @ORM\Column(name="cache_entry_created", type="integer", nullable=true)
+     */
+    private $created;
+
+
+}
