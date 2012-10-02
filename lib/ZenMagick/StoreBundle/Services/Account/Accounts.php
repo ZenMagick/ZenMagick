4bac2a0aab0f4681a230f74b131d05db12bcf17c
John Robeson <johnny@localmomentum.net>
2012-10-02 05:40:57 -0400

    fix accounts namespace

--- lib/ZenMagick/StoreBundle/Services/Account/Accounts.php
+++ lib/ZenMagick/StoreBundle/Services/Account/Accounts.php
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace ZenMagick\StoreBundle\Services;
+namespace ZenMagick\StoreBundle\Services\Account;
 
 use ZMRuntime;
 use ZenMagick\Base\Toolbox;
