3bf1be45783cdcbbf77168021cbdffa6d62f7b68
John Robeson <johnny@localmomentum.net>
2012-10-07 09:52:20 -0400

    get patch dir via __DIR__

--- apps/admin/Installation/InstallationPatcher.php
+++ apps/admin/Installation/InstallationPatcher.php
@@ -45,7 +45,7 @@ class InstallationPatcher extends ZMObject {
      * Load all patches.
      */
     public function _loadPatches() {
-        $path = Runtime::getInstallationPath().'/apps/admin/Installation/Patches';
+        $path = __DIR__.'/Patches';
         $ext = '.php';
         $this->patches_ = array();
         foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path)) as $filename => $fileInfo) {
