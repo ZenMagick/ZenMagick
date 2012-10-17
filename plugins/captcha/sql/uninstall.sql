7abb49d5e9ee5a6a6352a1435ac38781b29215e1
John Robeson <johnny@localmomentum.net>
2012-10-17 04:58:53 -0400

    remove captcha plugin in favor of other solutions
    Recaptcha is good, but another simpler possiblility is adding
    empty form fields that are hidden from the user to detect automated
    attempts.

--- plugins/captcha/sql/uninstall.sql
+++
@@ -1,4 +0,0 @@
-SET @configuration_group_id=0;
-SELECT (@configuration_group_id:=configuration_group_id) FROM configuration_group WHERE configuration_group_title= 'CAPTCHA' LIMIT 1;
-DELETE FROM configuration WHERE configuration_group_id = @configuration_group_id;
-DELETE FROM configuration_group WHERE configuration_group_id = @configuration_group_id;
