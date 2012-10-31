9f9133b06344def4fd7dbd75b08e9fa8686ab5b9
John Robeson <johnny@localmomentum.net>
2012-10-31 18:02:39 -0400

    fix 3 entity namespaces

--- lib/ZenMagick/StoreBundle/Entity/Catalog/ProductOptionType.php
+++ lib/ZenMagick/StoreBundle/Entity/Catalog/ProductOptionType.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace ZenMagick\StoreBundle\Entity;
+namespace ZenMagick\StoreBundle\Entity\Catalog;
 
 use Doctrine\ORM\Mapping as ORM;
 
--- lib/ZenMagick/StoreBundle/Entity/Order/Download.php
+++ lib/ZenMagick/StoreBundle/Entity/Order/Download.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace ZenMagick\StoreBundle\Entity;
+namespace ZenMagick\StoreBundle\Entity\Order;
 
 use Doctrine\ORM\Mapping as ORM;
 use ZenMagick\Base\Runtime;
