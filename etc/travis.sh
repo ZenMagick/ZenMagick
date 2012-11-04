b264151ed9aedeb70236f4ea674aeee605c0e7a6
DerManoMann <mano@zenmagick.org>
2012-11-04 15:18:42 +1300

    Move travis before into separate script to allow to run the same sequence locally

---
+++ etc/travis.sh
@@ -0,0 +1,7 @@
+php app/console doctrine:database:drop --env=install --force
+php app/console doctrine:database:create --env=install
+# the doctrine schema breaks the original SQL and also means we are missing the
+# default configuration settings that are loaded from the zc_install file
+#- php app/console doctrine:schema:update --force 
+php app/console --env=install dbal:import vendor/zenmagick/zencart/zc_install/sql/mysql_zencart.sql
+php app/console --env=install dbal:import etc/sql/mysql_demo.sql
