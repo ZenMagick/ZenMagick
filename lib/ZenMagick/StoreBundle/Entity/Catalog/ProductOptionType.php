01a824eadc711ea3e1163041a0a7111fee8a9876
John Robeson <johnny@localmomentum.net>
2012-11-06 13:31:40 -0500

    add getters/setters to ProductOptionType

--- lib/ZenMagick/StoreBundle/Entity/Catalog/ProductOptionType.php
+++ lib/ZenMagick/StoreBundle/Entity/Catalog/ProductOptionType.php
@@ -45,4 +45,50 @@ class ProductOptionType
      */
     private $name;
 
+
+    /**
+     * Set id
+     *
+     * @param integer $id
+     * @return ProductOptionType
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    
+        return $this;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return ProductOptionType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
