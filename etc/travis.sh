94fc36aad8c90a2443e657e77855ced3f2a9d209
John Robeson <johnny@localmomentum.net>
2012-11-09 15:57:44 -0500

    remove purge-with-truncate from travis.sh

--- etc/travis.sh
+++ etc/travis.sh
@@ -3,7 +3,6 @@
 php app/console --env=install doctrine:database:drop  --force
 php app/console --env=install doctrine:database:create
 php app/console --env=install doctrine:schema:create
-# @todo: purge with truncate won't work in tables with  foreign keys in mysql 5.5
-php app/console --env=install doctrine:fixtures:load --no-interaction --purge-with-truncate
+php app/console --env=install doctrine:fixtures:load --no-interaction
 php app/console --env=install dbal:import etc/sql/demo_tax.sql
 php app/console --env=install dbal:import etc/sql/mysql_demo.sql
