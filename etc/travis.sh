82fd7dac2eeab570b4af1e445eb7530a1871e1ad
John Robeson <johnny@localmomentum.net>
2012-11-09 15:48:40 -0500

    use doctrine schema and fixtures loading in etc/travis.sql

--- etc/travis.sh
+++ etc/travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-php app/console doctrine:database:drop --env=install --force
-php app/console doctrine:database:create --env=install
-# the doctrine schema breaks the original SQL and also means we are missing the
-# default configuration settings that are loaded from the zc_install file
-#- php app/console doctrine:schema:create
-php app/console --env=install dbal:import vendor/zenmagick/zencart/zc_install/sql/mysql_zencart.sql
+php app/console --env=install doctrine:database:drop  --force
+php app/console --env=install doctrine:database:create
+php app/console --env=install doctrine:schema:create
+# @todo: purge with truncate won't work in tables with  foreign keys in mysql 5.5
+php app/console --env=install doctrine:fixtures:load --no-interaction --purge-with-truncate
+php app/console --env=install dbal:import etc/sql/demo_tax.sql
 php app/console --env=install dbal:import etc/sql/mysql_demo.sql
