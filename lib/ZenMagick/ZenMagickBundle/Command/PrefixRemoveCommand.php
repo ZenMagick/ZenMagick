1f6b87aedbb27b7b116f5801990ae5243c2a4f4b
John Robeson <johnny@localmomentum.net>
2012-12-19 02:05:58 -0500

    add command to remove database tale prefixes
    It can be invoked by :
    
    app/console zenmagick:database:remove-prefix <prefix>

---
+++ lib/ZenMagick/ZenMagickBundle/Command/PrefixRemoveCommand.php
@@ -0,0 +1,50 @@
+<?php
+namespace ZenMagick\ZenMagickBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * DBAL import command.
+ */
+class PrefixRemoveCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+        ->setName('zenmagick:database:remove-prefix')
+        ->setDescription('Remove database table prefix.')
+        ->setDefinition(array(
+            new InputArgument(
+                'prefix', InputArgument::REQUIRED , 'Prefix to remove.'
+            )
+        ))
+        ->setHelp(<<<EOT
+Remove database table prefix.
+EOT
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $conn = $this->getContainer()->get('doctrine.dbal.default_connection');
+
+        $prefix = $input->getArgument('prefix');
+        $output->writeln('<info>Removing Prefix: </info>'.$prefix);
+
+        $sm = $conn->getSchemaManager();
+        foreach ($sm->listTables() as $table) {
+            if (0 === ($pos = strpos($table->getName(), $prefix))) {
+                $name = substr($table->getName(), strlen($prefix));
+                $sm->renameTable($table->getName(), $name);
+                $output->writeln($table->getName().' -> '.$name);
+            }
+        }
+
+    }
+
+}
