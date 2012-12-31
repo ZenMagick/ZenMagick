3159900ac3b16085ab15bd21dbc2277c81082a9c
John Robeson <johnny@localmomentum.net>
2012-12-31 07:06:28 -0500

    add ManyToOne for ConfigValue => ConfigGroup

--- lib/ZenMagick/StoreBundle/Entity/ConfigValue.php
+++ lib/ZenMagick/StoreBundle/Entity/ConfigValue.php
@@ -107,6 +107,12 @@ class ConfigValue extends ZMObject
     private $setFunction;
 
     /**
+     * @ORM\ManyToOne(targetEntity="ConfigGroup")
+     * @ORM\JoinColumn(name="configuration_group_id", referencedColumnName="configuration_group_id", onDelete="CASCADE")
+     */
+    private $group;
+
+    /**
      * Create new config value.
      */
     public function __construct()
