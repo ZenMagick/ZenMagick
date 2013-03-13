9f3a3eb5ea00d28c84de47f3a661559064283930
John Robeson <johnny@localmomentum.net>
2013-03-13 04:05:41 -0400

    remove <base> tag from <head>

--- lib/ZenMagick/StorefrontBundle/Resources/views/head.html.php
+++ lib/ZenMagick/StorefrontBundle/Resources/views/head.html.php
@@ -1,5 +1,4 @@
 <title><?php $view['slots']->output('title', $view->escape($metaTags->getTitle())) ?></title>
-<base href="/" />
 <meta charset="<?php echo $view->getCharset(); ?>" />
 <meta name="generator" content="ZenMagick <?php echo AppKernel::APP_VERSION ?>" />
 <meta name="keywords" content="<?php echo $view->escape($metaTags->getKeywords()) ?>" />
