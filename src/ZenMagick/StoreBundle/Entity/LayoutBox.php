e77cebd6382d44b0424f12cefed5c7577ccf966a
John Robeson <johnny@localmomentum.net>
2013-03-21 17:26:52 -0400

    fix 2 entity namespaces

--- src/ZenMagick/StoreBundle/Entity/LayoutBox.php
+++ src/ZenMagick/StoreBundle/Entity/LayoutBox.php
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace ZenMagick\StoreBundle\Entity\Templating;
+namespace ZenMagick\StoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
