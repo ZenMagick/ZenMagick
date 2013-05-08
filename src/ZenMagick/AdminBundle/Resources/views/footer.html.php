a3e383a4abba3b9a0f136a6f8ab4ca834c1ec7d4
John Robeson <johnny@localmomentum.net>
2013-05-08 03:25:09 -0400

    convert some admin templates to twig

--- src/ZenMagick/AdminBundle/Resources/views/footer.html.php
+++
@@ -1,7 +0,0 @@
-<div id="footer">
-  <a href="<?php echo $view['router']->generate('admin_index') ?>"><img src="<?php echo $view['assets']->getUrl('bundles/admin/images/logo-small.png') ?>" alt="ZenMagick" title="ZenMagick"></a>
-  <p>
-    <?php echo sprintf(_zm('Copyright &copy; 2006-2012 %s'), '<a target="_blank" href="http://www.zenmagick.org/">ZenMagick</a>') ?><br>
-    <?php echo sprintf(_zm('Design by %s'), '<a target="_blank" href="http://www.rubikintegration.com/">Rubik Integration</a>') ?>
-  </p>
-</div>
